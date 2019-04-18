af33bb8c494800ac9d9eff8ff842033d81aff7d5
Dylan Leard <dylan@button.is>
2019-04-18 16:02:14 -0700

    Completed 3 test TODOs in table_test

--- test/table_test.sql
+++ test/table_test.sql
@@ -4,20 +4,33 @@ reset client_min_messages;
 
 begin;
 
-select plan(0);
+select plan(3);
 
 /** Check table compliance **/
 
--- TODO: Schema ggircs should have descriptions for all tables
+-- Tables [table1, table2...] exist in Schema ggircs
+select tables_are('ggircs', array ['test']);
+
+-- Schema ggircs should have descriptions for all tables
+select matches(
+           obj_description('ggircs.test'::regclass, 'pg_class'),
+           '.+',
+           'Table raw_report has a description'
+         );
 
 -- TODO: Enforce table naming conventions
         -- Names are lower-case with underscores_as_word_separators
         -- Names are singular
         -- Avoid reserved keywords (ie. COMMENT -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
 
--- TODO: All tables must have a unique primary key
+-- All tables must have a unique primary key
+select has_pk(
+          'ggircs', 'test', 'Table has Primary key'
+        );
 
 -- TODO: Related tables must have foreign key constraints
         -- FK column names must match PK name from parent
+
+select * from finish();
 
 rollback;
