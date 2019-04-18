0c7e0946ba35b21875026ab3fcddc0f3a6b834f8
Dylan Leard <dylan@button.is>
2019-04-18 14:26:14 -0700

    added TODOs describing column compliance tests

--- test/column_ggircs.sql
+++ test/column_ggircs.sql
@@ -6,4 +6,17 @@ begin;
 
 select plan(0);
 
+-- TODO: DB should have descriptions for all columns
+
+-- TODO: Columns must have defined maximums for CHAR columns
+
+-- TODO: Columns must have defined Scale and Precision for NUMERIC columns
+
+-- TODO: Columns must be defined by an accepted data type
+
+-- TODO: Enforce column naming conventions
+        -- Names are lower-case with underscores_as_word_separators
+        -- Names are singular
+        -- Avoid reserved keywords (ie. COMMENT -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
+
 rollback;
