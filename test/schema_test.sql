871e08d0c8019097c51d6ae1584f007ddd9d8256
Dylan Leard <dylan@button.is>
2019-04-23 11:16:37 -0700

    added create schema ggircs_test_fixture to top of schema test file

--- test/schema_test.sql
+++ test/schema_test.sql
@@ -3,6 +3,8 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
+create schema ggircs_test_fixture;
+set search_path to ggircs_test_fixture,public;
 
 select plan(2);
 
@@ -10,12 +12,13 @@ select plan(2);
 
 -- TODO: generalize this to check multiple schemas
 -- Check schema exists
-select has_schema('ggircs');
+select has_schema('ggircs_test_fixture');
 
 -- GUIDELINE: Schema has a description
   -- Check schema for an existing description (regex '.+')
+  COMMENT ON SCHEMA ggircs_test_fixture IS 'has a description';
   select matches(
-            obj_description('ggircs'::regnamespace, 'pg_namespace'),
+            obj_description('ggircs_test_fixture'::regnamespace, 'pg_namespace'),
             '.+',
             'Schema ggircs has a description'
           );
