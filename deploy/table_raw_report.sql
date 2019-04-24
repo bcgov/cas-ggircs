7f7c4393bd60f33208893f4b3e1e61575f843e47
Dylan Leard <dylan@button.is>
2019-04-24 15:53:11 -0700

    added table_raw_report to plan and created deploy revert verify skeleton files

--- deploy/table_raw_report.sql
+++ deploy/table_raw_report.sql
@@ -1,6 +1,7 @@
 -- Deploy ggircs:table_raw_report to pg
 -- requires: schema_ggircs_private
 
+<<<<<<< befe56446eca5496e608064ee84ec21d33baf225
 begin;
 
 create table ggircs_private.raw_report (
@@ -16,3 +17,10 @@ comment on column ggircs_private.raw_report.imported_at is 'The timestamp noting
 --TODO: verify type and not null
 
 commit;
+=======
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
+>>>>>>> added table_raw_report to plan and created deploy revert verify skeleton files
--- revert/table_raw_report.sql
+++ revert/table_raw_report.sql
@@ -2,6 +2,10 @@
 
 BEGIN;
 
+<<<<<<< befe56446eca5496e608064ee84ec21d33baf225
 drop table ggircs_private.raw_report;
+=======
+-- XXX Add DDLs here.
+>>>>>>> added table_raw_report to plan and created deploy revert verify skeleton files
 
 COMMIT;
