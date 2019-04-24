322a88b5cf05ebb9803eea0d046ca540ed4f47ea
Alec Wenzowski <alec@button.is>
2019-04-24 16:50:59 -0700

    style adjustment

--- test/column_test.sql
+++ test/column_test.sql
@@ -1,112 +1,112 @@
-SET client_min_messages TO warning;
-CREATE extension IF NOT EXISTS pgtap;
-RESET client_min_messages;
+set client_min_messages to warning;
+create extension if not exists pgtap;
+reset client_min_messages;
 
-BEGIN;
+begin;
 
-CREATE schema ggircs_test_fixture;
-SET search_path TO ggircs_test_fixture,public;
-CREATE TABLE test_fixture(id SERIAL, name VARCHAR(50), lname VARCHAR, bad$Name VARCHAR(10), badnumber numeric);
-COMMENT ON COLUMN test_fixture.id IS 'has a description';
-COMMENT ON COLUMN test_fixture.name IS 'has a description';
-COMMENT ON COLUMN test_fixture.bad$name IS 'has a description';
-COMMENT ON COLUMN test_fixture.badnumber IS 'has a description';
+create schema ggircs_test_fixture;
+set search_path to ggircs_test_fixture,public;
+create table test_fixture(id serial, name varchar(50), lname varchar, bad$name varchar(10), bad_number numeric);
+comment on column test_fixture.id is 'has a description';
+comment on column test_fixture.name is 'has a description';
+comment on column test_fixture.bad$name is 'has a description';
+comment on column test_fixture.bad_number is 'has a description';
 
 
-SELECT plan(1657);
+select plan(1657);
 
-/** Check Column Compliance **/
+/** check column compliance **/
 
--- GUIDELINE: DB should have descriptions for all columns
-  -- Add comment to lname column, comment out next line to test the test
-  COMMENT ON COLUMN test_fixture.lname IS 'has a description';;
-  -- Get all columns within schema ggircs_test_fixture that do not have a comment
-  PREPARE nullComment AS SELECT pg_catalog.col_description(format('%s.%s',isc.table_schema,isc.table_name)::regclass::oid,isc.ordinal_position)
+-- guideline: db should have descriptions for all columns
+  -- add comment to lname column, comment out next line to test the test
+  comment on column test_fixture.lname is 'has a description';
+  -- get all columns within schema ggircs_test_fixture that do not have a comment
+  prepare null_comment as select pg_catalog.col_description(format('%s.%s',isc.table_schema,isc.table_name)::regclass::oid,isc.ordinal_position)
             as column_description
-            FROM information_schema.columns isc
-            WHERE table_schema='ggircs_test_fixture'
-            AND pg_catalog.col_description(format('%s.%s',isc.table_schema,isc.table_name)::regclass::oid,isc.ordinal_position) IS NULL;
-  -- Test that there are no results on the above query for null comments
-  SELECT is_empty(
-            'nullComment', 'Columns have descriptions'
+            from information_schema.columns isc
+            where table_schema='ggircs_test_fixture'
+            and pg_catalog.col_description(format('%s.%s',isc.table_schema,isc.table_name)::regclass::oid,isc.ordinal_position) is null;
+  -- test that there are no results on the above query for null comments
+  select is_empty(
+            'null_comment', 'Columns have descriptions'
             );
 
--- GUIDELINE: Columns must have defined maximums for CHAR columns
-  -- Drop column lname (has no char max length) comment out to test the test
-  ALTER TABLE test_fixture DROP COLUMN lname;
-  -- Get all max char lengths from char tables
-  PREPARE charCol AS SELECT columns.character_maximum_length
-                  FROM information_schema.columns
-                  WHERE table_schema = 'ggircs_test_fixture'
-                  AND data_type LIKE 'char%';
-  -- Get all nulls from character_maximum_length column
-  PREPARE nullCol AS SELECT columns.character_maximum_length
-                      FROM information_schema.columns
-                      WHERE table_schema = 'ggircs_test_fixture'
-                      AND character_maximum_length IS NULL;
-  -- Check there are no nulls for character_max_length when datatype is like 'char%' (INTERSECT ALL charcol <-> nullCol)
-  SELECT bag_hasnt(
-            'charCol', 'nullCol', 'columns have defined maximums'
+-- guideline: columns must have defined maximums for char columns
+  -- drop column lname (has no char max length) comment out to test the test
+  alter table test_fixture drop column lname;
+  -- get all max char lengths from char tables
+  prepare char_col as select columns.character_maximum_length
+                  from information_schema.columns
+                  where table_schema = 'ggircs_test_fixture'
+                  and data_type like 'char%';
+  -- get all nulls from character_maximum_length column
+  prepare null_col as select columns.character_maximum_length
+                      from information_schema.columns
+                      where table_schema = 'ggircs_test_fixture'
+                      and character_maximum_length is null;
+  -- check there are no nulls for character_max_length when datatype is like 'char%' (intersect all char_col <-> null_col)
+  select bag_hasnt(
+            'char_col', 'null_col', 'Columns have defined maximums'
         );
 
--- GUIDELINE: Columns must have defined Scale and Precision for NUMERIC columns
-  -- Drop column lacking precision and scale, add column with (p,s) to satisfy guideline. Comment out next line to test the test
-  ALTER TABLE test_fixture drop badnumber;
-  ALTER TABLE test_fixture ADD goodnumber numeric(6);
-  -- Get all numeric data types that return null when queried for their precision or scale
-  PREPARE numericCol AS SELECT columns.numeric_precision, columns.numeric_scale
-                    FROM information_schema.columns
-                    WHERE table_schema = 'ggircs_test_fixture'
-                    AND (data_type LIKE '%int%' 
-                        OR data_type LIKE '%serial%'
-                        OR data_type LIKE 'double%'
-                        OR data_type = 'decimal'
-                        OR data_type = 'numeric'
-                        OR data_type = 'real'
+-- guideline: columns must have defined scale and precision for numeric columns
+  -- drop column lacking precision and scale, add column with (p,s) to satisfy guideline. comment out next line to test the test
+  alter table test_fixture drop bad_number;
+  alter table test_fixture add good_number numeric(6);
+  -- get all numeric data types that return null when queried for their precision or scale
+  prepare numeric_col as select columns.numeric_precision, columns.numeric_scale
+                    from information_schema.columns
+                    where table_schema = 'ggircs_test_fixture'
+                    and (data_type like '%int%' 
+                        or data_type like '%serial%'
+                        or data_type like 'double%'
+                        or data_type = 'decimal'
+                        or data_type = 'numeric'
+                        or data_type = 'real'
                         )
-                    AND (columns.numeric_precision IS NULL OR columns.numeric_scale IS NULL);               
-  -- Check that the result of the above query is empty
-  SELECT is_empty(
-            'numericCol', 'numeric columns have precison and scale'
+                    and (columns.numeric_precision is null or columns.numeric_scale is null);               
+  -- check that the result of the above query is empty
+  select is_empty(
+            'numeric_col', 'Numeric columns have precison and scale'
   );
 
--- GUIDELINE: Columns must be defined by an accepted data_type
-  -- Get all colums in schema ggircs that have an undefined data_type
-  prepare noDataType AS SELECT data_type FROM information_schema.columns
-                  WHERE table_schema='ggircs_test_fixture'
-                  AND data_type IS NULL;
-  -- Check that the results returned by the above prepared statement are empty (no undefined data_types)
-  SELECT is_empty('noDataType', 'Columns must be defined by an accepted data_type');
+-- guideline: columns must be defined by an accepted data_type
+  -- get all colums in schema ggircs that have an undefined data_type
+  prepare no_data_type as select data_type from information_schema.columns
+                  where table_schema='ggircs_test_fixture'
+                  and data_type is null;
+  -- check that the results returned by the above prepared statement are empty (no undefined data_types)
+  select is_empty('no_data_type', 'columns must be defined by an accepted data_type');
 
--- GUIDELINE GROUP: Enforce column naming conventions
-        -- GUIDELINE: Names are lower-case with underscores_as_word_separators
-          -- Drop column bad$Name to comply with naming guideline, comment out next line to test the test
-          ALTER TABLE test_fixture DROP COLUMN bad$Name;
-          -- Check that all columns in schema do not return a match of capital letters or non-word characters
-          WITH cnames AS (SELECT column_name FROM information_schema.columns WHERE table_schema = 'ggircs_test_fixture')
-          SELECT doesnt_match(
+-- guideline group: enforce column naming conventions
+        -- guideline: names are lower-case with underscores_as_word_separators
+          -- drop column bad$name to comply with naming guideline, comment out next line to test the test
+          alter table test_fixture drop column bad$name;
+          -- check that all columns in schema do not return a match of capital letters or non-word characters
+          with cnames as (select column_name from information_schema.columns where table_schema = 'ggircs_test_fixture')
+          select doesnt_match(
                   col,
                   '[A-Z]|\W',
-                  'Column names are lower-case and separated by underscores'
-          ) FROM cnames F(col);
+                  'column names are lower-case and separated by underscores'
+          ) from cnames f(col);
 
-        -- TODO: Names are singular
+        -- TODO: names are singular
 
-        -- GUIDELINE: Avoid reserved keywords (ie. COMMENT -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
-          -- Drop table 'name' to comply with reserved keywords guideline, comment out next line to test the test
-          ALTER TABLE test_fixture DROP COLUMN name;
+        -- guideline: avoid reserved keywords (ie. comment -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
+          -- drop table 'name' to comply with reserved keywords guideline, comment out next line to test the test
+          alter table test_fixture drop column name;
           -- create table from csv list of reserved words
-          CREATE TABLE csv_import_fixture (csv_column_fixture TEXT);
-          \COPY csv_import_fixture FROM './test/fixture/sql_reserved_words.csv' delimiter ',' csv;
+          create table csv_import_fixture (csv_column_fixture text);
+          \copy csv_import_fixture from './test/fixture/sql_reserved_words.csv' delimiter ',' csv;
           -- test that all tables in schema do not contain any column names that intersect with reserved words csv dictionary
-          WITH reserved_words AS (SELECT csv_column_fixture FROM csv_import_fixture),
-          tnames AS (SELECT table_name FROM information_schema.tables WHERE table_schema = 'ggircs_test_fixture')
-          SELECT hasnt_column(
+          with reserved_words as (select csv_column_fixture from csv_import_fixture),
+          tnames as (select table_name from information_schema.tables where table_schema = 'ggircs_test_fixture')
+          select hasnt_column(
                   'ggircs_test_fixture',
                   tbl,
                   word,
                   format('Column names avoid reserved keywords. Violation: %I', word)
-          ) FROM reserved_words AS wtmp (word)
-          CROSS JOIN tnames AS ttmp (tbl);
+          ) from reserved_words as wtmp (word)
+          cross join tnames as ttmp (tbl);
 
-ROLLBACK;
+rollback;
--- test/table_test.sql
+++ test/table_test.sql
@@ -1,77 +1,77 @@
-SET client_min_messages TO warning;
-CREATE extension IF NOT EXISTS pgtap;
-RESET client_min_messages;
+set client_min_messages to warning;
+create extension if not exists pgtap;
+reset client_min_messages;
 
-BEGIN;
+begin;
 
-CREATE schema ggircs_test_fixture;
-SET search_path TO ggircs_test_fixture,public;
+create schema ggircs_test_fixture;
+set search_path to ggircs_test_fixture,public;
 
-SELECT plan(834);
+select plan(834);
 
-/** Check table compliance **/
+/** check table compliance **/
 
--- Create test tables
-  CREATE TABLE test_fixture(id SERIAL PRIMARY KEY, name VARCHAR(250));
-  COMMENT ON TABLE test_fixture IS 'has a description';
-  CREATE TABLE no_comment_no_pkey_fixture(id SERIAL, team VARCHAR(250));
-  CREATE TABLE name(id SERIAL, name VARCHAR(250));
-  COMMENT ON TABLE name IS 'has a description';
+-- create test tables
+  create table test_fixture(id serial primary key, name varchar(250));
+  comment on table test_fixture is 'has a description';
+  create table no_comment_no_pkey_fixture(id serial, team varchar(250));
+  create table name(id serial, name varchar(250));
+  comment on table name is 'has a description';
 
--- Tables [table1, table2...] exist in Schema ggircs
-SELECT tables_are('ggircs_test_fixture', array ['test_fixture', 'no_comment_no_pkey_fixture', 'name']);
+-- tables [table1, table2...] exist in schema ggircs
+select tables_are('ggircs_test_fixture', array ['test_fixture', 'no_comment_no_pkey_fixture', 'name']);
 
--- GUIDELINE: Schema ggircs should have descriptions for all tables
-  -- Add comment on test_1_fixture to comply with guideline, comment out to test the test
-  COMMENT ON TABLE no_comment_no_pkey_fixture IS 'has a description';
-  -- Check all tables for an existing description (regex '.+')
-  WITH tnames AS (SELECT table_name FROM information_schema.tables WHERE table_schema = 'ggircs_test_fixture')
-  SELECT matches(
+-- guideline: schema ggircs should have descriptions for all tables
+  -- add comment on test_1_fixture to comply with guideline, comment out to test the test
+  comment on table no_comment_no_pkey_fixture is 'has a description';
+  -- check all tables for an existing description (regex '.+')
+  with tnames as (select table_name from information_schema.tables where table_schema = 'ggircs_test_fixture')
+  select matches(
             obj_description(tbl::regclass, 'pg_class'),
             '.+',
             format('Table has a description. Violation: %I', tbl)
-          ) FROM tnames F(tbl);
+          ) from tnames f(tbl);
 
---GUIDELINE GROUP: Enforce table naming conventions
-  -- GUIDELINE: Names are lower-case with underscores_as_word_separators
-    -- Check that all table names do not return a match of capital letters or non-word characters
-    WITH tnames AS (SELECT table_name FROM information_schema.tables WHERE table_schema = 'ggircs_test_fixture')
-    SELECT doesnt_match(
+--guideline group: enforce table naming conventions
+  -- guideline: names are lower-case with underscores_as_word_separators
+    -- check that all table names do not return a match of capital letters or non-word characters
+    with tnames as (select table_name from information_schema.tables where table_schema = 'ggircs_test_fixture')
+    select doesnt_match(
           tbl,
           '[A-Z]|\W',
-          format('table names are lower-case and separated by underscores. Violation: %I', tbl)
-  ) FROM tnames F(tbl);
+          format('Table names are lower-case and separated by underscores. Violation: %I', tbl)
+  ) from tnames f(tbl);
 
-  -- TODO: Names are singular
-    -- POSTGRES stemmer
-    -- ACTIVE RECORD (Ruby/Rails)
+  -- TODO: names are singular
+    -- postgres stemmer
+    -- active record (ruby/rails)
 
-  -- GUIDELINE: Avoid reserved keywords (ie. COMMENT -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
-    -- Drop table 'name' to comply with reserved keywords guideline, comment out next line to test the test
-    DROP TABLE name;
+  -- guideline: avoid reserved keywords (ie. comment -> [name]_comment) https://www.drupal.org/docs/develop/coding-standards/list-of-sql-reserved-words
+    -- drop table 'name' to comply with reserved keywords guideline, comment out next line to test the test
+    drop table name;
     -- create table from csv list of reserved words
-    CREATE TABLE csv_import_fixture (csv_column_fixture TEXT);
-    \COPY csv_import_fixture FROM './test/fixture/sql_reserved_words.csv' delimiter ',' csv;
+    create table csv_import_fixture (csv_column_fixture text);
+    \copy csv_import_fixture from './test/fixture/sql_reserved_words.csv' delimiter ',' csv;
     -- test that schema does not contain any table names that intersect with reserved words csv dictionary
-    WITH reserved_words AS (SELECT csv_column_fixture FROM csv_import_fixture)
-    SELECT hasnt_table(
+    with reserved_words as (select csv_column_fixture from csv_import_fixture)
+    select hasnt_table(
             'ggircs_test_fixture',
             tbl,
             format('Table names avoid reserved keywords. Violation: %I', tbl)
-    ) FROM reserved_words F(tbl);
-    DROP TABLE csv_import_fixture;
+    ) from reserved_words f(tbl);
+    drop table csv_import_fixture;
 
--- GUIDELINE: All tables must have a unique primary key
-  -- Add Primary Key to table test_1_fixture (comment out to test the has_pk test)
-  ALTER TABLE no_comment_no_pkey_fixture ADD PRIMARY KEY (id);
-  -- pg_TAP built in test functuon for checking all tables in schema have a primary key
-  WITH tnames AS (SELECT table_name FROM information_schema.tables WHERE table_schema = 'ggircs_test_fixture')
-  SELECT has_pk(
-            'ggircs_test_fixture', tbl, format('Table has Primary key. Violation: %I', tbl)
-          ) FROM tnames F(tbl);
+-- guideline: all tables must have a unique primary key
+  -- add primary key to table test_1_fixture (comment out to test the has_pk test)
+  alter table no_comment_no_pkey_fixture add primary key (id);
+  -- pg_tap built in test functuon for checking all tables in schema have a primary key
+  with tnames as (select table_name from information_schema.tables where table_schema = 'ggircs_test_fixture')
+  select has_pk(
+            'ggircs_test_fixture', tbl, format('Table has primary key. Violation: %I', tbl)
+          ) from tnames f(tbl);
 
--- TODO: Related tables must have foreign key constraints : FK column names must match PK name from parent
+-- TODO: related tables must have foreign key constraints : fk column names must match pk name from parent
 
-SELECT * FROM finish();
+select * from finish();
 
-ROLLBACK;
+rollback;
