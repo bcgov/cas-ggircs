785b0f8d77181689e71f4b85a1e6bbee66f7aa4b
Dylan Leard <dylan@button.is>
2019-04-26 15:25:43 -0700

    added comments for columns in materialized view 'report'

--- deploy/materialized_view_report.sql
+++ deploy/materialized_view_report.sql
@@ -56,5 +56,21 @@ create unique index ggircs_swrs_report_primary_key on ggircs_swrs.report (id);
 create index ggircs_swrs_report_history on ggircs_swrs.report (swrs_report_history_id);
 
 comment on materialized view ggircs_swrs.report is 'the materialized view housing all report data, derived from raw_report table';
+comment on column ggircs_swrs.report.id is 'The primary key for the materialized view';
+comment on column ggircs_swrs.report.ghgr_id is 'The internal primary key for the file';
+comment on column ggircs_swrs.report.source_xml is 'The raw xml file imported from GHGR';
+comment on column ggircs_swrs.report.imported_at is 'The timestamp noting when the file was imported';
+comment on column ggircs_swrs.report.swrs_report_id is 'The swrs report id';
+comment on column ggircs_swrs.report.prepop_report_id is 'The prepop report id';
+comment on column ggircs_swrs.report.report_type is 'The type of report';
+comment on column ggircs_swrs.report.swrs_facility_id is 'The ID for the reporting facility';
+comment on column ggircs_swrs.report.swrs_organisation_id is 'The ID for the reporting organisation';
+comment on column ggircs_swrs.report.reporting_period_duration is 'The length of the reporting period contained in report';
+comment on column ggircs_swrs.report.status is 'The status of the report';
+comment on column ggircs_swrs.report.version is 'The report version';
+comment on column ggircs_swrs.report.submission_date is 'The date the report was submitted';
+comment on column ggircs_swrs.report.last_modified_by is 'The person who last modified the report';
+comment on column ggircs_swrs.report.update_comment is 'The description of the update';
+comment on column ggircs_swrs.report.swrs_report_history_id is 'The id denoting the history of the report (1 = latest)';
 
 commit;
