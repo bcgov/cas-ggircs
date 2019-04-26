40048e1632565bd11ac8bdd9fc306a332e08a9ec
Dylan Leard <dylan@button.is>
2019-04-26 11:47:54 -0700

    automated schema style tests to run through all schemas like ggircs%

--- test/style/schema_test.sql
+++ test/style/schema_test.sql
@@ -3,25 +3,28 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-create schema ggircs_test_fixture;
-set search_path to ggircs_test_fixture,public;
+-- create schema ggircs_test_fixture;
+-- set search_path to ggircs_test_fixture,public;
 
-select plan(2);
+select plan(4);
 
 /** Check schema compliance **/
 
--- TODO: generalize this to check multiple schemas
--- Check schema exists
-select has_schema('ggircs_test_fixture');
+-- Check schemas like ggircs%  exist
+with schema_names as (select schema_name from information_schema.schemata where schema_name like 'ggircs%')
+select has_schema(sch)
+from schema_names f(sch);
 
 -- GUIDELINE: Schema has a description
 -- Check schema for an existing description (regex '.+')
-comment on schema ggircs_test_fixture is 'has a description';
+-- comment on schema ggircs_swrs is 'has a description';
+with schema_names as (select schema_name from information_schema.schemata where schema_name like 'ggircs%')
 select matches(
-               obj_description('ggircs_test_fixture'::regnamespace, 'pg_namespace'),
+               obj_description(sch::regnamespace, 'pg_namespace'),
                '.+',
-               'Schema ggircs has a description'
-           );
+               format('All schemas have a description. Violation: %I', sch)
+           )
+from schema_names f(sch);
 
 select *
 from finish();
