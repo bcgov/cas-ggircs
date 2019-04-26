93720906c726477a43961caaff97cf136d44073b
Dylan Leard <dylan@button.is>
2019-04-26 11:49:23 -0700

    cleaned up dead code in schema_test.sql

--- test/style/schema_test.sql
+++ test/style/schema_test.sql
@@ -3,8 +3,6 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
--- create schema ggircs_test_fixture;
--- set search_path to ggircs_test_fixture,public;
 
 select plan(4);
 
@@ -17,7 +15,6 @@ from schema_names f(sch);
 
 -- GUIDELINE: Schema has a description
 -- Check schema for an existing description (regex '.+')
--- comment on schema ggircs_swrs is 'has a description';
 with schema_names as (select schema_name from information_schema.schemata where schema_name like 'ggircs%')
 select matches(
                obj_description(sch::regnamespace, 'pg_namespace'),
