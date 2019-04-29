e01c6cbc9dfb6f5eccbbb06cfa62ee938c03a001
Dylan Leard <dylan@button.is>
2019-04-29 14:27:33 -0700

    wrote tests relating to indices for matview organisation

--- test/unit/materialized_view_organisation_test.sql
+++ test/unit/materialized_view_organisation_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(12);
+select plan(14);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'organisation', 'Materialized view report exists');
@@ -22,9 +22,10 @@ select has_column('ggircs_swrs', 'organisation', 'swrs_organisation_history_id',
 --
 -- -- Test index names in matview report exist and are correct
 select has_index('ggircs_swrs', 'organisation', 'ggircs_organisation_primary_key', 'Matview organisation has index: ggircs_organisation_primary_key');
+select has_index('ggircs_swrs', 'organisation', 'ggircs_organisation_history', 'Matview organisation has index: ggircs_organisation_history');
 --
 -- -- Test unique indicies are defined unique
--- select index_is_unique('ggircs_swrs', 'report', 'ggircs_swrs_report_primary_key', 'Matview report index ggircs_swrs_report_primary_key is unique');
+select index_is_unique('ggircs_swrs', 'organisation', 'ggircs_organisation_primary_key', 'Matview report index ggircs_organisation_primary_key is unique');
 --
 -- -- Test columns in matview report have correct types
 -- select col_type_is('ggircs_swrs', 'report', 'id', 'bigint', 'Matview report column id has type bigint');
