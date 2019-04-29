fb16d0bf5b2f21ed3202b083d8caebc9a9229f89
Dylan Leard <dylan@button.is>
2019-04-29 15:25:51 -0700

    added facility to sqitch plan

---
+++ deploy/materialized_view_facility.sql
@@ -0,0 +1,10 @@
+-- Deploy ggircs:materialized_view_facility to pg
+-- requires: schema_ggircs_swrs
+-- requires: table_ghgr_import
+-- requires: materialized_view_report
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/materialized_view_facility.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs:materialized_view_facility on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
