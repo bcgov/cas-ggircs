43a97feb4b6c811226fe9951b5d0a4579aa6c80a
Dylan Leard <dylan@button.is>
2019-04-29 16:37:36 -0700

    added column type unit tests to matview facility

--- test/unit/materialized_view_facility_test.sql
+++ test/unit/materialized_view_facility_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(15);
+select plan(25);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'facility', 'Materialized view facility exists');
@@ -29,7 +29,16 @@ select has_index('ggircs_swrs', 'facility', 'ggircs_swrs_facility_history', 'Mat
 select index_is_unique('ggircs_swrs', 'facility', 'ggircs_facility_primary_key', 'Matview report index ggircs_facility_primary_key is unique');
 
 -- Test columns in matview report have correct types
--- select col_type_is('ggircs_swrs', 'facility', 'id', 'bigint', 'Matview facility column id has type bigint');
+select col_type_is('ggircs_swrs', 'facility', 'id', 'bigint', 'Matview facility column id has type bigint');
+select col_type_is('ggircs_swrs', 'facility', 'report_id', 'bigint', 'Matview facility column report_id has type bigint');
+select col_type_is('ggircs_swrs', 'facility', 'swrs_facility_id', 'numeric(1000,0)', 'Matview facility column swrs_facility_id has type numeric');
+select col_type_is('ggircs_swrs', 'facility', 'facility_name', 'character varying(1000)', 'Matview facility column facility_name has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'relationship_type', 'character varying(1000)', 'Matview facility column relationship_type has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'portability_indicator', 'character varying(1000)', 'Matview facility column portability_indicator has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'status', 'character varying(1000)', 'Matview facility column status has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'latitude', 'character varying(1000)', 'Matview facility column latitude has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'longitude', 'character varying(1000)', 'Matview facility column longitude has type varchar');
+select col_type_is('ggircs_swrs', 'facility', 'swrs_facility_history_id', 'bigint', 'Matview facility column swrs_facility_history_id has type bigint');
 
 select finish();
 rollback;
