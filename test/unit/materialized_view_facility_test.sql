e03a96fe4c7f6bfbc24979eba83b0924bfbef8e6
Dylan Leard <dylan@button.is>
2019-04-29 16:26:14 -0700

    added 'has column' tests for matview facility

--- test/unit/materialized_view_facility_test.sql
+++ test/unit/materialized_view_facility_test.sql
@@ -3,22 +3,32 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(1);
+select plan(12);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'facility', 'Materialized view facility exists');
 
 -- Test column names in matview report exist and are correct
--- select has_column('ggircs_swrs', 'organisation', 'id', 'Matview organisation has column: id');
+select has_column('ggircs_swrs', 'facility', 'id', 'Matview facility has column: id');
+select has_column('ggircs_swrs', 'facility', 'report_id', 'Matview facility has column: report_id');
+select has_column('ggircs_swrs', 'facility', 'swrs_facility_id', 'Matview facility has column: swrs_facility_id');
+select has_column('ggircs_swrs', 'facility', 'facility_name', 'Matview facility has column: facility_name');
+select has_column('ggircs_swrs', 'facility', 'facility_type', 'Matview facility has column: facility_type');
+select has_column('ggircs_swrs', 'facility', 'relationship_type', 'Matview facility has column: relationship_type');
+select has_column('ggircs_swrs', 'facility', 'portability_indicator', 'Matview facility has column: portability_indicator');
+select has_column('ggircs_swrs', 'facility', 'status', 'Matview facility has column: status');
+select has_column('ggircs_swrs', 'facility', 'latitude', 'Matview facility has column: latitude');
+select has_column('ggircs_swrs', 'facility', 'longitude', 'Matview facility has column: longitude');
+select has_column('ggircs_swrs', 'facility', 'swrs_facility_history_id', 'Matview facility has column: swrs_facility_history_id');
 
 -- Test index names in matview report exist and are correct
--- select has_index('ggircs_swrs', 'organisation', 'ggircs_organisation_primary_key', 'Matview organisation has index: ggircs_organisation_primary_key');
+-- select has_index('ggircs_swrs', 'facility', 'ggircs_facility_primary_key', 'Matview facility has index: ggircs_facility_primary_key');
 
 -- Test unique indicies are defined unique
--- select index_is_unique('ggircs_swrs', 'organisation', 'ggircs_organisation_primary_key', 'Matview report index ggircs_organisation_primary_key is unique');
+-- select index_is_unique('ggircs_swrs', 'facility', 'ggircs_facility_primary_key', 'Matview report index ggircs_facility_primary_key is unique');
 
 -- Test columns in matview report have correct types
--- select col_type_is('ggircs_swrs', 'organisation', 'id', 'bigint', 'Matview organisation column id has type bigint');
+-- select col_type_is('ggircs_swrs', 'facility', 'id', 'bigint', 'Matview facility column id has type bigint');
 
 select finish();
 rollback;
