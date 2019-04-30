f3cfd0b5d5c829eaaabce65c1d11c614f29b2b4b
Alec Wenzowski <alec@button.is>
2019-04-30 14:18:51 -0700

    remove extranneous join from ggircs_swrs.flat

--- deploy/materialized_view_flat.sql
+++ deploy/materialized_view_flat.sql
@@ -6,14 +6,10 @@ begin;
 create materialized view ggircs_swrs.flat as (
   with x as (
     select ghgr_import.id,
-           swrs_report_id,
            xpath('//*[./text()[normalize-space(.)]/parent::* or ./@*]', xml_file) as tag
     from ggircs_swrs.ghgr_import
-           inner join ggircs_swrs.report
-                      on report.ghgr_id = ghgr_import.id
-    where report.swrs_report_history_id = 1
   )
-  select x.swrs_report_id,
+  select x.id as ghgr_import_id,
          element_id,
          (xpath('name(/*)', node))[1]::varchar(1000)        as class,
          (xpath('name(/*/@*)', node))[1]::varchar(1000)     as attr,
@@ -21,13 +17,13 @@ create materialized view ggircs_swrs.flat as (
   from x,
        unnest(x.tag)
          with ordinality as t(node, element_id)
-  order by swrs_report_id desc, element_id asc
+  order by ghgr_import_id desc, element_id asc
 );
-create unique index ggircs_flat_primary_key on ggircs_swrs.flat (swrs_report_id, element_id);
-create index ggircs_flat_report on ggircs_swrs.flat (swrs_report_id);
+create unique index ggircs_flat_primary_key on ggircs_swrs.flat (ghgr_import_id, element_id);
+create index ggircs_flat_report on ggircs_swrs.flat (ghgr_import_id);
 
 comment on materialized view ggircs_swrs.flat is 'The flat view of data in the swrs report';
-comment on column ggircs_swrs.flat.swrs_report_id is 'The swrs report id';
+comment on column ggircs_swrs.flat.ghgr_import_id is 'The internal ghgr_import id';
 comment on column ggircs_swrs.flat.element_id is 'The element id';
 comment on column ggircs_swrs.flat.class is 'The class';
 comment on column ggircs_swrs.flat.attr is 'The attribute';
--- test/unit/materialized_view_flat_test.sql
+++ test/unit/materialized_view_flat_test.sql
@@ -9,43 +9,31 @@ select plan(15);
 select has_materialized_view('ggircs_swrs', 'flat', 'Materialized view flat exists');
 
 -- Test column names in matview report exist and are correct
-select has_column('ggircs_swrs', 'flat', 'swrs_report_id', 'Matview flat has column: swrs_report_id');
-select has_column('ggircs_swrs', 'flat', 'element_id', 'Matview flat has column: element_id');
-select has_column('ggircs_swrs', 'flat', 'class', 'Matview flat has column: class');
-select has_column('ggircs_swrs', 'flat', 'attr', 'Matview flat has column: attr');
-select has_column('ggircs_swrs', 'flat', 'value', 'Matview flat has column: value');
+select has_column('ggircs_swrs', 'flat', 'ghgr_import_id', 'ggircs_swrs.flat.ghgr_import_id exists');
+select has_column('ggircs_swrs', 'flat', 'element_id', 'ggircs_swrs.flat.element_id exists');
+select has_column('ggircs_swrs', 'flat', 'class', 'ggircs_swrs.flat.class exists');
+select has_column('ggircs_swrs', 'flat', 'attr', 'ggircs_swrs.flat.attr exists');
+select has_column('ggircs_swrs', 'flat', 'value', 'ggircs_swrs.flat.value exists');
 
 -- Test index names in matview report exist and are correct
-select has_index('ggircs_swrs', 'flat', 'ggircs_flat_primary_key', 'Matview flat has index: ggircs_flat_primary_key');
-select has_index('ggircs_swrs', 'flat', 'ggircs_flat_report', 'Matview flat has index: ggircs_flat_report');
+select has_index('ggircs_swrs', 'flat', 'ggircs_flat_primary_key', 'ggircs_swrs.flat has index: ggircs_flat_primary_key');
+select has_index('ggircs_swrs', 'flat', 'ggircs_flat_report', 'ggircs_swrs.flat has index: ggircs_flat_report');
 
 -- Test unique indicies are defined unique
-select index_is_unique('ggircs_swrs', 'flat', 'ggircs_flat_primary_key', 'Matview report index ggircs_flat_primary_key is unique');
+select index_is_unique('ggircs_swrs', 'flat', 'ggircs_flat_primary_key', 'ggircs_swrs.flat index ggircs_flat_primary_key is unique');
 
 -- Test columns in matview report have correct types
-select col_type_is('ggircs_swrs', 'flat', 'swrs_report_id', 'numeric(1000,0)', 'Matview flat column swrs_report_id has type numeric');
-select col_type_is('ggircs_swrs', 'flat', 'element_id', 'bigint', 'Matview flat column element_id has type bigint');
-select col_type_is('ggircs_swrs', 'flat', 'class', 'character varying(1000)', 'Matview flat column class has type varchar');
-select col_type_is('ggircs_swrs', 'flat', 'attr', 'character varying(1000)', 'Matview flat column attr has type varchar');
-select col_type_is('ggircs_swrs', 'flat', 'value', 'character varying(1000)', 'Matview flat column value has type varchar');
-
+select col_type_is('ggircs_swrs', 'flat', 'ghgr_import_id', 'integer', 'ggircs_swrs.flat.ghgr_import_id has type integer');
+select col_type_is('ggircs_swrs', 'flat', 'element_id', 'bigint', 'ggircs_swrs.flat.element_id has type bigint');
+select col_type_is('ggircs_swrs', 'flat', 'class', 'character varying(1000)', 'ggircs_swrs.flat.class has type varchar');
+select col_type_is('ggircs_swrs', 'flat', 'attr', 'character varying(1000)', 'ggircs_swrs.flat.attr has type varchar');
+select col_type_is('ggircs_swrs', 'flat', 'value', 'character varying(1000)', 'ggircs_swrs.flat.value has type varchar');
 
 -- insert necessary data into table ghgr_import
 insert into ggircs_swrs.ghgr_import (imported_at, xml_file) values ('2018-09-29T11:55:39.423', $$
     <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
       <ReportDetails>
         <ReportID>800855555</ReportID>
-        <PrepopReportID>5</PrepopReportID>
-        <ReportType>R7</ReportType>
-        <FacilityId>666</FacilityId>
-        <OrganisationId>1337</OrganisationId>
-        <ReportingPeriodDuration>1999</ReportingPeriodDuration>
-        <ReportStatus>
-          <Status>In Progress</Status>
-          <Version>3</Version>
-          <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
-          <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
-        </ReportStatus>
       </ReportDetails>
     </ReportData>
 $$);
@@ -56,16 +44,7 @@ refresh materialized view ggircs_swrs.flat with data;
 
 -- test the columnns for matview facility have been properly parsed from xml
 select results_eq('select * from ggircs_swrs.flat', $$ VALUES
-(800855555::numeric,1::bigint, 'ReportID'::varchar,''::varchar,800855555::varchar),
-(800855555::numeric,2::bigint, 'PrepopReportID'::varchar,''::varchar,5::varchar),
-(800855555::numeric,3::bigint, 'ReportType'::varchar,''::varchar,'R7'::varchar),
-(800855555::numeric,4::bigint, 'FacilityId'::varchar,''::varchar,666::varchar),
-(800855555::numeric,5::bigint, 'OrganisationId'::varchar,''::varchar,1337::varchar),
-(800855555::numeric,6::bigint, 'ReportingPeriodDuration'::varchar,''::varchar,1999::varchar),
-(800855555::numeric,7::bigint, 'Status'::varchar,''::varchar,'In Progress'::varchar),
-(800855555::numeric,8::bigint, 'Version'::varchar,''::varchar,3::varchar),
-(800855555::numeric,9::bigint, 'LastModifiedBy'::varchar,''::varchar,'Donny Donaldson McDonaldface'::varchar),
-(800855555::numeric,10::bigint,'LastModifiedDate'::varchar,''::varchar,'2018-09-28T11:55:39.423'::varchar)
+(2::integer,1::bigint, 'ReportID'::varchar,''::varchar,'800855555'::varchar)
 $$, 'ggircs_swrs.flat() should return all xml nodes with values as rows');
 
 select finish();
