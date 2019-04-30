0e81f1feff22c1aa737cecee553a339287a779ac
Dylan Leard <dylan@button.is>
2019-04-30 16:34:36 -0700

    added matview identifier to sqitch plan

---
+++ revert/materialized_view_identifier.sql
@@ -0,0 +1,7 @@
+-- Revert ggircs:materialized_view_identifier from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/materialized_view_identifier.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs:materialized_view_identifier on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
