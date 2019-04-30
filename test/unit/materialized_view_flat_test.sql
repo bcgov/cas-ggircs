e4bd8da9995cbe25d708b0a28c7e34713c78c9d1
Dylan Leard <dylan@button.is>
2019-04-30 12:42:44 -0700

    added xml import fixture to matview flat test

--- test/unit/materialized_view_flat_test.sql
+++ test/unit/materialized_view_flat_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(14);
+select plan(19);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'flat', 'Materialized view flat exists');
@@ -31,47 +31,34 @@ select col_type_is('ggircs_swrs', 'flat', 'value', 'character varying(1000)', 'M
 
 
 -- insert necessary data into table ghgr_import
--- insert into ggircs_swrs.ghgr_import (xml_file) values ($$
---     <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
---       <RegistrationData>
---         <Facility>
---           <Details>
---             <FacilityName>Stark Tower</FacilityName>
---             <RelationshipType>complicated</RelationshipType>
---             <PortabilityIndicator>P</PortabilityIndicator>
---             <Status>Active</Status>
---           </Details>
---           <Address>
---             <GeographicAddress>
---               <Latitude>23.45125</Latitude>
---               <Longitude>-90.59062</Longitude>
---             </GeographicAddress>
---           </Address>
---         </Facility>
---       </RegistrationData>
---       <ReportDetails>
---         <ReportID>800855555</ReportID>
---         <PrepopReportID>5</PrepopReportID>
---         <ReportType>R7</ReportType>
---         <FacilityId>666</FacilityId>
---         <OrganisationId>1337</OrganisationId>
---         <ReportingPeriodDuration>1999</ReportingPeriodDuration>
---         <ReportStatus>
---           <Status>In Progress</Status>
---           <Version>3</Version>
---           <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
---           <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
---         </ReportStatus>
---       </ReportDetails>
---     </ReportData>
---
--- $$);
+insert into ggircs_swrs.ghgr_import (imported_at, xml_file) values ('2018-09-29T11:55:39.423', $$
+    <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <ReportDetails>
+        <ReportID>800855555</ReportID>
+        <PrepopReportID>5</PrepopReportID>
+        <ReportType>R7</ReportType>
+        <FacilityId>666</FacilityId>
+        <OrganisationId>1337</OrganisationId>
+        <ReportingPeriodDuration>1999</ReportingPeriodDuration>
+        <ReportStatus>
+          <Status>In Progress</Status>
+          <Version>3</Version>
+          <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
+          <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
+        </ReportStatus>
+      </ReportDetails>
+    </ReportData>
+$$);
 
 -- refresh necessary views with data
--- refresh materialized view ggircs_swrs.report with data;
+refresh materialized view ggircs_swrs.report with data;
 
 -- test the columnns for matview facility have been properly parsed from xml
--- select results_eq('select id from ggircs_swrs.facility', ARRAY[1::bigint], 'Matview facility parsed column id');
+select results_eq('select swrs_report_id from ggircs_swrs.flat', ARRAY[800855555::numeric], 'Matview flat parsed column swrs_report_id');
+select results_eq('select element_id from ggircs_swrs.flat', ARRAY[1::bigint], 'Matview flat parsed column element_id');
+select results_eq('select class from ggircs_swrs.flat', ARRAY[''::varchar], 'Matview flat parsed column class');
+select results_eq('select attr from ggircs_swrs.flat', ARRAY[''::varchar], 'Matview flat parsed column attr');
+select results_eq('select value from ggircs_swrs.flat', ARRAY[''::varchar], 'Matview flat parsed column value');
 
 select finish();
 rollback;
