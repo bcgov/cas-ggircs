863f605c5acc5d879bcc285c8804ed5db065b9d6
Dylan Leard <dylan@button.is>
2019-05-01 16:53:35 -0700

    added matview naics to sqitch plan

---
+++ deploy/materialized_view_naics.sql
@@ -0,0 +1,8 @@
+-- Deploy ggircs:materialized_view_naics to pg
+-- requires: materialized_view_facility
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ verify/materialized_view_naics.sql
@@ -0,0 +1,7 @@
+-- Verify ggircs:materialized_view_naics on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
