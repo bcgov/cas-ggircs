43fef0fc3bb14f54f6bdbb9787b29247f03fcdc5
Dylan Leard <dylan@button.is>
2019-05-02 16:46:51 -0700

    cleaned up ggircs_swrs.unit to align with changes made to ggircs_swrs.activity

--- deploy/materialized_view_unit.sql
+++ deploy/materialized_view_unit.sql
@@ -5,40 +5,36 @@ begin;
 
 -- Units from SubActivity
 -- todo: explore any other attributes for units
-create materialized view ggircs_private.unit as (
+create materialized view ggircs_swrs.unit as (
   with x as (
-    select report_id,
+    select ghgr_import_id,
            id              as activity_id,
-           swrs_report_id,
-           swrs_report_history_id,
            process_name    as activity,
            subprocess_name as sub_activity,
-           sub_activity_xml
-    from activity
-    order by report_id desc
+           xml_hunk
+    from ggircs_swrs.activity
+    order by ghgr_import_id desc
   )
   select
          row_number() over (order by x.activity_id asc) as id,
-         report_id,
-         swrs_report_id,
+         ghgr_import_id,
          activity_id,
          activity,
          sub_activity,
-         sub_activity_xml, -- todo: remove in cleanup phase
-         unit_details.*,
-         swrs_report_history_id
+         xml_hunk, -- todo: remove in cleanup phase
+         unit_details.*
 
   from x,
        xmltable(
            '/SubProcess/Units/Unit'
-           passing sub_activity_xml
+           passing xml_hunk
            columns
             unit_name text path './UnitName',
             unit_description text path './UnitDesc',
             unit_xml xml path '.'
          ) as unit_details
-);
+)with no data;
 
-create unique index ggircs_unit_primary_key on ggircs_private.unit (id);
+create unique index ggircs_unit_primary_key on ggircs_swrs.unit (id);
 
 commit;
