253fc79b3c4c830beee21751fcec14fe20ddd476
Dylan Leard <dylan@button.is>
2019-05-02 14:31:43 -0700

    changed dependancy for ggircs_swrs.naics

--- deploy/materialized_view_naics.sql
+++ deploy/materialized_view_naics.sql
@@ -5,35 +5,31 @@ begin;
 
 create materialized view ggircs_swrs.naics as (
   with x as (
-    select _report.source_xml as source_xml,
-           _report.id         as report_id,
-           _report.swrs_report_id,
-           _report.imported_at,
-           _report.swrs_facility_id,
-           _facility.id       as facility_id
-    from ggircs_swrs.report as _report
-           inner join ggircs_swrs.facility as _facility on _report.id = _facility.report_id
-    order by _report.id asc
+    select _ghgr_import.xml_file as source_xml,
+           _ghgr_import.id         as ghgr_id,
+           _ghgr_import.imported_at
+    from ggircs_swrs.ghgr_import as _ghgr_import
+    order by _ghgr_import.id asc
   )
-  select row_number() over (order by report_id asc) as id,
-         facility_id,
-         swrs_facility_id,
+  select row_number() over (order by ghgr_id asc) as id,
+         ghgr_id,
          naics.*,
          row_number() over (
            partition by swrs_facility_id
            order by
-             report_id desc,
+             ghgr_id desc,
              naics_priority desc,
              naics_code desc
            ) as swrs_naics_history_id
   from x,
        xmltable(
-           '/ReportData/RegistrationData/Facility/Identifiers/NAICSCodeList/NAICSCode/NaicsPriority[normalize-space(.)][contains(., "Primary")]|/ReportData/RegistrationData/Facility/Identifiers/NAICSCodeList/NAICSCode/ActivityPercentage[normalize-space(.)][contains(., "100")]'
+           '/ReportData'
            passing x.source_xml
            columns
-             naics_classification varchar(1000) path './../NAICSClassification[normalize-space(.)]',
-             naics_code varchar(1000) path './../Code[normalize-space(.)]',
-             naics_priority varchar(1000) path 'normalize-space(.)'
+             swrs_facility_id numeric(1000,0) path '//descendant-or-self::FacilityId',
+             naics_classification varchar(1000) path '//descendant-or-self::NAICSClassification[normalize-space(.)]',
+             naics_code varchar(1000) path '//descendant-or-self::Code[normalize-space(.)]',
+             naics_priority varchar(1000) path '//descendant-or-self:: NaicsPriority[normalize-space(.)][contains(., "Primary")]|NAICSCode/ActivityPercentage[normalize-space(.)][contains(., "100")]'
          ) as naics
 ) with no data;
 create unique index ggircs_naics_primary_key on ggircs_swrs.naics (id);
@@ -41,7 +37,7 @@ create index ggircs_swrs_naics_history on ggircs_swrs.naics (swrs_naics_history_
 
 comment on materialized view ggircs_swrs.naics is 'The materialized view housing all report data pertaining to naics';
 comment on column ggircs_swrs.naics.id is 'The primary key for the materialized view';
-comment on column ggircs_swrs.naics.facility_id is 'The facility id (foreign key)';
+comment on column ggircs_swrs.naics.ghgr_id is 'The ghgr_import id (foreign key)';
 comment on column ggircs_swrs.naics.swrs_facility_id is 'The reporting facility swrs id';
 
 comment on column ggircs_swrs.naics.naics_classification is 'The naics classification';
