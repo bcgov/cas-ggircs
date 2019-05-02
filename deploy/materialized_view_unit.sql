854539f5df3767be9231fdd8f7065e8d3bf48fbd
Dylan Leard <dylan@button.is>
2019-05-02 16:52:44 -0700

    added comments to ggircs_swrs.unit

--- deploy/materialized_view_unit.sql
+++ deploy/materialized_view_unit.sql
@@ -31,10 +31,21 @@ create materialized view ggircs_swrs.unit as (
            columns
             unit_name text path './UnitName',
             unit_description text path './UnitDesc',
-            unit_xml xml path '.'
+            unit_xml_hunk xml path '.'
          ) as unit_details
 )with no data;
 
 create unique index ggircs_unit_primary_key on ggircs_swrs.unit (id);
+
+comment on materialized view ggircs_swrs.unit is 'The materialized view containing the information on units';
+comment on column ggircs_swrs.unit.id is 'The primary key for the unit';
+comment on column ggircs_swrs.unit.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
+comment on column ggircs_swrs.unit.activity_id is 'A foreign key reference to ggrics_swrs.activity';
+comment on column ggircs_swrs.unit.activity is 'The name of the activity';
+comment on column ggircs_swrs.unit.sub_activity is 'The name of the sub-activity';
+comment on column ggircs_swrs.unit.xml_hunk is 'The raw xml hunk used to extract this unit';
+comment on column ggircs_swrs.unit.unit_name is 'The name of the unit';
+comment on column ggircs_swrs.unit.unit_description is 'The description of the unit';
+comment on column ggircs_swrs.unit.unit_xml_hunk is 'The xml hunk beneath the unit';
 
 commit;
