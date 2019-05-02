c9c5b35d821c4793bff5df7929225533cab5f54f
Dylan Leard <dylan@button.is>
2019-05-02 11:04:02 -0700

    added test fixture that verifies data pulled in from xml

--- test/unit/materialized_view_naics_test.sql
+++ test/unit/materialized_view_naics_test.sql
@@ -3,7 +3,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(18);
+select plan(25);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'naics', 'ggircs_swrs.naics exists');
@@ -35,58 +35,52 @@ select col_type_is('ggircs_swrs', 'naics', 'naics_priority', 'character varying(
 select col_type_is('ggircs_swrs', 'naics', 'swrs_naics_history_id', 'bigint', 'ggircs_swrs.naics.swrs_naics_history_id has type bigint');
 
 --
--- -- insert necessary data into table ghgr_import
--- insert into ggircs_swrs.ghgr_import (xml_file) values ($$
---     <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
---       <RegistrationData>
---         <Facility>
---           <Details>
---             <FacilityName>Stark Tower</FacilityName>
---             <RelationshipType>complicated</RelationshipType>
---             <PortabilityIndicator>P</PortabilityIndicator>
---             <Status>Active</Status>
---           </Details>
---           <Address>
---             <GeographicAddress>
---               <Latitude>23.45125</Latitude>
---               <Longitude>-90.59062</Longitude>
---             </GeographicAddress>
---           </Address>
---         </Facility>
---       </RegistrationData>
---       <ReportDetails>
---         <ReportID>800855555</ReportID>
---         <PrepopReportID>5</PrepopReportID>
---         <ReportType>R7</ReportType>
---         <FacilityId>666</FacilityId>
---         <OrganisationId>1337</OrganisationId>
---         <ReportingPeriodDuration>1999</ReportingPeriodDuration>
---         <ReportStatus>
---           <Status>In Progress</Status>
---           <Version>3</Version>
---           <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
---           <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
---         </ReportStatus>
---       </ReportDetails>
---     </ReportData>
---
--- $$);
---
--- -- refresh necessary views with data
--- refresh materialized view ggircs_swrs.report with data;
--- refresh materialized view ggircs_swrs.facility with data;
---
--- -- test the columnns for matview facility have been properly parsed from xml
--- select results_eq('select id from ggircs_swrs.facility', ARRAY[1::bigint], 'Matview facility parsed column id');
--- select results_eq('select report_id from ggircs_swrs.facility', ARRAY[1::bigint], 'Matview facility parsed column report_id');
--- select results_eq('select swrs_facility_id from ggircs_swrs.facility', ARRAY[666::numeric], 'Matview facility parsed column swrs_facility_id');
--- select results_eq('select facility_name from ggircs_swrs.facility', ARRAY['Stark Tower'::varchar], 'Matview facility parsed column facility_name');
--- select results_eq('select relationship_type from ggircs_swrs.facility', ARRAY['complicated'::varchar], 'Matview facility parsed column relationship_type');
--- select results_eq('select portability_indicator from ggircs_swrs.facility', ARRAY['P'::varchar], 'Matview facility parsed column portability_indicator');
--- select results_eq('select status from ggircs_swrs.facility', ARRAY['Active'::varchar], 'Matview facility parsed column status');
--- select results_eq('select latitude from ggircs_swrs.facility', ARRAY['23.45125'::varchar], 'Matview facility parsed column latitude');
--- select results_eq('select longitude from ggircs_swrs.facility', ARRAY['-90.59062'::varchar], 'Matview facility parsed column longitude');
--- select results_eq('select swrs_facility_history_id from ggircs_swrs.facility', ARRAY[1::bigint], 'Matview facility parsed column swrs_facility_history_id');
+-- insert necessary data into table ghgr_import
+insert into ggircs_swrs.ghgr_import (xml_file) values ($$
+    <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <RegistrationData>
+        <Facility>
+         <Identifiers>
+           <NAICSCodeList>
+              <NAICSCode>
+                <NAICSClassification>Conventional Oil and Gas Extraction</NAICSClassification>
+                <Code>123456</Code>
+                <NaicsPriority>Primary</NaicsPriority>
+              </NAICSCode>
+            </NAICSCodeList>
+          </Identifiers>
+        </Facility>
+      </RegistrationData>
+      <ReportDetails>
+        <ReportID>800855555</ReportID>
+        <PrepopReportID>5</PrepopReportID>
+        <ReportType>R7</ReportType>
+        <FacilityId>666</FacilityId>
+        <OrganisationId>1337</OrganisationId>
+        <ReportingPeriodDuration>1999</ReportingPeriodDuration>
+        <ReportStatus>
+          <Status>In Progress</Status>
+          <Version>3</Version>
+          <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
+          <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
+        </ReportStatus>
+      </ReportDetails>
+    </ReportData>
+$$);
+
+-- refresh necessary views with data
+refresh materialized view ggircs_swrs.report with data;
+refresh materialized view ggircs_swrs.facility with data;
+refresh materialized view ggircs_swrs.naics with data;
+
+-- test that the columns in ggircs_swrs.naics have been properly parsed from xml
+select results_eq('select id from ggircs_swrs.naics', ARRAY[1::bigint], 'ggircs_swrs.naics parsed column id');
+select results_eq('select facility_id from ggircs_swrs.naics', ARRAY[1::bigint], 'ggircs_swrs.naics parsed column facility_id');
+select results_eq('select swrs_facility_id from ggircs_swrs.naics', ARRAY[666::numeric], 'ggircs_swrs.naics parsed column swrs_facility_id');
+select results_eq('select naics_classification from ggircs_swrs.naics', ARRAY['Conventional Oil and Gas Extraction'::varchar], 'ggircs_swrs.naics parsed column naics_classification');
+select results_eq('select naics_code from ggircs_swrs.naics', ARRAY[123456::varchar], 'ggircs_swrs.naics parsed column naics_code');
+select results_eq('select naics_priority from ggircs_swrs.naics', ARRAY['Primary'::varchar], 'ggircs_swrs.naics parsed column naics_priority');
+select results_eq('select swrs_naics_history_id from ggircs_swrs.naics', ARRAY[1::bigint], 'ggircs_swrs.naics parsed column swrs_naics_history_id');
 
 select finish();
 rollback;
