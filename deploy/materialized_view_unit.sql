a681c09000c1cb03274d61a9810298f54677e027
Alec Wenzowski <alec@button.is>
2019-05-03 09:47:19 -0700

    separate concerns by not importing columns we can get by join

--- deploy/materialized_view_unit.sql
+++ deploy/materialized_view_unit.sql
@@ -9,19 +9,14 @@ create materialized view ggircs_swrs.unit as (
   with x as (
     select ghgr_import_id,
            id              as activity_id,
-           process_name    as activity,
-           subprocess_name as sub_activity,
            xml_hunk        as activity_xml_hunk
     from ggircs_swrs.activity
     order by ghgr_import_id desc
   )
   select
-         row_number() over (order by x.activity_id asc) as id,
+         row_number() over (order by ghgr_import_id asc, x.activity_id asc) as id,
          ghgr_import_id,
          activity_id,
-         activity,
-         sub_activity,
-         activity_xml_hunk, -- todo: remove in cleanup phase
          unit_details.*
 
   from x,
@@ -31,21 +26,18 @@ create materialized view ggircs_swrs.unit as (
            columns
             unit_name varchar(1000) path './UnitName',
             unit_description varchar(1000) path './UnitDesc',
-            unit_xml_hunk xml path '.'
+            xml_hunk xml path '.'
          ) as unit_details
 )with no data;
 
 create unique index ggircs_unit_primary_key on ggircs_swrs.unit (id);
 
-comment on materialized view ggircs_swrs.unit is 'The materialized view containing the information on units';
+comment on materialized view ggircs_swrs.unit is 'The materialized view containing the information on swrs machinery units';
 comment on column ggircs_swrs.unit.id is 'The primary key for the unit';
-comment on column ggircs_swrs.unit.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
-comment on column ggircs_swrs.unit.activity_id is 'A foreign key reference to ggrics_swrs.activity';
-comment on column ggircs_swrs.unit.activity is 'The name of the activity';
-comment on column ggircs_swrs.unit.sub_activity is 'The name of the sub-activity';
-comment on column ggircs_swrs.unit.activity_xml_hunk is 'The raw xml hunk used to extract this unit';
-comment on column ggircs_swrs.unit.unit_name is 'The name of the unit';
-comment on column ggircs_swrs.unit.unit_description is 'The description of the unit';
-comment on column ggircs_swrs.unit.unit_xml_hunk is 'The xml hunk beneath the unit';
+comment on column ggircs_swrs.unit.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import.id';
+comment on column ggircs_swrs.unit.activity_id is 'A foreign key reference to ggrics_swrs.activity.id';
+comment on column ggircs_swrs.unit.unit_name is 'The name of the unit of machinery emitting greenhouse gas';
+comment on column ggircs_swrs.unit.unit_description is 'The description of the unit of machinery emitting greenhouse gas';
+comment on column ggircs_swrs.unit.xml_hunk is 'The raw xml hunk representing the unit';
 
 commit;
--- test/unit/materialized_view_unit_test.sql
+++ test/unit/materialized_view_unit_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(27);
+select plan(18);
 
 select has_materialized_view(
     'ggircs_swrs', 'unit',
@@ -20,12 +20,9 @@ select columns_are('ggircs_swrs'::name, 'unit'::name, array[
     'id'::name,
     'ghgr_import_id'::name,
     'activity_id'::name,
-    'activity'::name,
-    'sub_activity'::name,
-    'activity_xml_hunk'::name,
     'unit_name'::name,
     'unit_description'::name,
-    'unit_xml_hunk'::name
+    'xml_hunk'::name
 ]);
 
 --  select has_column(       'ggircs_swrs', 'unit', 'id', 'unit.id column should exist');
@@ -35,25 +32,10 @@ select col_type_is(      'ggircs_swrs', 'unit', 'id', 'bigint', 'unit.id column 
 select col_type_is(      'ggircs_swrs', 'unit', 'ghgr_import_id', 'integer', 'unit.ghgr_import_id column should be type integer');
 select col_hasnt_default('ggircs_swrs', 'unit', 'ghgr_import_id', 'unit.ghgr_import_id column should not have a default value');
 
---  select has_column(       'ggircs_swrs', 'unit', 'process_name', 'unit.activity_id column should exist');
-select col_type_is(      'ggircs_swrs', 'unit', 'activity_id', 'bigint', 'unit.process_name column should be type bigint');
+--  select has_column(       'ggircs_swrs', 'unit', 'activity_id', 'unit.activity_id column should exist');
+select col_type_is(      'ggircs_swrs', 'unit', 'activity_id', 'bigint', 'unit.activity_id column should be type bigint');
 select col_is_null(      'ggircs_swrs', 'unit', 'activity_id', 'unit.activity_id_name column should allow null');
 select col_hasnt_default('ggircs_swrs', 'unit', 'activity_id', 'unit.activity_id column should not  have a default');
-
---  select has_column(       'ggircs_swrs', 'unit', 'activity', 'unit.activity column should exist');
-select col_type_is(      'ggircs_swrs', 'unit', 'activity', 'character varying(1000)', 'unit.activity column should be type varchar');
-select col_is_null(      'ggircs_swrs', 'unit', 'activity', 'unit.activity column should allow null');
-select col_hasnt_default('ggircs_swrs', 'unit', 'activity', 'unit.subprocess_name column should not have a default value');
-
---  select has_column(       'ggircs_swrs', 'unit', 'sub_activity', 'unit.sub_activity column should exist');
-select col_type_is(      'ggircs_swrs', 'unit', 'sub_activity', 'character varying(1000)', 'unit.sub_activity column should be type varchar');
-select col_is_null(      'ggircs_swrs', 'unit', 'sub_activity', 'unit.sub_activity column should allow null');
-select col_hasnt_default('ggircs_swrs', 'unit', 'sub_activity', 'unit.sub_activity column should not have a default value');
-
---  select has_column(       'ggircs_swrs', 'unit', 'activity_xml_hunk', 'unit.xml_hunk column should exist');
-select col_type_is(      'ggircs_swrs', 'unit', 'activity_xml_hunk', 'xml', 'unit.activity_xml_hunk column should be type xml');
-select col_is_null(      'ggircs_swrs', 'unit', 'activity_xml_hunk', 'unit.activity_xml_hunk column should allow null');
-select col_hasnt_default('ggircs_swrs', 'unit', 'activity_xml_hunk', 'unit.activity_xml_hunk column should not have a default value');
 
 --  select has_column(       'ggircs_swrs', 'unit', 'unit_name', 'unit.unit_name column should exist');
 select col_type_is(      'ggircs_swrs', 'unit', 'unit_name', 'character varying(1000)', 'unit.unit_name column should be type varchar');
@@ -66,9 +48,9 @@ select col_is_null(      'ggircs_swrs', 'unit', 'unit_description', 'unit.unit_d
 select col_hasnt_default('ggircs_swrs', 'unit', 'unit_description', 'unit.unit_description column should not have a default value');
 
 --  select has_column(       'ggircs_swrs', 'unit', 'unit_xml_hunk', 'unit.xml_hunk column should exist');
-select col_type_is(      'ggircs_swrs', 'unit', 'unit_xml_hunk', 'xml', 'unit.unit_xml_hunk column should be type xml');
-select col_is_null(      'ggircs_swrs', 'unit', 'unit_xml_hunk', 'unit.unit_xml_hunk column should allow null');
-select col_hasnt_default('ggircs_swrs', 'unit', 'unit_xml_hunk', 'unit.unit_xml_hunk column should not have a default value');
+select col_type_is(      'ggircs_swrs', 'unit', 'xml_hunk', 'xml', 'unit.xml_hunk column should be type xml');
+select col_is_null(      'ggircs_swrs', 'unit', 'xml_hunk', 'unit.xml_hunk column should allow null');
+select col_hasnt_default('ggircs_swrs', 'unit', 'xml_hunk', 'unit.xml_hunk column should not have a default value');
 
 select * from finish();
 rollback;
