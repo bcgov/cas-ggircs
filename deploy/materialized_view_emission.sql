ac43dfaeafc89f11ee63f2afea7ac3fb6b0b55e8
Dylan Leard <dylan@button.is>
2019-05-03 10:47:27 -0700

    added comments to matview ggircs_swrs.emission

--- deploy/materialized_view_emission.sql
+++ deploy/materialized_view_emission.sql
@@ -35,13 +35,27 @@ from x,
            emission_type varchar(1000) path '../@EmissionsType[normalize-space(.)]',
            methodology varchar(1000) path 'Methodology[normalize-space(.)]',
            not_applicable boolean path 'NotApplicable[normalize-space(.)]',
-           quantity numeric path 'Quantity[normalize-space(.)]' default 0,
-           calculated_quantity numeric path 'CalculatedQuantity[normalize-space(.)]' default 0,
+           quantity numeric(1000,0) path 'Quantity[normalize-space(.)]' default 0,
+           calculated_quantity numeric(1000,0) path 'CalculatedQuantity[normalize-space(.)]' default 0,
            groups xml path 'Groups/EmissionGroupTypes/varchar(1000)()[contains(normalize-space(.), "BC_ScheduleB_")]',
-           emission_xml xml path '.'
+           xml_hunk xml path '.'
     ) as emission_details
-);
+) with no data;
 
 create unique index ggircs_emission_primary_key on ggircs_swrs.emission (id);
+
+comment on materialized view ggircs_swrs.emission is 'The materialized view containing the information on emissions';
+comment on column ggircs_swrs.emission.id is 'The primary key for the fuel';
+comment on column ggircs_swrs.emission.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
+comment on column ggircs_swrs.emission.unit_id is 'A foreign key reference to ggircs_swrs.unit';
+comment on column ggircs_swrs.emission.activity_id is 'A foreign key reference to ggrics_swrs.activity';
+comment on column ggircs_swrs.emission.fuel_type is 'The type of the fuel';
+comment on column ggircs_swrs.emission.emission_type is 'The type of the emission';
+comment on column ggircs_swrs.emission.gas_type is 'The type of the gas';
+comment on column ggircs_swrs.emission.methodology is 'The emission methodology';
+comment on column ggircs_swrs.emission.not_applicable is 'Is the emission applicable/NA';
+comment on column ggircs_swrs.emission.quantity is 'The quantity of the emission';
+comment on column ggircs_swrs.emission.calculated_quantity is 'The calcualted quantity of the emission';
+comment on column ggircs_swrs.emission.emission_category is 'The emission groups (categories) as xml';
 
 commit;
