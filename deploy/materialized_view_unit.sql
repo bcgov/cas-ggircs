7963b20b4644dc4d1be86104f6cdee99e3bff8a9
Dylan Leard <dylan@button.is>
2019-05-02 17:07:20 -0700

    updated ggircs_swrs.unit to comply with style guidelines

--- deploy/materialized_view_unit.sql
+++ deploy/materialized_view_unit.sql
@@ -11,7 +11,7 @@ create materialized view ggircs_swrs.unit as (
            id              as activity_id,
            process_name    as activity,
            subprocess_name as sub_activity,
-           xml_hunk
+           xml_hunk        as activity_xml_hunk
     from ggircs_swrs.activity
     order by ghgr_import_id desc
   )
@@ -21,16 +21,16 @@ create materialized view ggircs_swrs.unit as (
          activity_id,
          activity,
          sub_activity,
-         xml_hunk, -- todo: remove in cleanup phase
+         activity_xml_hunk, -- todo: remove in cleanup phase
          unit_details.*
 
   from x,
        xmltable(
            '/SubProcess/Units/Unit'
-           passing xml_hunk
+           passing activity_xml_hunk
            columns
-            unit_name text path './UnitName',
-            unit_description text path './UnitDesc',
+            unit_name varchar(1000) path './UnitName',
+            unit_description varchar(1000) path './UnitDesc',
             unit_xml_hunk xml path '.'
          ) as unit_details
 )with no data;
@@ -43,7 +43,7 @@ comment on column ggircs_swrs.unit.ghgr_import_id is 'A foreign key reference to
 comment on column ggircs_swrs.unit.activity_id is 'A foreign key reference to ggrics_swrs.activity';
 comment on column ggircs_swrs.unit.activity is 'The name of the activity';
 comment on column ggircs_swrs.unit.sub_activity is 'The name of the sub-activity';
-comment on column ggircs_swrs.unit.xml_hunk is 'The raw xml hunk used to extract this unit';
+comment on column ggircs_swrs.unit.activity_xml_hunk is 'The raw xml hunk used to extract this unit';
 comment on column ggircs_swrs.unit.unit_name is 'The name of the unit';
 comment on column ggircs_swrs.unit.unit_description is 'The description of the unit';
 comment on column ggircs_swrs.unit.unit_xml_hunk is 'The xml hunk beneath the unit';
