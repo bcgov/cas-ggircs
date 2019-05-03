9ffeff81281ce5f6ddb178f01ded23f76e237f11
Dylan Leard <dylan@button.is>
2019-05-03 13:38:05 -0700

    add skeleton for ggircs_swrs.address

--- deploy/materialized_view_address.sql
+++ deploy/materialized_view_address.sql
@@ -4,6 +4,27 @@
 
 begin;
 
--- XXX Add DDLs here.
+create materialized view ggircs_swrs.address as (
+  with x as (
+    select _ghgr_import.xml_file as source_xml,
+           _ghgr_import.id         as ghgr_import_id,
+           _ghgr_import.imported_at
+    from ggircs_swrs.ghgr_import as _ghgr_import
+    order by _ghgr_import.id asc
+  )
+  select row_number() over (order by ghgr_import_id asc) as id,
+         x.ghgr_import_id,
+         address_details.*
+  from x,
+       xmltable(
+           '//Address'
+           passing source_xml
+           columns
+             physical_address_municipality varchar(1000) path './PhysicalAddress/Municipality[normalize-space(.)]'
+         ) as address_details
+) with no data;
+
+-- create unique index ggircs_identifier_primary_key on ggircs_swrs.identifier (id);
+-- create index ggircs_swrs_identifier_history on ggircs_swrs.identifier (swrs_identifier_history_id);
 
 commit;
