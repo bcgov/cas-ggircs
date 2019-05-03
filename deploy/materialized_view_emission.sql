107a6a395dc6cee15c4a91731745d05c3d49c19b
Dylan Leard <dylan@button.is>
2019-05-03 10:38:02 -0700

    changed text type to varchar(1000)

--- deploy/materialized_view_emission.sql
+++ deploy/materialized_view_emission.sql
@@ -25,19 +25,19 @@ select
       emission_details.not_applicable,
       emission_details.quantity,
       emission_details.calculated_quantity,
-      CAST(emission_details.groups as text) as emission_category
+      CAST(emission_details.groups as varchar(1000)) as emission_category
 from x,
      xmltable(
        '//Emission'
        passing fuel_xml
        columns
-           gas_type text path 'GasType[normalize-space(.)]',
-           emission_type text path '../@EmissionsType[normalize-space(.)]',
-           methodology text path 'Methodology[normalize-space(.)]',
+           gas_type varchar(1000) path 'GasType[normalize-space(.)]',
+           emission_type varchar(1000) path '../@EmissionsType[normalize-space(.)]',
+           methodology varchar(1000) path 'Methodology[normalize-space(.)]',
            not_applicable boolean path 'NotApplicable[normalize-space(.)]',
            quantity numeric path 'Quantity[normalize-space(.)]' default 0,
            calculated_quantity numeric path 'CalculatedQuantity[normalize-space(.)]' default 0,
-           groups xml path 'Groups/EmissionGroupTypes/text()[contains(normalize-space(.), "BC_ScheduleB_")]',
+           groups xml path 'Groups/EmissionGroupTypes/varchar(1000)()[contains(normalize-space(.), "BC_ScheduleB_")]',
            emission_xml xml path '.'
     ) as emission_details
 );
