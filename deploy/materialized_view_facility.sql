d026a36aade376682a0bee67b52bf43790b90f24
Dylan Leard <dylan@button.is>
2019-05-03 10:12:04 -0700

    updated comments to reflect proper dependencies

--- deploy/materialized_view_facility.sql
+++ deploy/materialized_view_facility.sql
@@ -1,7 +1,5 @@
 -- Deploy ggircs:materialized_view_facility to pg
--- requires: schema_ggircs_swrs
 -- requires: table_ghgr_import
--- requires: materialized_view_report
 
 begin;
 
--- deploy/materialized_view_fuel.sql
+++ deploy/materialized_view_fuel.sql
@@ -1,5 +1,5 @@
 -- Deploy ggircs:materialized_view_fuel to pg
--- requires: table_ghgr_import
+-- requires: materialized_view_unit
 
 BEGIN;
 
@@ -10,10 +10,8 @@ create materialized view ggircs_swrs.fuel as (
     select ghgr_import_id,
            id              as unit_id,
            activity_id,
-           activity,
            unit_name,
-           sub_activity,
-           unit_xml_hunk
+           xml_hunk        as unit_xml_hunk
     from ggircs_swrs.unit
     order by ghgr_import_id desc
   )
@@ -22,8 +20,6 @@ create materialized view ggircs_swrs.fuel as (
          activity_id,
          unit_id,
          unit_name,
-         activity,
-         sub_activity,
          fuel_details.*
 
   from x,
@@ -36,9 +32,9 @@ create materialized view ggircs_swrs.fuel as (
              fuel_units varchar(1000) path './FuelUnits',
              annual_fuel_amount varchar(1000) path './AnnualFuelAmount',
              annual_weighted_avg_hhv varchar(1000) path './AnnualWeightedAverageHighHeatingValue',
-             fuel_xml_hunk xml path '.'
+             xml_hunk xml path '.'
          ) as fuel_details
-);
+) with no data;
 
 create unique index ggircs_fuel_primary_key on ggircs_swrs.fuel (id);
 
@@ -46,15 +42,13 @@ comment on materialized view ggircs_swrs.fuel is 'The materialized view containi
 comment on column ggircs_swrs.fuel.id is 'The primary key for the fuel';
 comment on column ggircs_swrs.fuel.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
 comment on column ggircs_swrs.fuel.activity_id is 'A foreign key reference to ggrics_swrs.activity';
-comment on column ggircs_swrs.fuel.activity is 'The name of the activity';
 comment on column ggircs_swrs.fuel.unit_id is 'A foreign key reference to ggircs_swrs.unit';
 comment on column ggircs_swrs.fuel.unit_name is 'The name of the unit';
-comment on column ggircs_swrs.fuel.sub_activity is 'The name of the sub-activity';
 comment on column ggircs_swrs.fuel.fuel_type is 'The type of the fuel';
 comment on column ggircs_swrs.fuel.fuel_classification is 'The classification of the fuel';
 comment on column ggircs_swrs.fuel.fuel_units is 'The units of the fuel';
 comment on column ggircs_swrs.fuel.annual_fuel_amount is 'The annual amount of the fuel';
 comment on column ggircs_swrs.fuel.annual_weighted_avg_hhv is 'The annual weight avg of the high heating value of the fuel';
-comment on column ggircs_swrs.fuel.fuel_xml_hunk is 'The xml hunk beneath the unit';
+comment on column ggircs_swrs.fuel.xml_hunk is 'The xml hunk beneath the unit';
 
 COMMIT;
