c317362fbe82325460c6e4dd0c4d6926c095eaa4
Alec Wenzowski <alec@button.is>
2019-05-03 10:17:16 -0700

    dry up identifier view

--- deploy/materialized_view_identifier.sql
+++ deploy/materialized_view_identifier.sql
@@ -6,41 +6,29 @@ begin;
 create materialized view ggircs_swrs.identifier as (
   with import_xml as (
     select _ghgr_import.xml_file as source_xml,
-           _ghgr_import.id         as ghgr_id,
+           _ghgr_import.id         as ghgr_import_id,
            _ghgr_import.imported_at
     from ggircs_swrs.ghgr_import as _ghgr_import
     order by _ghgr_import.id asc
   )
-  select row_number() over (order by ghgr_id asc) as id,
-         ghgr_id,
-         report_details.swrs_report_id,
-         report_details.swrs_facility_id,
-         identifier_type,
-         identifier_value,
-         -- imported_at,
+  select row_number() over (order by ghgr_import_id asc) as id,
+         ghgr_import_id,
+         swrs_identifier.*,
          row_number() over (
            partition by swrs_facility_id, identifier_type
            order by
-             ghgr_id desc,
+             ghgr_import_id desc,
              identifier_value desc
            ) as swrs_identifier_history_id
   from import_xml,
        xmltable(
-           '/ReportData'
+           '//Identifier'
            passing import_xml.source_xml
            columns
-           swrs_report_id numeric(1000,0) path '//descendant-or-self::ReportID[normalize-space(.)]' NOT NULL,
-           swrs_facility_id numeric(1000,0) path '//descendant-or-self::FacilityId[normalize-space(.)]' NOT NULL
-        ) as report_details,
-       xmltable(
-           '/ReportData//descendant-or-self::Identifier'
-           passing import_xml.source_xml
-           columns
-             -- _report.swrs_report_id,
-             -- _report.swrs_facility_id,
-             identifier_type varchar(1000) path '//descendant-or-self::IdentifierType[normalize-space(.)]' NOT NULL,
-             identifier_value varchar(1000) path '//descendant-or-self::IdentifierValue[normalize-space(.)]' NOT NULL
-         )
+             swrs_facility_id numeric(1000,0) not null path '//FacilityId[normalize-space(.)]',
+             identifier_type varchar(1000) not null path './IdentifierType[normalize-space(.)]',
+             identifier_value varchar(1000) not null path './IdentifierValue[normalize-space(.)]'
+         ) as swrs_identifier
 ) with no data;
 
 create unique index ggircs_identifier_primary_key on ggircs_swrs.identifier (id);
@@ -48,8 +36,7 @@ create index ggircs_swrs_identifier_history on ggircs_swrs.identifier (swrs_iden
 
 comment on materialized view ggircs_swrs.identifier is 'The materialized view housing information regarding identifiers';
 comment on column ggircs_swrs.identifier.id is 'The primary key for ggircs_swrs.identifier';
-comment on column ggircs_swrs.identifier.swrs_report_id is 'The swrs report id';
-comment on column ggircs_swrs.identifier.ghgr_id is 'The foreign key referencing the ggrics_swrs.report Primary Key';
+comment on column ggircs_swrs.identifier.ghgr_import_id is 'The foreign key referencing ggrics_swrs.ghgr_import.id';
 comment on column ggircs_swrs.identifier.swrs_facility_id is 'The swrs facility id';
 comment on column ggircs_swrs.identifier.identifier_type is 'The type of identifier';
 comment on column ggircs_swrs.identifier.identifier_value is 'The value of the identifier';
--- test/unit/materialized_view_identifier_test.sql
+++ test/unit/materialized_view_identifier_test.sql
@@ -3,15 +3,14 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(25);
+select plan(22);
 
 -- Test matview report exists in schema ggircs_swrs
 select has_materialized_view('ggircs_swrs', 'identifier', 'Materialized view facility exists');
 
 -- Test column names in matview report exist and are correct
 select has_column('ggircs_swrs', 'identifier', 'id', 'ggircs_swrs.identifier has column: id');
-select has_column('ggircs_swrs', 'identifier', 'ghgr_id', 'ggircs_swrs.identifier has column: ghgr_id');
-select has_column('ggircs_swrs', 'identifier', 'swrs_report_id', 'ggircs_swrs.identifier has column: swrs_report_id');
+select has_column('ggircs_swrs', 'identifier', 'ghgr_import_id', 'ggircs_swrs.identifier has column: ghgr_import_id');
 select has_column('ggircs_swrs', 'identifier', 'swrs_facility_id', 'ggircs_swrs.identifier has column: swrs_facility_id');
 select has_column('ggircs_swrs', 'identifier', 'identifier_type', 'ggircs_swrs.identifier has column: identifier_type');
 select has_column('ggircs_swrs', 'identifier', 'identifier_value', 'ggircs_swrs.identifier has column: identifier_value');
@@ -26,8 +25,7 @@ select index_is_unique('ggircs_swrs', 'facility', 'ggircs_facility_primary_key',
 
 -- Test columns in matview report have correct types
 select col_type_is('ggircs_swrs', 'identifier', 'id', 'bigint', 'ggircs_swrs.identifier column id has type bigint');
-select col_type_is('ggircs_swrs', 'identifier', 'ghgr_id', 'integer', 'ggircs_swrs.identifier column ghgr_id has type integer');
-select col_type_is('ggircs_swrs', 'identifier', 'swrs_report_id', 'numeric(1000,0)', 'ggircs_swrs.identifier column swrs_report_id has type numeric');
+select col_type_is('ggircs_swrs', 'identifier', 'ghgr_import_id', 'integer', 'ggircs_swrs.identifier column ghgr_import_id has type integer');
 select col_type_is('ggircs_swrs', 'identifier', 'swrs_facility_id', 'numeric(1000,0)', 'ggircs_swrs.identifier column swrs_facility_id has type numeric');
 select col_type_is('ggircs_swrs', 'identifier', 'identifier_type', 'character varying(1000)', 'ggircs_swrs.identifier column identifier_type has type varchar');
 select col_type_is('ggircs_swrs', 'identifier', 'identifier_value', 'character varying(1000)', 'ggircs_swrs.identifier column identifier_value has type varchar');
@@ -36,48 +34,59 @@ select col_type_is('ggircs_swrs', 'identifier', 'swrs_identifier_history_id', 'b
 
 -- insert necessary data into table ghgr_import
 insert into ggircs_swrs.ghgr_import (xml_file) values ($$
-    <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-      <RegistrationData>
-        <Facility>
-          <Identifiers>
-            <IdentifierList>
-              <Identifier>
-                <IdentifierType>GHGRP Identification Number</IdentifierType>
-                <IdentifierValue>R0B0T2</IdentifierValue>
-              </Identifier>
-            </IdentifierList>
-          </Identifiers>
-        </Facility>
-      </RegistrationData>
-      <ReportDetails>
-        <ReportID>800855555</ReportID>
-        <PrepopReportID>5</PrepopReportID>
-        <ReportType>R7</ReportType>
-        <FacilityId>666</FacilityId>
-        <OrganisationId>1337</OrganisationId>
-        <ReportingPeriodDuration>1999</ReportingPeriodDuration>
-        <ReportStatus>
-          <Status>In Progress</Status>
-          <Version>3</Version>
-          <LastModifiedBy>Donny Donaldson McDonaldface</LastModifiedBy>
-          <LastModifiedDate>2018-09-28T11:55:39.423</LastModifiedDate>
-        </ReportStatus>
-      </ReportDetails>
-    </ReportData>
-
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <RegistrationData>
+    <Facility>
+      <Identifiers>
+        <IdentifierList>
+          <Identifier>
+            <IdentifierType>GHGRP Identification Number</IdentifierType>
+            <IdentifierValue>R0B0T2</IdentifierValue>
+          </Identifier>
+        </IdentifierList>
+      </Identifiers>
+    </Facility>
+  </RegistrationData>
+  <ReportDetails>
+    <FacilityId>666</FacilityId>
+  </ReportDetails>
+</ReportData>
 $$);
 
 -- refresh necessary views with data
 refresh materialized view ggircs_swrs.identifier with data;
 
 -- test the columnns for ggircs_swrs.identifier have been properly parsed from xml
-select results_eq('select id from ggircs_swrs.identifier', ARRAY[1::bigint], 'ggircs_swrs.identifier parsed column id');
-select results_eq('select ghgr_id from ggircs_swrs.identifier', ARRAY[3::integer], 'ggircs_swrs.identifier parsed column ghgr_id');
-select results_eq('select swrs_report_id from ggircs_swrs.identifier', ARRAY[800855555::numeric], 'ggircs_swrs.identifier parsed swrs_report_id');
-select results_eq('select swrs_facility_id from ggircs_swrs.identifier', ARRAY[666::numeric], 'ggircs_swrs.identifier parsed column swrs_facility_id');
-select results_eq('select identifier_type from ggircs_swrs.identifier', ARRAY['GHGRP Identification Number'::varchar], 'ggircs_swrs.identifier parsed column identifier_type');
-select results_eq('select identifier_value from ggircs_swrs.identifier', ARRAY['R0B0T2'::varchar], 'ggircs_swrs.identifier parsed column identifier_value');
-select results_eq('select swrs_identifier_history_id from ggircs_swrs.identifier', ARRAY[1::bigint], 'ggircs_swrs.identifier parsed column swrs_identifier_history_id');
+select results_eq(
+  'select id from ggircs_swrs.identifier',
+  ARRAY[1::bigint],
+  'ggircs_swrs.identifier parsed column id'
+);
+select results_eq(
+  'select ghgr_import_id from ggircs_swrs.identifier',
+  'select id from ggircs_swrs.ghgr_import',
+  'ggircs_swrs.identifier parsed column ghgr_import_id'
+);
+select results_eq(
+  'select swrs_facility_id from ggircs_swrs.identifier',
+  ARRAY[666::numeric],
+  'ggircs_swrs.identifier parsed column swrs_facility_id'
+);
+select results_eq(
+  'select identifier_type from ggircs_swrs.identifier',
+  ARRAY['GHGRP Identification Number'::varchar],
+  'ggircs_swrs.identifier parsed column identifier_type'
+);
+select results_eq(
+  'select identifier_value from ggircs_swrs.identifier',
+  ARRAY['R0B0T2'::varchar],
+  'ggircs_swrs.identifier parsed column identifier_value'
+);
+select results_eq(
+  'select swrs_identifier_history_id from ggircs_swrs.identifier',
+  ARRAY[1::bigint],
+  'ggircs_swrs.identifier parsed column swrs_identifier_history_id'
+);
 
 select finish();
 rollback;
