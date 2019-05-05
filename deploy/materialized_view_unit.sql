9a36bbd3c02113bb3b661e798d59f2db8eef1959
Alec Wenzowski <alec@button.is>
2019-05-04 20:03:07 -0700

    (WIP) make fuel mview standalone

--- deploy/materialized_view_unit.sql
+++ deploy/materialized_view_unit.sql
@@ -7,37 +7,53 @@ begin;
 -- todo: explore any other attributes for units
 create materialized view ggircs_swrs.unit as (
   with x as (
-    select ghgr_import_id,
-           id              as activity_id,
-           xml_hunk        as activity_xml_hunk
-    from ggircs_swrs.activity
+    select ghgr_import.id       as ghgr_import_id,
+           ghgr_import.xml_file as source_xml
+    from ggircs_swrs.ghgr_import
     order by ghgr_import_id desc
   )
-  select
-         row_number() over (order by ghgr_import_id asc, x.activity_id asc) as id,
-         ghgr_import_id,
-         activity_id,
+  select ghgr_import_id,
          unit_details.*
 
   from x,
        xmltable(
-           '/SubProcess/Units/Unit'
-           passing activity_xml_hunk
+           '//Unit'
+           passing source_xml
            columns
-            unit_name varchar(1000) path './UnitName',
-            unit_description varchar(1000) path './UnitDesc',
-            xml_hunk xml path '.'
+             activity_name varchar(1000) not null path 'name(./ancestor::Process/parent::*)',
+             process_idx integer not null path 'string(count(./ancestor::Process/preceding-sibling::Process))',
+             sub_process_idx integer not null path 'string(count(./ancestor::SubProcess/preceding-sibling::SubProcess))',
+             units_idx integer not null path 'string(count(./ancestor::Units/preceding-sibling::Units))',
+             unit_idx integer not null path 'string(count(./preceding-sibling::Unit))',
+             unit_name varchar(1000) path './UnitName|./ancestor::Units/@UnitType',
+             unit_description varchar(1000) path './UnitDesc/text()',
+             -- cogen_unit xml path './COGenUnit',
+             cogen_unit_name varchar(1000) path './COGenUnit/CogenUnitName',
+             cogen_cycle_type varchar(1000) path './COGenUnit/CycleType',
+             cogen_nameplate_capacity varchar(1000) path './COGenUnit/NameplateCapacity',
+             cogen_net_power varchar(1000) path './COGenUnit/NetPower',
+             cogen_steam_heat_acq_quantity varchar(1000) path './COGenUnit/SteamHeatAcquisitionAcquiredQuantity',
+             cogen_steam_heat_acq_name varchar(1000) path './COGenUnit/SteamHeatAcquisitionProviderName',
+             cogen_supplemental_firing_purpose varchar(1000) path './COGenUnit/SupplementalFiringPurpose',
+             cogen_thermal_output_quantity varchar(1000) path './COGenUnit/ThermalOutputQuantity',
+             -- non_cogen_unit xml path './NonCOGenUnit'
+             non_cogen_nameplate_capacity varchar(1000) path './NonCOGenUnit/NameplateCapacity',
+             non_cogen_net_power varchar(1000) path './NonCOGenUnit/NetPower',
+             non_cogen_unit_name varchar(1000) path './NonCOGenUnit/NonCogenUnitName'
+             -- fuels xml path './Fuels/*' -- implemented as ggircs_swrs.fuel
          ) as unit_details
-)with no data;
+) with no data;
 
-create unique index ggircs_unit_primary_key on ggircs_swrs.unit (id);
+create unique index ggircs_unit_primary_key on ggircs_swrs.unit (ghgr_import_id, activity_name, process_idx, sub_process_idx, units_idx, unit_idx);
 
 comment on materialized view ggircs_swrs.unit is 'The materialized view containing the information on swrs machinery units';
-comment on column ggircs_swrs.unit.id is 'The primary key for the unit';
 comment on column ggircs_swrs.unit.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import.id';
-comment on column ggircs_swrs.unit.activity_id is 'A foreign key reference to ggrics_swrs.activity.id';
+comment on column ggircs_swrs.unit.activity_name is 'The name of the activity';
+comment on column ggircs_swrs.unit.process_idx is 'The number of preceding Process siblings before this Process';
+comment on column ggircs_swrs.unit.sub_process_idx is 'The number of preceding SubProcess siblings before this SubProcess';
+comment on column ggircs_swrs.unit.units_idx is 'The number of preceding Units siblings before this Units';
+comment on column ggircs_swrs.unit.unit_idx is 'The number of preceding Unit siblings before this Unit';
 comment on column ggircs_swrs.unit.unit_name is 'The name of the unit of machinery emitting greenhouse gas';
 comment on column ggircs_swrs.unit.unit_description is 'The description of the unit of machinery emitting greenhouse gas';
-comment on column ggircs_swrs.unit.xml_hunk is 'The raw xml hunk representing the unit';
 
 commit;
--- test/unit/materialized_view_activity_test.sql
+++ test/unit/materialized_view_activity_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(24);
+select plan(27);
 
 select has_materialized_view(
     'ggircs_swrs', 'activity',
@@ -17,20 +17,33 @@ select has_index(
 );
 
 select columns_are('ggircs_swrs'::name, 'activity'::name, array[
-    'id'::name,
     'ghgr_import_id'::name,
+    'process_idx'::name,
+    'sub_process_idx'::name,
+    'activity_name'::name,
     'process_name'::name,
     'subprocess_name'::name,
-    'information_requirement'::name,
-    'xml_hunk'::name
+    'information_requirement'::name
 ]);
-
---  select has_column(       'ggircs_swrs', 'activity', 'id', 'activity.id column should exist');
-select col_type_is(      'ggircs_swrs', 'activity', 'id', 'bigint', 'activity.id column should be type bigint');
 
 --  select has_column(       'ggircs_swrs', 'activity', 'ghgr_import_id', 'activity.ghgr_import_id column should exist');
 select col_type_is(      'ggircs_swrs', 'activity', 'ghgr_import_id', 'integer', 'activity.ghgr_import_id column should be type integer');
 select col_hasnt_default('ggircs_swrs', 'activity', 'ghgr_import_id', 'activity.ghgr_import_id column should not have a default value');
+
+--  select has_column(       'ggircs_swrs', 'activity', 'process_idx', 'activity.process_idx column should exist');
+select col_type_is(      'ggircs_swrs', 'activity', 'process_idx', 'integer', 'activity.process_idx column should be type integer');
+--  select col_is_null(      'ggircs_swrs', 'activity', 'process_idx', 'activity.process_idx column should allow null');
+select col_hasnt_default('ggircs_swrs', 'activity', 'process_idx', 'activity.process_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'activity', 'sub_process_idx', 'activity.sub_process_idx column should exist');
+select col_type_is(      'ggircs_swrs', 'activity', 'sub_process_idx', 'integer', 'activity.sub_process_idx column should be type integer');
+--  select col_is_null(      'ggircs_swrs', 'activity', 'sub_process_idx', 'activity.sub_process_idx column should allow null');
+select col_hasnt_default('ggircs_swrs', 'activity', 'sub_process_idx', 'activity.sub_process_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'activity', 'activity_name', 'activity.activity_name column should exist');
+select col_type_is(      'ggircs_swrs', 'activity', 'activity_name', 'character varying(1000)', 'activity.activity_name column should be type text');
+--  select col_is_null(      'ggircs_swrs', 'activity', 'activity_name', 'activity.activity_name column should allow null');
+select col_hasnt_default('ggircs_swrs', 'activity', 'activity_name', 'activity.activity_name column should not have a default');
 
 --  select has_column(       'ggircs_swrs', 'activity', 'process_name', 'activity.process_name column should exist');
 select col_type_is(      'ggircs_swrs', 'activity', 'process_name', 'character varying(1000)', 'activity.process_name column should be type text');
@@ -47,12 +60,6 @@ select col_type_is(      'ggircs_swrs', 'activity', 'information_requirement', '
 select col_is_null(      'ggircs_swrs', 'activity', 'information_requirement', 'activity.information_requirement column should allow null');
 select col_hasnt_default('ggircs_swrs', 'activity', 'information_requirement', 'activity.information_requirement column should not have a default value');
 
---  select has_column(       'ggircs_swrs', 'activity', 'sub_activity_xml', 'activity.sub_activity_xml column should exist');
-select col_type_is(      'ggircs_swrs', 'activity', 'xml_hunk', 'xml', 'activity.xml_hunk column should be type xml');
-select col_is_null(      'ggircs_swrs', 'activity', 'xml_hunk', 'activity.xml_hunk column should allow null');
-select col_hasnt_default('ggircs_swrs', 'activity', 'xml_hunk', 'activity.xml_hunk column should not have a default value');
-
-
 -- Insert data for fixture based testing
 insert into ggircs_swrs.ghgr_import (xml_file) values ($$
 <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -66,17 +73,45 @@ insert into ggircs_swrs.ghgr_import (xml_file) values ($$
 </ReportData>
 $$);
 
-
 -- refresh necessary views with data
 refresh materialized view ggircs_swrs.activity with data;
 
 -- test the columns for matview facility have been properly parsed from xml
-select results_eq('select id from ggircs_swrs.activity', ARRAY[1::bigint], 'ggircs_swrs.activity.id counts from 1');
-select results_eq('select ghgr_import_id from ggircs_swrs.activity', 'select id from ggircs_swrs.ghgr_import', 'ggircs_swrs.activity.ghgr_import_id relates to ggircs_swrs.ghgr_import.id');
-select results_eq('select process_name from ggircs_swrs.activity', ARRAY['GeneralStationaryCombustion'::varchar(1000)], 'ggircs_swrs.activity.process_name is extracted');
-select results_eq('select subprocess_name from ggircs_swrs.activity', ARRAY['(a) general stationary combustion, useful energy'::varchar(1000)], 'ggircs_swrs.activity.subprocess_name is extracted');
-select results_eq('select information_requirement from ggircs_swrs.activity', ARRAY['Required'::varchar(1000)], 'ggircs_swrs.activity.information_requirement is extracted');
-select results_eq('select xml_hunk::text from ggircs_swrs.activity', ARRAY['<SubProcess SubprocessName="(a) general stationary combustion, useful energy" InformationRequirement="Required"/>'::text], 'ggircs_swrs.activity.xml_hunk is extracted');
+select results_eq(
+  'select ghgr_import_id from ggircs_swrs.activity',
+  'select id from ggircs_swrs.ghgr_import',
+  'ggircs_swrs.activity.ghgr_import_id relates to ggircs_swrs.ghgr_import.id'
+);
+select results_eq(
+  'select process_idx from ggircs_swrs.activity',
+  ARRAY[0::integer],
+  'ggircs_swrs.activity.process_idx is extracted'
+);
+select results_eq(
+  'select sub_process_idx from ggircs_swrs.activity',
+  ARRAY[0::integer],
+  'ggircs_swrs.activity.sub_process_idx is extracted'
+);
+select results_eq(
+  'select activity_name from ggircs_swrs.activity',
+  ARRAY['ActivityPages'::varchar(1000)],
+  'ggircs_swrs.activity.activity_name is extracted'
+);
+select results_eq(
+  'select process_name from ggircs_swrs.activity',
+  ARRAY['GeneralStationaryCombustion'::varchar(1000)],
+  'ggircs_swrs.activity.process_name is extracted'
+);
+select results_eq(
+  'select subprocess_name from ggircs_swrs.activity',
+  ARRAY['(a) general stationary combustion, useful energy'::varchar(1000)],
+  'ggircs_swrs.activity.subprocess_name is extracted'
+);
+select results_eq(
+  'select information_requirement from ggircs_swrs.activity',
+  ARRAY['Required'::varchar(1000)],
+  'ggircs_swrs.activity.information_requirement is extracted'
+);
 
 
 select * from finish();
