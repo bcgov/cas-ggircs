0d11c03a12f3f6c6c1a7569b9de1902d1b5e9fad
Hamza Javed <hamza@button.is>
2019-05-06 15:55:05 -0700

    Updated Emissions MV and added new tests

--- deploy/materialized_view_emission.sql
+++ deploy/materialized_view_emission.sql
@@ -6,23 +6,18 @@ begin;
 -- Emissions from Fuel
 create materialized view ggircs_swrs.emission as (
 with x as (
-  select
-       ghgr_import_id,
-       id  as fuel_id,
-       unit_id,
-       activity_id,
-       fuel_type,
-       xml_hunk     as fuel_xml_hunk
-  from ggircs_swrs.fuel
-  order by ghgr_import_id desc
-)
+    select ghgr_import.id as ghgr_import_id,
+           ghgr_import.xml_file as source_xml
+    from ggircs_swrs.ghgr_import
+    order by ghgr_import_id desc
+  )
 select
-      row_number() over (order by ghgr_import_id asc, x.fuel_id asc) as id,
       x.ghgr_import_id,
-      x.fuel_id,
-      x.unit_id,
-      x.activity_id,
-      x.fuel_type,
+      emission_details.process_idx,
+      emission_details.sub_process_idx,
+      emission_details.unit_idx,
+      emission_details.fuel_idx,
+      emission_details.idx,
       emission_details.emission_type,
       emission_details.gas_type,
       emission_details.methodology,
@@ -33,28 +28,33 @@ select
 from x,
      xmltable(
        '//Emission'
-       passing fuel_xml_hunk
+       passing source_xml
        columns
+           process_idx integer path 'string(count(./ancestor::Process/preceding-sibling::Process))' not null,
+           sub_process_idx integer path 'string(count(./ancestor::SubProcess/preceding-sibling::SubProcess))' not null,
+           unit_idx integer path 'string(count(./ancestor::Unit/preceding-sibling::Unit))' not null,
+           fuel_idx integer path 'string(count(./ancestor::Fuel/preceding-sibling::Fuel))' not null,
+           idx integer  path 'string(count(./preceding-sibling::Emission))' not null,
            gas_type varchar(1000) path 'GasType[normalize-space(.)]',
            emission_type varchar(1000) path '../@EmissionsType[normalize-space(.)]',
            methodology varchar(1000) path 'Methodology[normalize-space(.)]',
            not_applicable boolean path 'NotApplicable[normalize-space(.)]',
            quantity numeric(1000,0) path 'Quantity[normalize-space(.)]' default 0,
            calculated_quantity numeric(1000,0) path 'CalculatedQuantity[normalize-space(.)]' default 0,
-           groups xml path 'Groups/EmissionGroupTypes/varchar(1000)()[contains(normalize-space(.), "BC_ScheduleB_")]',
-           xml_hunk xml path '.'
+           groups xml path 'Groups/EmissionGroupTypes/text()[contains(normalize-space(.), "BC_ScheduleB_")]'
     ) as emission_details
 ) with no data;
 
-create unique index ggircs_emission_primary_key on ggircs_swrs.emission (id);
+create unique index ggircs_emission_primary_key on ggircs_swrs.emission (ghgr_import_id, process_idx, sub_process_idx, unit_idx, fuel_idx, idx);
 
 comment on materialized view ggircs_swrs.emission is 'The materialized view containing the information on emissions';
-comment on column ggircs_swrs.emission.id is 'The primary key for the fuel';
 comment on column ggircs_swrs.emission.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
-comment on column ggircs_swrs.emission.fuel_id is 'A foreign key reference to ggircs_swrs.fuel';
-comment on column ggircs_swrs.emission.unit_id is 'A foreign key reference to ggircs_swrs.unit';
-comment on column ggircs_swrs.emission.activity_id is 'A foreign key reference to ggrics_swrs.activity';
-comment on column ggircs_swrs.emission.fuel_type is 'The type of the fuel';
+comment on column ggircs_swrs.emission.ghgr_import_id is 'A foreign key reference to ggircs_swrs.ghgr_import';
+comment on column ggircs_swrs.emission.process_idx is 'The number of preceding Process siblings before this activity';
+comment on column ggircs_swrs.emission.sub_process_idx is 'The number of preceding SubProcess siblings before this activity';
+comment on column ggircs_swrs.emission.unit_idx is 'The number of preceding Unit siblings before this activity';
+comment on column ggircs_swrs.emission.fuel_idx is 'The number of preceding Fuel siblings before this activity';
+comment on column ggircs_swrs.emission.idx is 'The number of preceding Emission siblings before this activity';
 comment on column ggircs_swrs.emission.emission_type is 'The type of the emission';
 comment on column ggircs_swrs.emission.gas_type is 'The type of the gas';
 comment on column ggircs_swrs.emission.methodology is 'The emission methodology';
--- test/unit/materialized_view_emission_test.sql
+++ test/unit/materialized_view_emission_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(12);
+select plan(40);
 
 select has_materialized_view(
     'ggircs_swrs', 'emission',
@@ -17,12 +17,12 @@ select has_index(
 );
 
 select columns_are('ggircs_swrs'::name, 'emission'::name, array[
-    'id'::name,
     'ghgr_import_id'::name,
-    'fuel_id'::name,
-    'activity_id'::name,
-    'unit_id'::name,
-    'fuel_type'::name,
+    'process_idx'::name,
+    'sub_process_idx'::name,
+    'unit_idx'::name,
+    'fuel_idx'::name,
+    'idx'::name,
     'emission_type'::name,
     'gas_type'::name,
     'methodology'::name,
@@ -33,23 +33,183 @@ select columns_are('ggircs_swrs'::name, 'emission'::name, array[
 
 ]);
 
---  select has_column(       'ggircs_swrs', 'emission', 'id', 'emission.id column should exist');
-select col_type_is(      'ggircs_swrs', 'emission', 'id', 'bigint', 'emission.id column should be type bigint');
 
---  select has_column(       'ggircs_swrs', 'emission', 'ghgr_import_id', 'emission.ghgr_import_id column should exist');
 select col_type_is(      'ggircs_swrs', 'emission', 'ghgr_import_id', 'integer', 'emission.ghgr_import_id column should be type integer');
 select col_hasnt_default('ggircs_swrs', 'emission', 'ghgr_import_id', 'emission.ghgr_import_id column should not have a default value');
 
---  select has_column(       'ggircs_swrs', 'emission', 'activity_id', 'emission.activity_id column should exist');
-select col_type_is(      'ggircs_swrs', 'emission', 'activity_id', 'bigint', 'emission.activity_id column should be type bigint');
-select col_is_null(      'ggircs_swrs', 'emission', 'activity_id', 'emission.activity_id column should allow null');
-select col_hasnt_default('ggircs_swrs', 'emission', 'activity_id', 'emission.activity_id column should not  have a default');
+--  select col_is_null(      'ggircs_swrs', 'emission', 'process_idx', 'emission.process_idx column should allow null');
+select col_type_is(      'ggircs_swrs', 'emission', 'process_idx', 'integer', 'emission.process_idx column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'emission', 'process_idx', 'emission.process_idx column should not have a default');
+
+--  select col_is_null(      'ggircs_swrs', 'emission', 'sub_process_idx', 'emission.sub_process_idx column should allow null');
+select col_type_is(      'ggircs_swrs', 'emission', 'sub_process_idx', 'integer', 'emission.sub_process_idx column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'emission', 'sub_process_idx', 'emission.sub_process_idx column should not have a default');
+
+--  select col_is_null(      'ggircs_swrs', 'emission', 'unit_idx', 'emission.unit_idx column should allow null');
+select col_type_is(      'ggircs_swrs', 'emission', 'unit_idx', 'integer', 'emission.unit_idx column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'emission', 'unit_idx', 'emission.unit_idx column should not have a default');
+
+-- select col_is_null(      'ggircs_swrs', 'emission', 'fuel_idx', 'emission.fuel_idx column should allow null');
+select col_type_is(      'ggircs_swrs', 'emission', 'fuel_idx', 'integer', 'emission.fuel_idx column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'emission', 'fuel_idx', 'emission.fuel_idx column should not have a default');
+
+--  select col_is_null(      'ggircs_swrs', 'emission', 'idx', 'emission.idx column should allow null');
+select col_type_is(      'ggircs_swrs', 'emission', 'idx', 'integer', 'emission.idx column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'emission', 'idx', 'emission.idx column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'gas_type', 'character varying(1000)', 'emission.gas_type column should be type text');
+select col_hasnt_default('ggircs_swrs', 'emission', 'gas_type', 'emission.gas_type column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'emission_type', 'character varying(1000)', 'emission.emission_type column should be type text');
+select col_hasnt_default('ggircs_swrs', 'emission', 'emission_type', 'emission.emission_type column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'methodology', 'character varying(1000)', 'emission.methodology column should be type text');
+select col_hasnt_default('ggircs_swrs', 'emission', 'methodology', 'emission.methodology column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'not_applicable', 'boolean', 'emission.not_applicable column should be type boolean');
+select col_hasnt_default('ggircs_swrs', 'emission', 'not_applicable', 'emission.not_applicable column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'emission_category', 'character varying(1000)', 'emission.emission_category column should be type text');
+select col_hasnt_default('ggircs_swrs', 'emission', 'emission_category', 'emission.emission_category column should not have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'quantity', 'numeric(1000,0)', 'emission.quantity column should be type numeric');
+-- select col_has_default(  'ggircs_swrs', 'emission', 'quantity', 'emission.quantity column should have a default');
+
+select col_type_is(      'ggircs_swrs', 'emission', 'calculated_quantity', 'numeric(1000,0)', 'emission.calculated_quantity column should be type numeric');
+-- select col_has_default(  'ggircs_swrs', 'emission', 'calculated_quantity', 'emission.calculated_quantity column should have a default');
+
+-- Insert data for fixture based testing
+insert into ggircs_swrs.ghgr_import (xml_file) values ($$
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ActivityPages>
+      <Process ProcessName="GeneralStationaryCombustion">
+        <SubProcess SubprocessName="(a) general stationary combustion, useful energy" InformationRequirement="Required">
+          <Units>
+            <Unit>
+              <Fuels>
+                <Fuel>
+                  <Emissions EmissionsType="Combustion: Field gas or Process Vent Gas">
+                    <Emission>
+                      <Groups>
+                        <EmissionGroupTypes>BC_FacilityTotal</EmissionGroupTypes>
+                        <EmissionGroupTypes>BC_ScheduleB_GeneralStationaryCombustionEmissions</EmissionGroupTypes>
+                        <EmissionGroupTypes>EC_GeneralStationaryCombustionEmissions</EmissionGroupTypes>
+                      </Groups>
+                      <NotApplicable>false</NotApplicable>
+                      <Quantity>28215</Quantity>
+                      <CalculatedQuantity>28215</CalculatedQuantity>
+                      <GasType>CO2nonbio</GasType>
+                      <Methodology>Methodology 2 (measured HHV/Steam)</Methodology>
+                    </Emission>
+                  </Emissions>
+                </Fuel>
+              </Fuels>
+            </Unit>
+          </Units>
+        </SubProcess>
+      </Process>
+    </ActivityPages>
+  </ActivityData>
+</ReportData>
+
+$$);
 
 
---  select has_column(       'ggircs_swrs', 'emission', 'unit_id', 'emission.unit_id column should exist');
-select col_type_is(      'ggircs_swrs', 'emission', 'unit_id', 'bigint', 'emission.unit_id column should be type bigint');
-select col_is_null(      'ggircs_swrs', 'emission', 'unit_id', 'emission.unit_id column should allow null');
-select col_hasnt_default('ggircs_swrs', 'emission', 'unit_id', 'emission.unit_id column should not  have a default');
+-- refresh necessary views with data
+refresh materialized view ggircs_swrs.emission with data;
+
+-- test the columns for matview facility have been properly parsed from xml
+select results_eq(
+  'select ghgr_import_id from ggircs_swrs.emission',
+  'select id from ggircs_swrs.ghgr_import',
+  'ggircs_swrs.emission.ghgr_import_id relates to ggircs_swrs.ghgr_import.id'
+);
+
+-- Extract process_idx
+select results_eq(
+  'select process_idx from ggircs_swrs.emission',
+   ARRAY[0::integer],
+  'ggircs_swrs.emission.process_idx is extracted'
+);
+
+-- Extract sub_process_idx
+select results_eq(
+  'select sub_process_idx from ggircs_swrs.emission',
+   ARRAY[0::integer],
+  'ggircs_swrs.emission.sub_process_idx is extracted'
+);
+
+-- Extract unit_idx
+select results_eq(
+  'select unit_idx from ggircs_swrs.emission',
+   ARRAY[0::integer],
+  'ggircs_swrs.emission.unit_idx is extracted'
+);
+
+-- Extract fuel_idx
+select results_eq(
+  'select fuel_idx from ggircs_swrs.emission',
+   ARRAY[0::integer],
+  'ggircs_swrs.emission.fuel_idx is extracted'
+);
+
+-- Extract idx
+select results_eq(
+  'select idx from ggircs_swrs.emission',
+   ARRAY[0::integer],
+  'ggircs_swrs.emission.idx is extracted'
+);
+
+-- Extract emission_type
+select results_eq(
+  'select emission_type from ggircs_swrs.emission',
+   ARRAY['Combustion: Field gas or Process Vent Gas'::varchar(1000)],
+  'ggircs_swrs.emission.emission_type is extracted'
+);
+
+-- Extract gas_type
+select results_eq(
+  'select gas_type from ggircs_swrs.emission',
+   ARRAY['CO2nonbio'::varchar(1000)],
+  'ggircs_swrs.emission.gas_type is extracted'
+);
+
+-- Extract methodology
+select results_eq(
+  'select methodology from ggircs_swrs.emission',
+   ARRAY['Methodology 2 (measured HHV/Steam)'::varchar(1000)],
+  'ggircs_swrs.emission.methodology is extracted'
+);
+
+-- Extract not_applicable
+  select results_eq(
+  'select not_applicable from ggircs_swrs.emission',
+   ARRAY['false'::bool],
+  'ggircs_swrs.emission.not_applicable is extracted'
+);
+
+-- Extract quantity
+  select results_eq(
+  'select quantity from ggircs_swrs.emission',
+   ARRAY[28215::numeric(1000,0)],
+  'ggircs_swrs.emission.quantity is extracted'
+);
+
+-- Extract calculated_quantity
+  select results_eq(
+  'select calculated_quantity from ggircs_swrs.emission',
+   ARRAY[28215::numeric(1000,0)],
+  'ggircs_swrs.emission.calculated_quantity is extracted'
+);
+
+-- Extract emission_category
+select results_eq(
+  'select emission_category from ggircs_swrs.emission',
+   ARRAY['BC_ScheduleB_GeneralStationaryCombustionEmissions'::varchar(1000)],
+  'ggircs_swrs.emission.emission_category is extracted'
+);
+
 
 select * from finish();
 rollback;
