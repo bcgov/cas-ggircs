486bc57612808006ab7ebf0bb90d40cb5cb62cb8
Dylan Leard <dylan@button.is>
2019-05-08 15:18:07 -0700

    added comments for columns and column datatype unit tests

--- deploy/materialized_view_contact.sql
+++ deploy/materialized_view_contact.sql
@@ -16,7 +16,7 @@ create materialized view ggircs_swrs.contact as (
            '//Contact[not(ancestor::ConfidentialityRequest)]'
            passing source_xml
            columns
-                facility_id numeric(1000,0) path './ancestor-or-self::Contact/ancestor::ReportData/ReportDetails/FacilityId[normalize-space(.)]',
+                swrs_facility_id numeric(1000,0) path './ancestor-or-self::Contact/ancestor::ReportData/ReportDetails/FacilityId[normalize-space(.)]',
                 path_context varchar(1000) path 'name(./ancestor::VerifyTombstone|./ancestor::RegistrationData)',
                 contact_idx integer path 'string(count(./ancestor-or-self::Contact/preceding-sibling::Contact))' not null,
                 contact_type varchar(1000) path './Details/ContactType[normalize-space(.)]',
@@ -29,9 +29,17 @@ create materialized view ggircs_swrs.contact as (
 ) with no data;
 
 create unique index ggircs_contact_primary_key
-    on ggircs_swrs.contact (ghgr_import_id, path_context, facility_id, contact_idx);
+    on ggircs_swrs.contact (ghgr_import_id, path_context, swrs_facility_id, contact_idx);
 
 comment on materialized view ggircs_swrs.contact is 'The materialized view housing contact information';
+comment on column ggircs_swrs.contact.swrs_facility_id is 'The facility id according to the swrs report';
+comment on column ggircs_swrs.contact.path_context is 'The umbrella context from which the contact was pulled from the xml (VerifyTombstone or RegistrationData';
+comment on column ggircs_swrs.contact.contact_idx is 'The number of preceding Contact siblings before this Contact node';
+comment on column ggircs_swrs.contact.contact_type is 'The type of contact';
+comment on column ggircs_swrs.contact.given_name is 'The given name of the contact';
+comment on column ggircs_swrs.contact.telephone_number is 'The phone number attached to this contact';
+comment on column ggircs_swrs.contact.email_address is 'The email address attached to this contact';
+comment on column ggircs_swrs.contact.position is 'The position of this contact';
 
 commit;
 
--- test/unit/materialized_view_contact_test.sql
+++ test/unit/materialized_view_contact_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(3);
+select plan(29);
 
 select has_materialized_view(
     'ggircs_swrs', 'contact',
@@ -19,7 +19,7 @@ select has_index(
 select columns_are('ggircs_swrs'::name, 'contact'::name, array[
     'ghgr_import_id'::name,
     'path_context'::name,
-    'facility_id'::name,
+    'swrs_facility_id'::name,
     'contact_idx'::name,
     'contact_type'::name,
     'given_name'::name,
@@ -28,7 +28,52 @@ select columns_are('ggircs_swrs'::name, 'contact'::name, array[
     'position'::name
 ]);
 
+select col_type_is(      'ggircs_swrs', 'contact', 'ghgr_import_id', 'integer', 'contact.ghgr_import_id column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'contact', 'ghgr_import_id', 'contact.ghgr_import_id column should not have a default value');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'path_context', 'contact.path_context column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'path_context', 'character varying(1000)', 'contact.path_context column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'path_context', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'path_context', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'swrs_facility_id', 'contact.facility_id column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'swrs_facility_id', 'numeric(1000,0)', 'contact.swrs_facility_id column should be type numeric');
+select col_is_null(      'ggircs_swrs', 'contact', 'swrs_facility_id', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'swrs_facility_id', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'contact_idx', 'contact.contact_idx column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'contact_idx', 'integer', 'contact.contact_idx column should be type integer');
+select col_is_null(      'ggircs_swrs', 'contact', 'contact_idx', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'contact_idx', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'contact_type', 'contact.contact_type column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'contact_type', 'character varying(1000)', 'contact.contact_type column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'contact_type', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'contact_type', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'given_name', 'contact.given_name column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'given_name', 'character varying(1000)', 'contact.given_name column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'given_name', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'given_name', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'telephone_number', 'contact.telephone_number column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'telephone_number', 'character varying(1000)', 'contact.telephone_number column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'telephone_number', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'telephone_number', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'email_address', 'contact.email_address column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'email_address', 'character varying(1000)', 'contact.email_address column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'email_address', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'email_address', 'contact.activity_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'contact', 'position', 'contact.position column should exist');
+select col_type_is(      'ggircs_swrs', 'contact', 'position', 'character varying(1000)', 'contact.position column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'contact', 'position', 'contact.activity_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'contact', 'position', 'contact.activity_idx column should not have a default');
+
 refresh materialized view ggircs_swrs.contact with data;
+
+-- TODO: Add a fixture to test the veracity of what is being pulled in to this view from xml
 
 select * from finish();
 rollback;
