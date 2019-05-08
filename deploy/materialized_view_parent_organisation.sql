80f2f2ab43f0f07270d86e65293715a881183bae
Dylan Leard <dylan@button.is>
2019-05-08 16:13:54 -0700

    added columns to matview parent_organisation

--- deploy/materialized_view_parent_organisation.sql
+++ deploy/materialized_view_parent_organisation.sql
@@ -10,18 +10,27 @@ create materialized view ggircs_swrs.parent_organisation as (
     from ggircs_swrs.ghgr_import as _ghgr_import
     order by ghgr_import_id asc
   )
-  select ghgr_import_id, contact_details.*
+  select ghgr_import_id, parent_organisation_details.*
   from x,
        xmltable(
-           '//parent_organisation'
+           '//ParentOrganisation'
            passing source_xml
            columns
+                swrs_organisation_id numeric(1000,100) path './ancestor-or-self::ParentOrganisation/ancestor::ReportData/ReportDetails/OrganisationId[normalize-space(.)]',
+                path_context varchar(1000) path 'name(./ancestor::VerifyTombstone|./ancestor::RegistrationData)',
+                parent_organisation_idx integer path 'string(count(./ancestor-or-self::ParentOrganisation/preceding-sibling::ParentOrganisation))' not null,
+                percentage_owned numeric(1000,100) path './Details/PercentageOwned',
+                french_trade_name varchar(1000) path './Details/FrenchTradeName',
+                english_trade_name varchar(1000) path './Details/EnglishTradeName',
+                duns varchar(1000) path './Details/DUNSNumber',
+                business_legal_name varchar(1000) path './Details/BusinessLegalName|./LegalName',
+                website varchar(1000) path './Details/WebSite'
 
-         ) as parent_organisation
+         ) as parent_organisation_details
 ) with no data;
 
 create unique index ggircs_parent_organisation_primary_key
-    on ggircs_swrs.parent_organisation (ghgr_import_id);
+    on ggircs_swrs.parent_organisation (ghgr_import_id, swrs_organisation_id, path_context, parent_organisation_idx);
 
 comment on materialized view ggircs_swrs.parent_organisation is 'The materialized view housing parent organisation information';
 comment on column ggircs_swrs.parent_organisation.ghgr_import_id is 'The foreign key reference to ggircs_swrs.ghgr_import';
