23a2cb73e24e04b65147b655c8038fd48b980c2c
Dylan Leard <dylan@button.is>
2019-05-08 16:52:26 -0700

    added column datatype tests for parent_organisation matview

--- test/unit/materialized_view_parent_organisation_test.sql
+++ test/unit/materialized_view_parent_organisation_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(3);
+select plan(32);
 
 select has_materialized_view(
     'ggircs_swrs', 'parent_organisation',
@@ -28,6 +28,55 @@ select columns_are('ggircs_swrs'::name, 'parent_organisation'::name, array[
     'business_legal_name'::name,
     'website'::name
 ]);
+
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'ghgr_import_id', 'integer', 'parent_organisation.ghgr_import_id column should be type integer');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'ghgr_import_id', 'parent_organisation.ghgr_import_id column should not have a default value');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'path_context', 'parent_organisation.path_context column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'path_context', 'character varying(1000)', 'parent_organisation.path_context column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'path_context', 'parent_organisation.path_context column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'path_context', 'parent_organisation.path_context column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'swrs_organisation_id', 'parent_organisation.swrs_organisation_id column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'swrs_organisation_id', 'numeric(1000,0)', 'parent_organisation.swrs_organisation_id column should be type numeric');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'swrs_organisation_id', 'parent_organisation.swrs_organisation_id column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'swrs_organisation_id', 'parent_organisation.swrs_organisation_id column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'parent_organisation_idx', 'parent_organisation.parent_organisation_idx column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'parent_organisation_idx', 'integer', 'parent_organisation.parent_organisation_idx column should be type integer');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'parent_organisation_idx', 'parent_organisation.parent_organisation_idx column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'parent_organisation_idx', 'parent_organisation.parent_organisation_idx column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'percentage_owned', 'parent_organisation.percentage_owned column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'percentage_owned', 'numeric(1000,2)', 'parent_organisation.percentage_owned column should be type numeric');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'percentage_owned', 'parent_organisation.percentage_owned column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'percentage_owned', 'parent_organisation.percentage_owned column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'french_trade_name', 'parent_organisation.french_trade_name column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'french_trade_name', 'character varying(1000)', 'parent_organisation.french_trade_name column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'french_trade_name', 'parent_organisation.french_trade_name column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'french_trade_name', 'parent_organisation.french_trade_name column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'english_trade_name', 'parent_organisation.english_trade_name column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'english_trade_name', 'character varying(1000)', 'parent_organisation.english_trade_name column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'english_trade_name', 'parent_organisation.english_trade_name column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'english_trade_name', 'parent_organisation.english_trade_name column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'duns', 'parent_organisation.duns column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'duns', 'character varying(1000)', 'parent_organisation.duns column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'duns', 'parent_organisation.duns column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'duns', 'parent_organisation.duns column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'business_legal_name', 'parent_organisation.business_legal_name column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'business_legal_name', 'character varying(1000)', 'parent_organisation.business_legal_name column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'business_legal_name', 'parent_organisation.business_legal_name column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'business_legal_name', 'parent_organisation.business_legal_name column should not have a default');
+
+--  select has_column(       'ggircs_swrs', 'parent_organisation', 'website', 'parent_organisation.website column should exist');
+select col_type_is(      'ggircs_swrs', 'parent_organisation', 'website', 'character varying(1000)', 'parent_organisation.website column should be type varchar');
+select col_is_null(      'ggircs_swrs', 'parent_organisation', 'website', 'parent_organisation.website column should not allow null');
+select col_hasnt_default('ggircs_swrs', 'parent_organisation', 'website', 'parent_organisation.website column should not have a default');
+
 
 refresh materialized view ggircs_swrs.parent_organisation with data;
 
