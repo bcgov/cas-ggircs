24bee97a8fe50a8c215d5d7c3237c95e9511920a
Dylan Leard <dylan@button.is>
2019-05-09 11:32:56 -0700

    added missing columns and comments for contact view

--- deploy/materialized_view_contact.sql
+++ deploy/materialized_view_contact.sql
@@ -21,9 +21,14 @@ create materialized view ggircs_swrs.contact as (
                 contact_idx integer path 'string(count(./ancestor-or-self::Contact/preceding-sibling::Contact))' not null,
                 contact_type varchar(1000) path './Details/ContactType[normalize-space(.)]',
                 given_name varchar(1000) path './Details/GivenName[normalize-space(.)]',
+                family_name varchar(1000) path './Details/FamilyName[normalize-space(.)]',
+                initials varchar(1000) path './Details/Initials[normalize-space(.)]',
                 telephone_number varchar(1000) path './Details/TelephoneNumber[normalize-space(.)]',
+                extension_number varchar(1000) path './Details/ExtensionNumber[normalize-space(.)]',
+                fax_number varchar(1000) path './Details/FaxNumber[normalize-space(.)]',
                 email_address varchar(1000) path './Details/EmailAddress[normalize-space(.)]',
-                position varchar(1000) path './Details/Position[normalize-space(.)]'
+                position varchar(1000) path './Details/Position[normalize-space(.)]',
+                language_correspondence varchar(1000) path './Details/LanguageCorrespondence[normalize-space(.)]'
 
          ) as contact_details
 ) with no data;
@@ -38,9 +43,14 @@ comment on column ggircs_swrs.contact.path_context is 'The umbrella context from
 comment on column ggircs_swrs.contact.contact_idx is 'The number of preceding Contact siblings before this Contact node';
 comment on column ggircs_swrs.contact.contact_type is 'The type of contact';
 comment on column ggircs_swrs.contact.given_name is 'The given name of the contact';
+comment on column ggircs_swrs.contact.family_name is 'The family name of the contact';
+comment on column ggircs_swrs.contact.initials is 'The initials of the contact';
 comment on column ggircs_swrs.contact.telephone_number is 'The phone number attached to this contact';
+comment on column ggircs_swrs.contact.extension_number is 'The extension number attached to this contact';
+comment on column ggircs_swrs.contact.fax_number is 'The fax number attached to this contact';
 comment on column ggircs_swrs.contact.email_address is 'The email address attached to this contact';
 comment on column ggircs_swrs.contact.position is 'The position of this contact';
+comment on column ggircs_swrs.contact.language_correspondence is 'The language of correspondence for thsi contact';
 
 commit;
 
