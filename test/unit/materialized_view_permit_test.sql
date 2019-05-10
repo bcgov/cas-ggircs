bca4290bcd0fe02dbf8b5f557927529359141f4a
Dylan Leard <dylan@button.is>
2019-05-10 16:06:24 -0700

    added fk relation test on permit -> facility

--- test/unit/materialized_view_permit_test.sql
+++ test/unit/materialized_view_permit_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(20);
+select plan(21);
 
 select has_materialized_view(
     'ggircs_swrs', 'permit',
@@ -53,7 +53,40 @@ select col_type_is(      'ggircs_swrs', 'permit', 'permit_number', 'character va
 select col_is_null(      'ggircs_swrs', 'permit', 'permit_number', 'permit.permit_number column should not allow null');
 select col_hasnt_default('ggircs_swrs', 'permit', 'permit_number', 'permit.permit_number column should not have a default');
 
+
+insert into ggircs_swrs.ghgr_import (xml_file) values ($$<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <RegistrationData>
+    <Facility>
+      <Identifiers>
+        <Permits>
+          <Permit>
+            <IssuingAgency>BC Ministry of Environment</IssuingAgency>
+            <PermitNumber>PA-14929</PermitNumber>
+          </Permit>
+        </Permits>
+      </Identifiers>
+    </Facility>
+  </RegistrationData>
+  <ReportDetails>
+    <FacilityId>666</FacilityId>
+  </ReportDetails>
+</ReportData>
+$$);
+
+
+refresh materialized view ggircs_swrs.facility with data;
 refresh materialized view ggircs_swrs.permit with data;
+
+select results_eq(
+    'select facility.swrs_facility_id from ggircs_swrs.permit ' ||
+    'join ggircs_swrs.facility ' ||
+    'on ' ||
+    'permit.ghgr_import_id = facility.ghgr_import_id',
+
+    'select swrs_facility_id from ggircs_swrs.facility',
+
+    'Foreign key ghgr_import_id in ggircs_swrs_parent_facility references ggircs_swrs.facility'
+);
 
 -- TODO: Add a fixture to test the veracity of what is being pulled in to this view from xml
 
