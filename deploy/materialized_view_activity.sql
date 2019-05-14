415725cbbd497793a2c3ab7b1ac26cee14361b4c
Dylan Leard <dylan@button.is>
2019-05-14 16:31:33 -0700

    changed subprocess_name to sub_process_name in activity to be in line with other views

--- deploy/materialized_view_activity.sql
+++ deploy/materialized_view_activity.sql
@@ -19,7 +19,7 @@ create materialized view ggircs_swrs.activity as (
              sub_process_idx integer path 'string(count(./preceding-sibling::SubProcess))' not null,
              activity_name varchar(1000) path 'name(./ancestor::Process/parent::*)' not null,
              process_name varchar(1000) path './ancestor::Process/@ProcessName[normalize-space(.)]',
-             subprocess_name varchar(1000) path './@SubprocessName[normalize-space(.)]',
+             sub_process_name varchar(1000) path './@SubprocessName[normalize-space(.)]',
              information_requirement varchar(1000) path './@InformationRequirement[normalize-space(.)]'
          ) as activity_details
 ) with no data;
@@ -32,7 +32,7 @@ comment on column ggircs_swrs.activity.process_idx is 'The number of preceding P
 comment on column ggircs_swrs.activity.sub_process_idx is 'The number of preceding SubProcess siblings before this activity';
 comment on column ggircs_swrs.activity.activity_name is 'The name of the activity (the name of the child class under the Activity)';
 comment on column ggircs_swrs.activity.process_name is 'The name of the process';
-comment on column ggircs_swrs.activity.subprocess_name is 'The name of the sub-process';
+comment on column ggircs_swrs.activity.sub_process_name is 'The name of the sub-process';
 comment on column ggircs_swrs.activity.information_requirement is 'The requirement in reporting regulation to report this activity';
 
 commit;
