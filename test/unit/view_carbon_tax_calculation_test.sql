b72c7732c94656e4316da8cf257562164808d146
Dylan Leard <dylan@button.is>
2019-05-31 15:19:08 -0700

    added unit tests for carbon tax calculation view

---
+++ test/unit/view_carbon_tax_calculation_test.sql
@@ -0,0 +1,125 @@
+set client_encoding = 'utf-8';
+set client_min_messages = warning;
+create extension if not exists pgtap;
+reset client_min_messages;
+
+begin;
+select * from no_plan();
+
+-- View should exist
+select has_view(
+    'ggircs', 'carbon_tax_calculation',
+    'ggircs.carbon_tax_calculation should be a view'
+);
+
+-- Columns are correct
+select columns_are('ggircs'::name, 'carbon_tax_calculation'::name, array[
+    'year'::name,
+    'fuel_type'::name,
+    'calculated_carbon_tax'::name
+]);
+
+-- Column attributes are correct
+select col_type_is('ggircs', 'carbon_tax_calculation', 'year', 'integer', 'pro_rated_carbon_tax_rate.reporting_year column should be type integer');
+select col_hasnt_default('ggircs', 'carbon_tax_calculation', 'year', 'pro_rated_carbon_tax_rate.reporting_year column should not have a default value');
+
+-- XML fixture for testing
+insert into ggircs_swrs.ghgr_import (xml_file) values ($$<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <ReportDetails>
+    <ReportID>1234</ReportID>
+    <ReportType>R1</ReportType>
+    <FacilityId>0000</FacilityId>
+    <FacilityType>EIO</FacilityType>
+    <OrganisationId>0000</OrganisationId>
+    <ReportingPeriodDuration>2025</ReportingPeriodDuration>
+    <ReportStatus>
+      <Status>Submitted</Status>
+      <SubmissionDate>2013-03-27T19:25:55.32</SubmissionDate>
+      <LastModifiedBy>Buddy</LastModifiedBy>
+    </ReportStatus>
+  </ReportDetails>
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ActivityPages>
+      <Process ProcessName="ElectricityGeneration">
+        <SubProcess SubprocessName="Emissions from fuel combustion for electricity generation" InformationRequirement="Required">
+          <Units UnitType="Cogen Units">
+            <Unit>
+              <Fuels>
+                <Fuel>
+                  <FuelType>Wood Waste</FuelType>
+                  <FuelClassification>Biomass in Schedule C</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>bone dry tonnes</FuelUnits>
+                  <AnnualFuelAmount>0</AnnualFuelAmount>
+                  <AnnualSteamGeneration>290471000</AnnualSteamGeneration>
+                </Fuel>
+                <Fuel>
+                  <FuelType>Residual Fuel Oil (#5 &amp; 6)</FuelType>
+                  <FuelClassification>non-biomass</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>kilolitres</FuelUnits>
+                  <AnnualFuelAmount>9441</AnnualFuelAmount>
+                  <AnnualWeightedAverageCarbonContent>0.862</AnnualWeightedAverageCarbonContent>
+                </Fuel>
+              </Fuels>
+            </Unit>
+          </Units>
+        </SubProcess>
+      </Process>
+    </ActivityPages>
+  </ActivityData>
+</ReportData>
+$$), ($$
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <ReportDetails>
+    <ReportID>1235</ReportID>
+    <ReportType>R1</ReportType>
+    <FacilityId>0001</FacilityId>
+    <FacilityType>ABC</FacilityType>
+    <OrganisationId>0000</OrganisationId>
+    <ReportingPeriodDuration>2020</ReportingPeriodDuration>
+    <ReportStatus>
+      <Status>Submitted</Status>
+      <SubmissionDate>2013-03-28T19:25:55.32</SubmissionDate>
+      <LastModifiedBy>Buddy</LastModifiedBy>
+    </ReportStatus>
+  </ReportDetails>
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ActivityPages>
+      <Process ProcessName="ElectricityGeneration">
+        <SubProcess SubprocessName="Emissions from fuel combustion for electricity generation" InformationRequirement="Required">
+          <Units UnitType="Cogen Units">
+            <Unit>
+              <Fuels>
+                <Fuel>
+                  <FuelType>Wood Waste</FuelType>
+                  <FuelClassification>Biomass in Schedule C</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>bone dry tonnes</FuelUnits>
+                  <AnnualFuelAmount>0</AnnualFuelAmount>
+                  <AnnualSteamGeneration>290471000</AnnualSteamGeneration>
+                </Fuel>
+                <Fuel>
+                  <FuelType>Residual Fuel Oil (#5 &amp; 6)</FuelType>
+                  <FuelClassification>non-biomass</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>kilolitres</FuelUnits>
+                  <AnnualFuelAmount>9441</AnnualFuelAmount>
+                  <AnnualWeightedAverageCarbonContent>0.862</AnnualWeightedAverageCarbonContent>
+                </Fuel>
+              </Fuels>
+            </Unit>
+          </Units>
+        </SubProcess>
+      </Process>
+    </ActivityPages>
+  </ActivityData>
+</ReportData>
+$$);
+
+refresh materialized view ggircs_swrs.report with data;
+refresh materialized view ggircs_swrs.fuel with data;
+
+
+select * from finish();
+rollback;
