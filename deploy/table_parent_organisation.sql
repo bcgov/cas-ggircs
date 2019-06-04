15d966176870c6bd796741dfdeaf4e33f5b8134f
Dylan Leard <dylan@button.is>
2019-06-04 11:29:13 -0700

    added table ggircs.parent_organisation to sqitch plan + deploy revert verify files

---
+++ deploy/table_parent_organisation.sql
@@ -0,0 +1,30 @@
+-- Deploy ggircs:table_parent_organisation to pg
+-- requires: schema_ggircs
+
+begin;
+
+create table ggircs.parent_organisation (
+
+    id                        int generated always as identity primary key,
+    ghgr_import_id            integer,
+    path_context              varchar(1000),
+    percentage_owned          numeric,
+    french_trade_name         varchar(1000),
+    english_trade_name        varchar(1000),
+    duns                      varchar(1000),
+    business_legal_name       varchar(1000),
+    website                   varchar(1000)
+);
+
+comment on table ggircs.parent_organisation is 'The table housing parent organisation information';
+comment on column ggircs.parent_organisation.id is 'The primary key';
+comment on column ggircs.parent_organisation.ghgr_import_id is 'The foreign key reference to ggircs.ghgr_import';
+comment on column ggircs.parent_organisation.path_context is 'The path context used to reach the ParentOrganisation node (VerifyTombstone or RegistrationData';
+comment on column ggircs.parent_organisation.percentage_owned is 'The % owned by this parent organisation';
+comment on column ggircs.parent_organisation.french_trade_name is 'The french trade name of this parent organisation';
+comment on column ggircs.parent_organisation.english_trade_name is 'The english trade name of this parent organisation';
+comment on column ggircs.parent_organisation.duns is 'The duns number for this parent organisation';
+comment on column ggircs.parent_organisation.business_legal_name is 'The legal busniess name of this parent organisation';
+comment on column ggircs.parent_organisation.website is 'The website for this parent organisation';
+
+commit;
