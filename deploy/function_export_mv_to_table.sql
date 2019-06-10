cecf086ed8e3202111a42441c6817d1f9ff95acb
Dylan Leard <dylan@button.is>
2019-06-10 16:09:18 -0700

    added 'delete from ggircs.table before each insert to ensure that we are replacing data when exporting, not appending data

--- deploy/function_export_mv_to_table.sql
+++ deploy/function_export_mv_to_table.sql
@@ -33,6 +33,7 @@ $function$
 
     -- Populate tables
     -- REPORT
+    delete from ggircs.report;
     insert into ggircs.report (id, ghgr_import_id, source_xml, imported_at, swrs_report_id, prepop_report_id, report_type, swrs_facility_id, swrs_organisation_id,
                                reporting_period_duration, status, version, submission_date, last_modified_by, last_modified_date, update_comment, swrs_report_history_id)
 
@@ -42,6 +43,7 @@ $function$
     from ggircs_swrs.report;
 
     -- ORGANISATION
+    delete from ggircs.organisation;
     insert into ggircs.organisation (id, ghgr_import_id, report_id, swrs_organisation_id, business_legal_name, english_trade_name, french_trade_name, cra_business_number, duns, website)
 
     select _organisation.id, _organisation.ghgr_import_id, _report.id, _organisation.swrs_organisation_id, _organisation.business_legal_name,
@@ -55,6 +57,7 @@ $function$
       on _organisation.ghgr_import_id = _report.ghgr_import_id;
 
     -- FACILITY
+    delete from ggircs.facility;
     insert into ggircs.facility (id, ghgr_import_id, identifier_id, organisation_id, report_id, swrs_facility_id, facility_name, facility_type, relationship_type, portability_indicator, status, latitude, longitude)
 
     select _facility.id, _facility.ghgr_import_id, null, _organisation.id, _report.id, _facility.swrs_facility_id, _facility.facility_name, _facility.facility_type,
@@ -75,6 +78,7 @@ $function$
     update ggircs.facility as f set identifier_id = x.id from x where f.ghgr_import_id = x.ghgr_import_id;
 
     -- ACTIVITY
+    delete from ggircs.activity;
     insert into ggircs.activity (id, ghgr_import_id, facility_id, report_id,  activity_name, process_name, sub_process_name, information_requirement)
 
     select _activity.id, _activity.ghgr_import_id,  _facility.id, _report.id, _activity.activity_name, _activity.process_name, _activity.sub_process_name, _activity.information_requirement
@@ -90,6 +94,7 @@ $function$
       on _activity.ghgr_import_id = _report.ghgr_import_id;
 
     -- UNIT
+    delete from ggircs.unit;
     insert into ggircs.unit (id, ghgr_import_id, activity_id, activity_name, unit_name, unit_description, cogen_unit_name, cogen_cycle_type, cogen_nameplate_capacity,
                              cogen_net_power, cogen_steam_heat_acq_quantity, cogen_steam_heat_acq_name, cogen_supplemental_firing_purpose, cogen_thermal_output_quantity,
                              non_cogen_nameplate_capacity, non_cogen_net_power, non_cogen_unit_name)
@@ -109,6 +114,7 @@ $function$
       and _unit.activity_name = _activity.activity_name;
 
     -- IDENTIFIER
+    delete from ggircs.identifier;
     insert into ggircs.identifier(id, ghgr_import_id, facility_id, report_id, swrs_facility_id, path_context, identifier_type, identifier_value)
 
     select _identifier.id, _identifier.ghgr_import_id, _facility.id, _report.id, _identifier.swrs_facility_id, _identifier.path_context, _identifier.identifier_type, _identifier.identifier_value
@@ -124,6 +130,7 @@ $function$
       on _identifier.ghgr_import_id = _report.ghgr_import_id;
 
     -- NAICS
+    delete from ggircs.naics;
     insert into ggircs.naics(id, ghgr_import_id, facility_id, registration_data_facility_id, naics_mapping_id, report_id, swrs_facility_id, path_context, naics_classification, naics_code, naics_priority)
 
     select _naics.id, _naics.ghgr_import_id, _facility.id, (select _facility.id where _naics.path_context = 'RegistrationData'), _naics_mapping.id, _report.id, _naics.swrs_facility_id,
@@ -142,6 +149,7 @@ $function$
       on _naics.naics_code = _naics_mapping.naics_code;
 
     -- EMISSION
+    delete from ggircs.emission;
     insert into ggircs.emission (id, ghgr_import_id, activity_id, facility_id, fuel_id, naics_id, organisation_id, report_id, unit_id, activity_name, sub_activity_name,
                                  unit_name, sub_unit_name, fuel_name, emission_type,
                                  gas_type, methodology, not_applicable, quantity, calculated_quantity, emission_category)
@@ -219,6 +227,7 @@ $function$
       and _emission.unit_idx = _unit.unit_idx;
 
     -- FINAL REPORT
+    delete from ggircs.final_report;
     insert into ggircs.final_report (id, ghgr_import_id, swrs_report_id)
 
     select id, ghgr_import_id, swrs_report_id
@@ -226,6 +235,7 @@ $function$
     from ggircs_swrs.final_report;
 
     -- FUEL
+    delete from ggircs.fuel;
     insert into ggircs.fuel(id, ghgr_import_id, report_id, unit_id, fuel_mapping_id,
                             activity_name, sub_activity_name, unit_name, sub_unit_name, fuel_type, fuel_classification, fuel_description,
                             fuel_units, annual_fuel_amount, annual_weighted_avg_carbon_content, annual_weighted_avg_hhv, annual_steam_generation, alternative_methodology_description,
@@ -253,6 +263,7 @@ $function$
       on _fuel.fuel_type = _fuel_mapping.fuel_type;
 
     -- PERMIT
+    delete from ggircs.permit;
     insert into ggircs.permit(id, ghgr_import_id, facility_id, path_context, issuing_agency, issuing_dept_agency_program, permit_number)
 
     select _permit.id, _permit.ghgr_import_id, _facility.id, _permit.path_context, _permit.issuing_agency, _permit.issuing_dept_agency_program, _permit.permit_number
@@ -265,6 +276,7 @@ $function$
     on _permit.ghgr_import_id = _facility.ghgr_import_id;
 
     -- PARENT ORGANISATION
+    delete from ggircs.parent_organisation;
     insert into ggircs.parent_organisation (id, ghgr_import_id, report_id, organisation_id, path_context, percentage_owned, french_trade_name, english_trade_name,
                                             duns, business_legal_name, website)
 
@@ -282,6 +294,7 @@ $function$
       on parent_organisation.ghgr_import_id = _report.ghgr_import_id;
 
     -- CONTACT
+    delete from ggircs.contact;
     insert into ggircs.contact (id, ghgr_import_id, address_id, facility_id, report_id, path_context, contact_type, given_name, family_name, initials, telephone_number, extension_number,
                                 fax_number, email_address, position, language_correspondence)
 
@@ -312,6 +325,7 @@ $function$
       on _contact.ghgr_import_id = _report.ghgr_import_id;
 
     -- ADDRESS
+    delete from ggircs.address;
     insert into ggircs.address (id, ghgr_import_id, facility_id, organisation_id, parent_organisation_id, report_id, swrs_facility_id, swrs_organisation_id, path_context, type, physical_address_municipality, physical_address_unit_number,
                                 physical_address_street_number, physical_address_street_number_suffix, physical_address_street_name, physical_address_street_type,
                                 physical_address_street_direction, physical_address_prov_terr_state, physical_address_postal_code_zip_code, physical_address_country,
@@ -359,6 +373,7 @@ $function$
       on _address.ghgr_import_id = _report.ghgr_import_id;
 
     -- ADDITIONAL DATA
+    delete from ggircs.additional_data;
     insert into ggircs.additional_data (id, ghgr_import_id, activity_id, report_id,
                                    activity_name, grandparent, parent, class, attribute, attr_value, node_value)
 
@@ -379,6 +394,7 @@ $function$
       on _additional_data.ghgr_import_id = _report.ghgr_import_id;
 
     -- MEASURED EMISSION FACTOR
+    delete from ggircs.measured_emission_factor;
     insert into ggircs.measured_emission_factor (id, ghgr_import_id, fuel_id,
                                                  activity_name, sub_activity_name, unit_name, sub_unit_name,
                                                  measured_emission_factor_amount, measured_emission_factor_gas, measured_emission_factor_unit_type)
