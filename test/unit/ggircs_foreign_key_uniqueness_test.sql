9409d6a01c350a0560e570f4da9e2cbd19e526db
Dylan Leard <dylan@button.is>
2019-06-18 15:49:51 -0700

    added to the test suite for unique left join fk relations

--- test/unit/ggircs_foreign_key_uniqueness_test.sql
+++ test/unit/ggircs_foreign_key_uniqueness_test.sql
@@ -75,6 +75,167 @@ select results_eq(
     'All columns in unique index ggircs_facility_primary_key are used in join when creating ggircs.activity -> ggircs.lfo_facility FK relation'
 );
 
+-- Additional Data -> Report
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'report'
+            and
+                indexname = 'ggircs_report_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id
+    $$ select 'ghgr_import_id' $$,
+
+    'All columns in unique index ggircs_report_primary_key are used in join when creating ggircs.additional_activity -> ggircs.report FK relation'
+);
+
+-- Additional Data -> Activity
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'activity'
+            and
+                indexname = 'ggircs_activity_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id, process_idx, sub_process_idx, activity_name
+    $$ select 'ghgr_import_id, process_idx, sub_process_idx, activity_name' $$,
+
+    'All columns in unique index ggircs_activity_primary_key are used in join when creating ggircs.additional_data -> ggircs.activity FK relation'
+);
+
+-- Address -> Report
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'report'
+            and
+                indexname = 'ggircs_report_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id
+    $$ select 'ghgr_import_id' $$,
+
+    'All columns in unique index ggircs_report_primary_key are used in join when creating ggircs.additional_data -> ggircs.report FK relation'
+);
+
+-- Address -> Single Facility
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'facility'
+            and
+                indexname = 'ggircs_facility_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id
+    $$ select 'ghgr_import_id' $$,
+
+    'All columns in unique index ggircs_facility_primary_key are used in join when creating ggircs.address -> ggircs.single_facility FK relation'
+);
+
+-- Address -> LFO Facility
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'facility'
+            and
+                indexname = 'ggircs_facility_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id
+    $$ select 'ghgr_import_id' $$,
+
+    'All columns in unique index ggircs_facility_primary_key are used in join when creating ggircs.address -> ggircs.lfo_facility FK relation'
+);
+
+-- Address -> Organisation
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'organisation'
+            and
+                indexname = 'ggircs_organisation_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id
+    $$ select 'ghgr_import_id' $$,
+
+    'All columns in unique index ggircs_organisation_primary_key are used in join when creating ggircs.address -> ggircs.organisation FK relation'
+);
+
+-- Address -> Parent Organisation
+select results_eq(
+    $$
+        select substring((
+            select
+                indexdef
+            from
+                pg_indexes
+            where
+                schemaname = 'ggircs_swrs'
+            and
+                tablename = 'parent_organisation'
+            and
+                indexname = 'ggircs_parent_organisation_primary_key')
+        from '(?<=\().+?(?=\))')
+    $$,
+
+    -- columns used in join: ghgr_import_id, path_context, parent_organisation_idx
+    $$ select 'ghgr_import_id, path_context, parent_organisation_idx' $$,
+
+    'All columns in unique index ggircs_parent_organisation_primary_key are used in join when creating ggircs.address -> ggircs.parent_organisation FK relation'
+);
+
 /** FACILITY **/
 
 -- Single Facility -> Organisation
