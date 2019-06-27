a6d7959fed66fb410ddd5db510e73345d2591f14
Matthieu Foucault <matthieu@button.is>
2019-06-26 17:16:28 -0700

    save high priority fields to db

--- deploy/ciip_table_contact.sql
+++ deploy/ciip_table_contact.sql
@@ -9,10 +9,11 @@ create table ciip.contact (
 
     id                        serial primary key,
     application_id            integer references ciip.application(id),
+    application_co_id         integer references ciip.application(id),
     address_id                integer references ciip.address(id),
     facility_id               integer references ciip.facility(id),
+    facility_rep_id           integer references ciip.facility(id),
     operator_id               integer references ciip.operator(id),
-    contact_type              varchar(1000),
     given_name                varchar(1000),
     family_name               varchar(1000),
     telephone_number          varchar(1000),
@@ -22,8 +23,10 @@ create table ciip.contact (
 );
 
 create index ciip_contact_application_foreign_key on ciip.contact(application_id);
+create index ciip_contact_application_co_foreign_key on ciip.contact(application_id);
 create index ciip_contact_address_foreign_key on ciip.contact(address_id);
 create index ciip_contact_facility_foreign_key on ciip.contact(facility_id);
+create index ciip_contact_facility_rep_foreign_key on ciip.contact(facility_id);
 create index ciip_contact_operator_foreign_key on ciip.contact(operator_id);
 
 commit;
--- extract/extract_incentives.py
+++ extract/extract_incentives.py
@@ -1,9 +1,11 @@
 import xlrd
 import requests
 import psycopg2
+import psycopg2.extras
 import hashlib
 import datetime
 import dateutil.parser
+import os
 
 # included_sheets = [
 #     'Administrative Info', 'Emissions',
@@ -11,6 +13,25 @@ import dateutil.parser
 #     'Fuel Usage', 'Electricity',
 #     'Statement of Certification'
 # ]
+
+
+def reduce_dicts_array(dicts_array, should_merge_fn):
+    if len(dicts_array) == 1:
+        return dicts_array
+
+    for i in range(len(dicts_array) - 1):
+        if (should_merge_fn(dicts_array[i], dicts_array[i+1])):
+            reduced_array = dicts_array[0:i] if i > 0 else []
+            reduced_array.append({**dicts_array[i], **dicts_array[i+1]})
+            reduced_array += dicts_array[i+2:len(dicts_array)]
+            return reduce_dicts_array(reduced_array, should_merge_fn)
+
+    return dicts_array
+
+def get_sheet_value(sheet, row, col, default = None) :
+    v = sheet.cell_value(row, col)
+    return v if (v != '' and v != 'N/A') else default
+
 
 def extract_book(book_path, cur):
     hasher = hashlib.sha1()
@@ -22,26 +43,27 @@ def extract_book(book_path, cur):
     admin_sheet = incentives_book.sheet_by_name('Administrative Info')
     cert_sheet = incentives_book.sheet_by_name('Statement of Certification')
 
+    # TODO: Find header of signature of CO section and offset from there.
+
     cur.execute(
         ('insert into ciip.application '
         '(source_file_name, source_sha1, imported_at, application_year, signature_date) '
         'values (%s, %s, %s, %s, %s) '
         'returning id'),
         (book_path, hasher.hexdigest(),datetime.datetime.now(),
-        int(cert_sheet.cell_value(7, 10)), dateutil.parser.parse(cert_sheet.cell_value(47, 6)))
+        int(cert_sheet.cell_value(7, 10)), dateutil.parser.parse(get_sheet_value(cert_sheet, 47, 6)))
     )
 
     application_id = cur.fetchone()[0]
 
+    duns = get_sheet_value(admin_sheet, 8, 1)
+    duns = duns if duns is None else str(int(duns))
+
     operator = {
-        'legal_name'      : admin_sheet.cell_value(4, 1),
-        'trade_name'      : admin_sheet.cell_value(6, 1),
-        'duns'            : admin_sheet.cell_value(8, 1),
-        'bc_corp_reg'  : admin_sheet.cell_value(10, 1),
-        'mailing_address' : admin_sheet.cell_value(12, 1),
-        'mailing_city'    : admin_sheet.cell_value(12, 3),
-        'province'        : admin_sheet.cell_value(14, 1),
-        'postal_code'     : admin_sheet.cell_value(14, 3),
+        'legal_name'      : get_sheet_value(admin_sheet, 4, 1),
+        'trade_name'      : get_sheet_value(admin_sheet, 6, 1),
+        'duns'            : duns,
+        'bc_corp_reg'     : get_sheet_value(admin_sheet, 10, 1),
     }
 
     orgbook_req = requests.get(
@@ -54,6 +76,8 @@ def extract_book(book_path, cur):
     else :
         operator['is_bc_cop_reg_valid'] = False
 
+    print(operator)
+
     cur.execute(
         ('insert into ciip.operator '
         '(application_id, business_legal_name, english_trade_name, bc_corp_reg_number, '
@@ -61,24 +85,18 @@ def extract_book(book_path, cur):
         'values (%s, %s, %s, %s, %s, %s, %s, %s) '
         'returning id'),
         (application_id, operator['legal_name'], operator['trade_name'], operator['bc_corp_reg'],
-        operator['is_bc_cop_reg_valid'], operator['orgbook_legal_name'], operator['is_registration_active'],
-        operator['duns'])
+        operator.get('is_bc_cop_reg_valid'), operator.get('orgbook_legal_name'), operator.get('is_registration_active'),
+        operator.get('duns'))
     )
     operator['id'] = cur.fetchone()[0]
 
 
     facility = {
-        'name'            : admin_sheet.cell_value(30, 1),
-        'bcghg_id'        : str(int(admin_sheet.cell_value(8, 3))),
-        'type'            : admin_sheet.cell_value(32, 1),
-        'naics'           : int(admin_sheet.cell_value(32, 3)),
-        'location_address': admin_sheet.cell_value(34, 1),
-        'nearest_city'    : admin_sheet.cell_value(36, 1),
-        'mailing_address' : admin_sheet.cell_value(38, 1),
-        'mailing_city'    : admin_sheet.cell_value(38, 3),
-        'province'        : admin_sheet.cell_value(40, 1),
-        'postal_code'     : admin_sheet.cell_value(40, 3),
-        'description'     : admin_sheet.cell_value(42, 1),
+        'name'            : get_sheet_value(admin_sheet, 30, 1),
+        'bcghg_id'        : str(int(get_sheet_value(admin_sheet, 8, 3))),
+        'type'            : get_sheet_value(admin_sheet, 32, 1),
+        'naics'           : int(get_sheet_value(admin_sheet, 32, 3)),
+        'description'     : get_sheet_value(admin_sheet, 42, 1) if admin_sheet.nrows >= 43 else None,
     }
 
     cur.execute(
@@ -92,47 +110,156 @@ def extract_book(book_path, cur):
     )
     facility['id'] = cur.fetchone()[0]
 
-
-    representative = {
-        'first_name'      : admin_sheet.cell_value(18, 1),
-        'last_name'       : admin_sheet.cell_value(18, 3),
-        'position'        : admin_sheet.cell_value(20, 1),
-        'email'           : admin_sheet.cell_value(20, 3),
-        'phone'           : admin_sheet.cell_value(22, 1),
-        'fax'             : admin_sheet.cell_value(22, 3),
-        'mailing_address' : admin_sheet.cell_value(24, 1),
-        'mailing_city'    : admin_sheet.cell_value(24, 3),
-        'province'        : admin_sheet.cell_value(26, 1),
-        'postal_code'     : admin_sheet.cell_value(26, 3),
+    rep_addr =  {
+        'street_address'  : get_sheet_value(admin_sheet, 24, 1),
+        'municipality'    : get_sheet_value(admin_sheet, 24, 3),
+        'province'        : get_sheet_value(admin_sheet, 26, 1),
+        'postal_code'     : get_sheet_value(admin_sheet, 26, 3),
+        'application_id'  : application_id,
     }
 
-    certifying_official = {
-        'first_name'      : cert_sheet.cell_value(47, 1),
-        'last_name'       : cert_sheet.cell_value(47, 4),
-        'position'        : cert_sheet.cell_value(49, 1),
-        'email'           : cert_sheet.cell_value(49, 4),
-        'phone'           : cert_sheet.cell_value(49, 6),
-        'mailing_address' : cert_sheet.cell_value(51, 1),
-        'mailing_city'    : cert_sheet.cell_value(51, 4),
+    co_addr =  {
+        'street_address'  : get_sheet_value(cert_sheet, 51, 1),
+        'municipality'    : cert_sheet.cell_value(51, 4),
         'province'        : cert_sheet.cell_value(53, 1),
         'postal_code'     : cert_sheet.cell_value(53, 4),
+        'application_id'  : application_id,
     }
+
+    addresses = [
+        # operator address
+        {
+            'street_address'  : admin_sheet.cell_value(12, 1),
+            'municipality'    : admin_sheet.cell_value(12, 3),
+            'province'        : admin_sheet.cell_value(14, 1),
+            'postal_code'     : admin_sheet.cell_value(14, 3),
+            'application_id'  : application_id,
+            'operator_id'     : operator['id'],
+        },
+        # facility location
+        {
+            'street_address'  : admin_sheet.cell_value(34, 1),
+            'municipality'    : admin_sheet.cell_value(36, 1),
+            'province'        : admin_sheet.cell_value(40, 1),
+            'postal_code'     : admin_sheet.cell_value(40, 3),
+            'application_id'  : application_id,
+            'facility_location_id' : facility['id'],
+        },
+        # facility mailing
+        {
+            'street_address'  : get_sheet_value(admin_sheet, 38, 1, get_sheet_value(admin_sheet, 34, 1)),
+            'municipality'    : get_sheet_value(admin_sheet, 38, 3, get_sheet_value(admin_sheet, 36, 1)),
+            'province'        : admin_sheet.cell_value(40, 1),
+            'postal_code'     : admin_sheet.cell_value(40, 3),
+            'application_id'  : application_id,
+            'facility_mailing_id' : facility['id'],
+        },
+        rep_addr,
+        co_addr
+    ]
+
+    def addresses_eq(a, b):
+        return (
+            a['street_address'] == b['street_address'] and
+            a['municipality'] == b['municipality'] and
+            a['province'] == b['province'] and
+            a['postal_code'] == b['postal_code']
+        )
+
+    addresses = reduce_dicts_array(addresses, addresses_eq)
+    print(addresses)
+    addr_ids = []
+
+    for a in addresses:
+        cur.execute('''insert into ciip.address (application_id, facility_location_id, facility_mailing_id,
+        operator_id, street_address, municipality, postal_code, province)
+        values (%s,%s,%s,%s,%s,%s,%s,%s)  returning id ''', (
+            a.get('application_id'), a.get('facility_location_id'), a.get('facility_mailing_id'),
+            a.get('operator_id'), a.get('street_address'), a.get('municipality'), a.get('postal_code'),
+            a.get('province')
+        ))
+        addr_ids.append(cur.fetchone())
+
+    print(addr_ids)
+    rep_addr_idx = next (i for i,a in enumerate(addresses) if addresses_eq(a, rep_addr))
+    co_addr_idx = next (i for i,a in enumerate(addresses) if addresses_eq(a, co_addr))
+    print(co_addr_idx)
+    contacts = [
+        {
+            'first_name'      : cert_sheet.cell_value(47, 1),
+            'last_name'       : cert_sheet.cell_value(47, 4),
+            'position'        : cert_sheet.cell_value(49, 1),
+            'email'           : cert_sheet.cell_value(49, 4),
+            'phone'           : cert_sheet.cell_value(49, 6),
+            'application_id'  : application_id,
+            'facility_id'     : facility['id'],
+            'operator_id'     : operator['id'],
+            'application_co_id' : application_id,
+            'address_id'      : addr_ids[co_addr_idx][0]
+        },
+        {
+            'first_name'      : admin_sheet.cell_value(18, 1),
+            'last_name'       : admin_sheet.cell_value(18, 3),
+            'position'        : admin_sheet.cell_value(20, 1),
+            'email'           : admin_sheet.cell_value(20, 3),
+            'phone'           : admin_sheet.cell_value(22, 1),
+            'application_id'  : application_id,
+            'facility_id'     : facility['id'],
+            'operator_id'     : operator['id'],
+            'fax'             : get_sheet_value(admin_sheet, 22, 3),
+            'facility_rep_id' : facility['id'],
+            'address_id'      : addr_ids[rep_addr_idx][0]
+        }
+    ]
+
+    def should_merge_contacts(a,b):
+        return (
+            a['first_name'] == b['first_name'] and
+            a['last_name'] == b['last_name'] and
+            a['position'] == b['position'] and
+            a['email'] == b['email'] and
+            a['phone'] == b['phone']
+        )
+
+    contacts = reduce_dicts_array(contacts, should_merge_contacts)
+    print(contacts)
+    def contact_dict_to_tuple(c) :
+        return (
+            c['application_id'], c.get('application_co_id'), c['address_id'],
+            c['facility_id'], c.get('facility_rep_id'), c['operator_id'], c['first_name'],
+            c['last_name'], c.get('phone'), c.get('fax'), c['email'],
+            c['position']
+        )
+
+    psycopg2.extras.execute_values(
+        cur,
+        '''insert into ciip.contact (application_id, application_co_id, address_id,
+        facility_id, facility_rep_id, operator_id, given_name, family_name, telephone_number,
+        fax_number, email_address, position)
+        values %s returning id''',
+        list(map(contact_dict_to_tuple, contacts))
+    )
 
     return {
         'operator': operator,
-        'representative': representative,
+        'contacts': contacts,
         'facility': facility,
-        'certifying_official': certifying_official,
+        'addresses': addresses,
     }
 
 conn = psycopg2.connect(dbname='ggircs_dev', host='localhost')
 cur = conn.cursor()
 
+directory = 'data/ciip/SFO Received/Applications'
+
 try:
-    appl = extract_book('data/test_incentive_appl.xls', cur)
+    for filename in os.listdir(directory):
+        print('parsing:' + filename)
+        appl = extract_book(os.path.join(directory,filename), cur)
     conn.commit()
 except Exception as e:
     conn.rollback()
-    print(e)
-cur.close()
-conn.close()
+    raise e
+finally:
+    cur.close()
+    conn.close()
