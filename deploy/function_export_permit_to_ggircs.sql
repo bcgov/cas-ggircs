fe149df48ff773b628ff834752d45b3b97510481
Dylan Leard <dylan@button.is>
2019-07-02 13:50:47 -0700

    added deploy revert verify for export permit function

---
+++ deploy/function_export_permit_to_ggircs.sql
@@ -0,0 +1,28 @@
+-- Deploy ggircs:function_export_permit_to_ggircs to pg
+-- requires: materialized_view_facility
+-- requires: materialized_view_final_report
+-- requires: materialized_view_permit
+
+begin;
+
+create or replace function ggircs_swrs.export_permit_to_ggircs()
+  returns void as
+$function$
+    begin
+
+        delete from ggircs.permit;
+        insert into ggircs.permit(id, ghgr_import_id, facility_id,  path_context, issuing_agency, issuing_dept_agency_program, permit_number)
+
+        select _permit.id, _permit.ghgr_import_id, _facility.id,  _permit.path_context, _permit.issuing_agency, _permit.issuing_dept_agency_program, _permit.permit_number
+
+        from ggircs_swrs.permit as _permit
+
+        inner join ggircs_swrs.final_report as _final_report on _permit.ghgr_import_id = _final_report.ghgr_import_id
+        -- FK Permit -> Facility
+        left join ggircs_swrs.facility as _facility
+            on _permit.ghgr_import_id = _facility.ghgr_import_id;
+
+    end
+$function$ language plpgsql volatile;
+
+commit;
