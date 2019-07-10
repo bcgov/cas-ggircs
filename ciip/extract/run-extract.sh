f629699ac7ac967d88fbfced2383781ed5c28b51
Matthieu Foucault <matthieu@button.is>
2019-07-10 15:40:52 -0700

    add new PVC

--- ciip/extract/run-extract.sh
+++ ciip/extract/run-extract.sh
@@ -3,5 +3,5 @@
 # This script executes the extract python script
 # It is intended to be executed in an openshift container having the variables used below defined
 
-python ./extract.py --dirslist /opt/config/ciip-dirs --db $PGDATABASE --user $PGUSER --password $PGPASSWORD && echo 'extraction done'
+python ./extract.py --dirslist /opt/configs/ciip-dirs --db $PGDATABASE --user $PGUSER --password $PGPASSWORD && echo 'extraction done'
 
