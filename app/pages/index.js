66f1bce2632694b56aa76cd9f4267b6a0543d0f7
Hamza Javed <hamza@button.is>
2019-07-11 19:18:07 -0400

    fix relay next clash

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,71 +1,50 @@
-import React, { Component } from 'react'
-import { graphql } from 'react-relay'
-import withData from '../lib/withData'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React , { Component } from 'react'
+import { QueryRenderer, graphql } from 'react-relay';
 import SignIn from '../components/SignIn'
-import { Container, Row, Col } from 'react-bootstrap';
+import initEnvironment from '../lib/createRelayEnvironment'
+import {Col, Container, Row} from "react-bootstrap";
+import Header from '../components/Header'
 import UserList from "../components/UserList";
+
 
 class Index extends Component {
   static displayName = `Index`
 
-  static async getInitialProps (context) {
-    let { after, before, first, last } = context.query
-
-    if (last === undefined) {
-      first = 2
-    }
-
-    return {
-      relayVariables: {
-        after,
-        before,
-        first: parseInt(first, 10),
-        last: parseInt(last, 10)
-      }
-    }
-  }
-
-
-  render (props) {
+  render(props) {
     return (
-      <React.Fragment>
-        <Header/>
-        <Container>
-          <Row>
-            <Col md={6}>
-              <UserList users={this.props.allUsers}/>
-            </Col>
-            <Col md={6}>
-              <SignIn/>
-            </Col>
-          </Row>
-        </Container>
-
-      </React.Fragment>
-    )
+        <React.Fragment>
+          <Header/>
+          <Container>
+            <Row>
+              <Col>
+                  <h1>About GGIRCS</h1>
+                  <p>We are creating a web-application to provide reliable access to a database for BC's industrial greenhouse gas (GHG) emissions data. This interface will initially work with an existing database and industrial reporting system. Over time, the project may extend to database improvements or other parts of BC’s GHG data system. We are looking for a collaborative team, ready to learn, and passionate about providing solutions that will contribute to reducing industrial GHG emissions in BC and improving reporting to the public about our progress to our GHG targets.</p>
+              </Col>
+            </Row>
+              <br/><br/>
+            <Row>
+                <Col md={6}>
+                    <UserList/>
+                </Col>
+                <Col md={6}>
+                    <SignIn/>
+                </Col>
+            </Row>
+              <br/><br/>
+            <Row>
+              <iframe
+                  src="https://metabase-wksv3k-dev.pathfinder.gov.bc.ca/public/question/2b7ec16a-5793-4788-a2da-0fe08a100e55"
+                  frameBorder="0"
+                  width="100%"
+                  height="600"
+                  allowTransparency
+              ></iframe>
+            </Row>
+          </Container>
+        </React.Fragment>
+    );
   }
 }
 
-export default withData(Index, {
-  query: graphql`
-    query pages_indexQuery {
-      allUsers{
-        pageInfo{
-          hasNextPage
-          hasPreviousPage
-          startCursor
-          endCursor
-        }
-        edges{
-          node{
-            id
-            firstName
-            lastName
-          }
-        }
-      }
-    }
-  `
-})
+
+export default Index;
