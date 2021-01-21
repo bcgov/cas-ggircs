1a27af95bca980847c888870d70e0daf2f753f7b
Dylan Leard <dylan@button.is>
2021-01-21 14:59:53 -0800

    wip: test mocking methods

--- eccc/file_server/tests/test_routes.py
+++ eccc/file_server/tests/test_routes.py
@@ -5,6 +5,7 @@ from app import app
 from app.gcs_bucket_service import GcsBucketService
 
 import unittest.mock as mock
+from unittest.mock import MagicMock
 import datetime
 
 @pytest.fixture
@@ -20,17 +21,26 @@ def test_root_url(client):
   assert b'GIIRCS internal API server' in response.data
 
 
+def list_zip_blobs_mocked(self):
+    a = []
+    blob = MockBlob()
+    return [blob]
+
 def test_files_list_url(client):
-  with mock.patch('app.gcs_bucket_service.GcsBucketService') as MockedService:
-    instance = MockedService.return_value
-    instance.list_zip_files.return_value = []
+  # with mock.patch('app.gcs_bucket_service.GcsBucketService') as MockedService:
+    # instance = MockedService.return_value
+    # instance.list_zip_files.return_value = []
+    GcsBucketService.list_zip_blobs = list_zip_blobs_mocked
+
 
     response = client.get('/files')
+    print(response.data.decode('utf-8'))
 
-    assert b'[]' in response.data
+    assert '"name":"a"' in response.data.decode('utf-8')
+    assert '"created_at":"Thu, 21 Jan 1999 00:00:00 GMT"' in response.data.decode('utf-8')
 
 
 class MockBlob:
   name = "a"
   size = 1
-  time_created = datetime.date.today()
+  time_created = "Thu, 21 Jan 1999 00:00:00 GMT" #datetime.date.today()
