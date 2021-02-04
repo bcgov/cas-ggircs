6cf03062e116cabb6d9b5acf125798bf6b495e49
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-02-04 15:22:53 -0800

    feat: router forwards headers and query params

--- ggircs-app/app/server/routers/ecccFiles.js
+++ ggircs-app/app/server/routers/ecccFiles.js
@@ -7,9 +7,16 @@ const { ECCC_FILE_BROWSER_PORT } = process.env;
 
 ecccFilesRouter.get("*", async (req, res) => {
   const ecccApiRes = await fetch(
-    `${ECCC_FILE_BROWSER_HOST}:${ECCC_FILE_BROWSER_PORT}${req.path}`
+    `${ECCC_FILE_BROWSER_HOST}:${ECCC_FILE_BROWSER_PORT}${req.url}`
   );
-  res.headers = ecccApiRes.headers;
+
+  res.setHeader('Content-Type', ecccApiRes.headers.get('content-type'))
+  res.setHeader('Content-Length', ecccApiRes.headers.get('content-length'))
+
+  const contentDispositionHeader = ecccApiRes.headers.get('content-disposition');
+  if(contentDispositionHeader)
+    res.setHeader('Content-Disposition', contentDispositionHeader)
+
   ecccApiRes.body.pipe(res);
 });
 
