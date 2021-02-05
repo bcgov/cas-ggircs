02faaef0b3c47ae403e2649deacdf6085a24c97e
Dylan Leard <dylan@button.is>
2021-02-05 15:07:27 -0800

    test: add jest unit test

---
+++ ggircs-app/app/tests/unit/pages/login-redirect.test.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { shallow } from "enzyme";
+import LoginRedirect from "pages/login-redirect";
+
+describe("re-direct", () => {
+  it("matches the last accepted Snapshot", () => {
+    const wrapper = shallow(
+      <LoginRedirect
+        query={{
+          session: null,
+        }}
+        router={{
+          query:{
+            sessionIdled: false
+          }
+        }}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
+describe("re-direct session idled", () => {
+  it("matches the last accepted Snapshot", () => {
+    const wrapper = shallow(
+      <LoginRedirect
+        query={{
+          session: null,
+        }}
+        router={{
+          query:{
+            sessionIdled: true
+          }
+        }}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
