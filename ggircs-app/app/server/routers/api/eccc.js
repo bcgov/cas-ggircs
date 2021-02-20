71ad80e1253ccb20dd93a12f6824da1635c5fb0f
Matthieu Foucault <matthieu@button.is>
2021-02-19 16:04:35 -0800

    feat: eccc API route logs file downloads

--- ggircs-app/app/server/routers/api/eccc.js
+++ ggircs-app/app/server/routers/api/eccc.js
@@ -3,8 +3,12 @@ const express = require("express");
 const { keycloak } = require("../sso");
 
 const ecccApiRouter = express.Router();
-const { ECCC_FILE_BROWSER_HOST } = process.env;
-const { ECCC_FILE_BROWSER_PORT } = process.env;
+const {
+  ECCC_FILE_BROWSER_HOST,
+  ECCC_FILE_BROWSER_PORT,
+  HOST,
+  PORT,
+} = process.env;
 
 /**
  * This router forwards all requests to the ECCC file browser service
@@ -21,6 +25,46 @@ ecccApiRouter.get(
     const ecccApiRes = await fetch(
       `${ECCC_FILE_BROWSER_HOST}:${ECCC_FILE_BROWSER_PORT}${req.url}`
     );
+
+    if (ecccApiRes.ok && req.path.endsWith("/download")) {
+      const splitPath = req.path.split("/");
+      const ecccZipFileName = splitPath[splitPath.length - 2];
+      const mutationBody = {
+        id: "createEcccFileDownloadLogMutation",
+        query: `mutation createEcccFileDownloadLogMutation(
+            $input: CreateEcccFileDownloadLogInput!
+          ) {
+            createEcccFileDownloadLog(input: $input) {
+              ecccFileDownloadLog {
+                id
+              }
+            }
+          }`,
+        variables: {
+          input: {
+            ecccFileDownloadLog: {
+              ecccZipFileName,
+              ecccIndividualFilePath: req.query.filename,
+            },
+          },
+        },
+      };
+      // log the download in the database.
+      try {
+        await fetch(`${HOST}:${PORT}/graphql`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: req.get("Cookie"), // forward the cookie so that the graphql request is authenticated
+          },
+          body: JSON.stringify(mutationBody),
+        });
+      } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+        return;
+      }
+    }
 
     res.setHeader("Content-Type", ecccApiRes.headers.get("content-type"));
     res.setHeader("Content-Length", ecccApiRes.headers.get("content-length"));
