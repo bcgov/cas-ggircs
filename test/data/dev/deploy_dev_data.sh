66c14643c9b12ed1a9c69a94eec3160e584f851e
Dylan Leard <dylan@button.is>
2021-05-06 11:40:55 -0700

    refactor: clean up shell script

--- test/data/dev/deploy_dev_data.sh
+++ test/data/dev/deploy_dev_data.sh
@@ -6,6 +6,23 @@ user=${PGUSER:-$(whoami)}
 host=${PGHOST:-localhost}
 port=${PGPORT:-5432}
 
+# =============================================================================
+# Usage:
+# -----------------------------------------------------------------------------
+usage() {
+    cat << EOF
+$0 [-d] [-p] [-s] [-h]
+
+Truncates all existing data in the swrs schema & deploys dev data for every year 2018-2025.
+Requires one parameter "--oc-project".
+
+Example:
+
+deploy_dev_data.sh --oc-project=<NAMESPACE>
+
+EOF
+}
+
 __dirname="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd "$__dirname" > /dev/null
 
@@ -18,8 +35,14 @@ deployDevData() {
   return 0;
 }
 
+if [ "$#" != 1 ]; then
+    echo "Passed $# parameters. Expected 1."
+    usage
+    exit 1
+fi
+
 if [[ $1 =~ -dev$ ]]; then
-  echo 'deploying dev data...';
+  echo 'Truncating all tables in the swrs schema & deploying dev data...';
   deployDevData
 fi
 
