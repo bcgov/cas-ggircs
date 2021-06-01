294c640d15d30caf8436f1087c7b70d4a9364ff3
Dylan Leard <dylan@button.is>
2021-06-01 12:33:05 -0700

    chore: fix pre-commit issues on --all-files

--- ggircs-app/app/lib/relay-environment/debounce-mutations.ts
+++ ggircs-app/app/lib/relay-environment/debounce-mutations.ts
@@ -11,50 +11,54 @@ let debouncedMutation: {
   timeoutId: number;
 } = null;
 
-const debounceMutationMiddleware = (timeout = 250): Middleware => (
-  next
-) => async (req) => {
-  if (!(req instanceof RelayNetworkLayerRequest) || !req.isMutation()) {
-    if (debouncedMutation) {
-      debouncedMutation.debouncedFn();
-    }
-
-    return next(req);
-  }
-
-  const { debounceKey } = (req.cacheConfig as CacheConfigWithDebounce) || {};
-  if (!debounceKey) {
-    if (debouncedMutation) {
-      debouncedMutation.debouncedFn();
-    }
-
-    return next(req);
-  }
-
-  if (debouncedMutation && debouncedMutation.debounceKey !== debounceKey) {
-    window.clearTimeout(debouncedMutation.timeoutId);
-    debouncedMutation.debouncedFn();
-  }
-
-  const debounced = async () =>
-    new Promise<RelayNetworkLayerResponse>((resolve) => {
-      if (debouncedMutation && debouncedMutation.debounceKey === debounceKey) {
-        window.clearTimeout(debouncedMutation.timeoutId);
+const debounceMutationMiddleware =
+  (timeout = 250): Middleware =>
+  (next) =>
+  async (req) => {
+    if (!(req instanceof RelayNetworkLayerRequest) || !req.isMutation()) {
+      if (debouncedMutation) {
+        debouncedMutation.debouncedFn();
       }
 
-      const debouncedFn = () => {
-        window.clearTimeout(debouncedMutation.timeoutId);
-        resolve(next(req));
-      };
+      return next(req);
+    }
 
-      debouncedMutation = {
-        debounceKey,
-        debouncedFn,
-        timeoutId: window.setTimeout(debouncedFn, timeout),
-      };
-    });
+    const { debounceKey } = (req.cacheConfig as CacheConfigWithDebounce) || {};
+    if (!debounceKey) {
+      if (debouncedMutation) {
+        debouncedMutation.debouncedFn();
+      }
 
-  return debounced();
-};
+      return next(req);
+    }
+
+    if (debouncedMutation && debouncedMutation.debounceKey !== debounceKey) {
+      window.clearTimeout(debouncedMutation.timeoutId);
+      debouncedMutation.debouncedFn();
+    }
+
+    const debounced = async () =>
+      new Promise<RelayNetworkLayerResponse>((resolve) => {
+        if (
+          debouncedMutation &&
+          debouncedMutation.debounceKey === debounceKey
+        ) {
+          window.clearTimeout(debouncedMutation.timeoutId);
+        }
+
+        const debouncedFn = () => {
+          window.clearTimeout(debouncedMutation.timeoutId);
+          resolve(next(req));
+        };
+
+        debouncedMutation = {
+          debounceKey,
+          debouncedFn,
+          timeoutId: window.setTimeout(debouncedFn, timeout),
+        };
+      });
+
+    return debounced();
+  };
 
 export default debounceMutationMiddleware;
