1350b1ae4856120a2aa3768320c7c26887f1ac7d
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2021-07-07 16:53:14 -0700

    refactor: swrs access component

--- ggircs-app/app/components/Dashboard/SwrsDataAccessCard.tsx
+++ ggircs-app/app/components/Dashboard/SwrsDataAccessCard.tsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import Card from "@button-inc/bcgov-theme/Card";
+import Button from "@button-inc/bcgov-theme/Button";
 import Link from "next/link";
 
 const SwrsDataAccessCard: React.FunctionComponent = () => (
-  <Card>
-    <Card.Body>
-      <Card.Title>SWRS</Card.Title>
-      <Card.Text>
-        Access reports and attachments from the Single Window Reporting System
-      </Card.Text>
-    </Card.Body>
-    <ListGroup variant="flush">
-      <ListGroup.Item>
-        <Link href="/swrs-browser" passHref>
-          <Card.Link href="#">Browse SWRS files</Card.Link>
-        </Link>
-      </ListGroup.Item>
-    </ListGroup>
+  <Card title="SWRS">
+    Access reports and attachments from the Single Window Reporting System
+    <br />
+    <br />
+    <Link href="/swrs-browser" passHref>
+      <Button>Browse SWRS files</Button>
+    </Link>
   </Card>
 );
 
