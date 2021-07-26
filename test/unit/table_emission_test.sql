e329508b0ed2bd9cfa6a47c0d62b5335e59c7752
Dylan Leard <dylan@button.is>
2021-07-26 15:18:57 -0700

    test: update emission table test to include R3 data

--- test/unit/table_emission_test.sql
+++ test/unit/table_emission_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select * from no_plan();
+select plan(12);
 
 insert into swrs_extract.eccc_xml_file (xml_file) values ($$
 <ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
@@ -386,6 +386,140 @@ $$), ($$
     </ActivityPages>
   </ActivityData>
 </ReportData>
+$$) , ($$
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <RegistrationData>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <FrenchTradeName/>
+        <CRABusinessNumber>12345</CRABusinessNumber>
+        <DUNSNumber>0</DUNSNumber>
+        <WebSite>www.nhl.com</WebSite>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>fname</FacilityName>
+        <RelationshipType>Owned and Operated</RelationshipType>
+        <PortabilityIndicator>N</PortabilityIndicator>
+        <Status>Active</Status>
+      </Details>
+      <Identifiers>
+        <IdentifierList>
+          <Identifier>
+            <IdentifierType>BCGHGID</IdentifierType>
+            <IdentifierValue>RD_123456</IdentifierValue>
+          </Identifier>
+          <Identifier>
+            <IdentifierType>GHGRP Identification Number</IdentifierType>
+            <IdentifierValue>654321</IdentifierValue>
+          </Identifier>
+          <Identifier>
+            <IdentifierType>National Emission Reduction Masterplan</IdentifierType>
+            <IdentifierValue>1234</IdentifierValue>
+          </Identifier>
+          <Identifier>
+            <IdentifierType>National Pollutant Release Inventory Identifier</IdentifierType>
+            <IdentifierValue>0000</IdentifierValue>
+          </Identifier>
+        </IdentifierList>
+        <NAICSCodeList>
+          <NAICSCode>
+            <NAICSClassification>Chemical Pulp Mills </NAICSClassification>
+            <Code>321111</Code>
+            <NaicsPriority>Primary</NaicsPriority>
+          </NAICSCode>
+        </NAICSCodeList>
+        <Permits>
+          <Permit>
+            <IssuingAgency>British Columbia</IssuingAgency>
+            <PermitNumber>0000</PermitNumber>
+          </Permit>
+        </Permits>
+      </Identifiers>
+    </Facility>
+  </RegistrationData>
+  <ReportDetails>
+    <ReportID>1236</ReportID>
+    <ReportType>R3</ReportType>
+    <FacilityId>0001</FacilityId>
+    <FacilityType>LF_a</FacilityType>
+    <OrganisationId>0000</OrganisationId>
+    <ReportingPeriodDuration>2021</ReportingPeriodDuration>
+    <ReportStatus>
+      <Status>Submitted</Status>
+      <SubmissionDate>2013-03-28T19:25:55.32</SubmissionDate>
+      <LastModifiedBy>Buddy</LastModifiedBy>
+    </ReportStatus>
+  </ReportDetails>
+  <OperationalWorkerReport/>
+  <VerifyTombstone>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <CRABusinessNumber>123456778</CRABusinessNumber>
+        <DUNSNumber>00-000-0000</DUNSNumber>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>Bart Simpson</FacilityName>
+        <Identifiers>
+          <IdentifierList>
+            <Identifier>
+              <IdentifierType>NPRI</IdentifierType>
+              <IdentifierValue>12345</IdentifierValue>
+            </Identifier>
+            <Identifier>
+              <IdentifierType>BCGHGID</IdentifierType>
+              <IdentifierValue></IdentifierValue>
+            </Identifier>
+          </IdentifierList>
+          <NAICSCodeList>
+            <NAICSCode>
+              <Code>321111</Code>
+            </NAICSCode>
+          </NAICSCodeList>
+        </Identifiers>
+      </Details>
+    </Facility>
+    <ParentOrganisations/>
+  </VerifyTombstone>
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <TotalAttributableEmissions>
+      <TotalGroups TotalGroupType="TotalAttribEmissions">
+        <Totals>
+          <Emissions EmissionsGasType="AttributableEmissions">
+            <TotalRow>
+              <Quantity>7437.645</Quantity>
+              <CalculatedQuantity>7437.645</CalculatedQuantity>
+              <CasNumber>124-38-9</CasNumber>
+              <GasName>CO2 nonbio</GasName>
+            </TotalRow>
+            <TotalRow>
+              <Quantity>1.89</Quantity>
+              <CalculatedQuantity>47.25</CalculatedQuantity>
+              <CasNumber>74-82-8</CasNumber>
+              <GasName>CH4</GasName>
+            </TotalRow>
+            <TotalRow>
+              <Quantity>0.19</Quantity>
+              <CalculatedQuantity>56.62</CalculatedQuantity>
+              <CasNumber>10024-97-2</CasNumber>
+              <GasName>N2O</GasName>
+            </TotalRow>
+            <GrandTotal>
+              <Total>7541.515</Total>
+            </GrandTotal>
+          </Emissions>
+        </Totals>
+      </TotalGroups>
+    </TotalAttributableEmissions>
+  </ActivityData>
+</ReportData>
 $$);
 
 -- Run table export function without clearing the materialized views (for data equality tests below)
@@ -485,47 +619,87 @@ select set_eq(
     'Foreign key unit_id in swrs.emission references swrs.unit.id'
 );
 
--- Data in swrs_transform.emission === data in swrs.emission
+-- Data in swrs.emission contains the swrs_transform.emission data
 select set_eq(
-              $$
-              select
-                emission.eccc_xml_file_id,
-                activity_name,
-                sub_activity_name,
-                unit_name,
-                sub_unit_name,
-                fuel_name,
-                emission_type,
-                gas_type,
-                methodology,
-                not_applicable,
-                quantity,
-                calculated_quantity,
-                emission_category
-              from swrs_transform.emission
-              order by
-                eccc_xml_file_id asc
-              $$,
+  $$
+  select
+    emission.eccc_xml_file_id,
+    activity_name,
+    sub_activity_name,
+    unit_name,
+    sub_unit_name,
+    fuel_name,
+    emission_type,
+    gas_type,
+    methodology,
+    not_applicable,
+    quantity,
+    calculated_quantity,
+    emission_category
+  from swrs_transform.emission
+  join swrs_transform.report
+    on emission.eccc_xml_file_id = report.eccc_xml_file_id
+    and report.report_type != 'R3'
+  order by
+    eccc_xml_file_id asc
+  $$,
 
-              $$
-              select
-                  eccc_xml_file_id,
-                  activity_name,
-                  sub_activity_name,
-                  unit_name,
-                  sub_unit_name,
-                  fuel_name,
-                  emission_type,
-                  gas_type,
-                  methodology,
-                  not_applicable,
-                  quantity,
-                  calculated_quantity,
-                  emission_category
-                from swrs.emission
-              $$,
+  $$
+  select
+      emission.eccc_xml_file_id,
+      activity_name,
+      sub_activity_name,
+      unit_name,
+      sub_unit_name,
+      fuel_name,
+      emission_type,
+      gas_type,
+      methodology,
+      not_applicable,
+      quantity,
+      calculated_quantity,
+      emission_category
+    from swrs.emission
+    join swrs.report
+      on emission.eccc_xml_file_id = report.eccc_xml_file_id
+      and report.report_type != 'R3'
+  $$,
+  'Data from swrs_transform.emission is contained in swrs.emission'
+);
 
-              'data in swrs_transform.emission === swrs.emission');
+-- Data in swrs.emission contains the swrs_transform.emission data
+select set_eq(
+  $$
+  select
+    emission.eccc_xml_file_id,
+    activity_name,
+    gas_type,
+    quantity,
+    calculated_quantity,
+    cas_number
+  from swrs_transform.r3_emission as emission
+  join swrs_transform.report
+    on emission.eccc_xml_file_id = report.eccc_xml_file_id
+    and report.report_type = 'R3'
+  order by
+    eccc_xml_file_id asc
+  $$,
+
+  $$
+  select
+      emission.eccc_xml_file_id,
+      activity_name,
+      gas_type,
+      quantity,
+      calculated_quantity,
+      cas_number
+    from swrs.emission
+    join swrs.report
+      on emission.eccc_xml_file_id = report.eccc_xml_file_id
+      and report.report_type = 'R3'
+  $$,
+  'R3 report data from swrs_transform.r3_emission is contained in swrs.emission'
+);
 
 select * from finish();
 rollback;
