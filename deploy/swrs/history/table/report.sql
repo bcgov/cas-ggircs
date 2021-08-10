ed5dae9d8955e51c55b4b1b283f51b327be6ec8d
Naomi Aro <naomi.aro@gov.bc.ca>
2021-08-10 15:39:14 -0700

    refactor: update schema name

--- deploy/swrs/history/table/report.sql
+++ deploy/swrs/history/table/report.sql
@@ -23,22 +23,22 @@ create table swrs_history.report
     update_comment            varchar(1000)
 );
 
-comment on table swrs.report is 'The table housing all report data, derived from eccc_xml_file table';
-comment on column swrs.report.id is 'The primary key';
-comment on column swrs.report.eccc_xml_file_id is 'The internal primary key for the file';
-comment on column swrs.report.source_xml is 'The raw xml file imported from GHGR';
-comment on column swrs.report.imported_at is 'The timestamp noting when the file was imported';
-comment on column swrs.report.swrs_report_id is 'The swrs report id';
-comment on column swrs.report.prepop_report_id is 'The prepop report id';
-comment on column swrs.report.report_type is 'The type of report';
-comment on column swrs.report.swrs_facility_id is 'The ID for the reporting facility';
-comment on column swrs.report.swrs_organisation_id is 'The ID for the reporting organisation';
-comment on column swrs.report.reporting_period_duration is 'The length of the reporting period contained in report';
-comment on column swrs.report.status is 'The status of the report';
-comment on column swrs.report.version is 'The report version';
-comment on column swrs.report.submission_date is 'The date the report was submitted';
-comment on column swrs.report.last_modified_by is 'The person who last modified the report';
-comment on column swrs.report.last_modified_date is 'The timestamp recorded in SWRS when the report was last modified';
-comment on column swrs.report.update_comment is 'The description of the update';
+comment on table swrs_history.report is 'The table housing all report data, derived from eccc_xml_file table';
+comment on column swrs_history.report.id is 'The primary key';
+comment on column swrs_history.report.eccc_xml_file_id is 'The internal primary key for the file';
+comment on column swrs_history.report.source_xml is 'The raw xml file imported from GHGR';
+comment on column swrs_history.report.imported_at is 'The timestamp noting when the file was imported';
+comment on column swrs_history.report.swrs_report_id is 'The swrs report id';
+comment on column swrs_history.report.prepop_report_id is 'The prepop report id';
+comment on column swrs_history.report.report_type is 'The type of report';
+comment on column swrs_history.report.swrs_facility_id is 'The ID for the reporting facility';
+comment on column swrs_history.report.swrs_organisation_id is 'The ID for the reporting organisation';
+comment on column swrs_history.report.reporting_period_duration is 'The length of the reporting period contained in report';
+comment on column swrs_history.report.status is 'The status of the report';
+comment on column swrs_history.report.version is 'The report version';
+comment on column swrs_history.report.submission_date is 'The date the report was submitted';
+comment on column swrs_history.report.last_modified_by is 'The person who last modified the report';
+comment on column swrs_history.report.last_modified_date is 'The timestamp recorded in SWRS when the report was last modified';
+comment on column swrs_history.report.update_comment is 'The description of the update';
 
 commit;
