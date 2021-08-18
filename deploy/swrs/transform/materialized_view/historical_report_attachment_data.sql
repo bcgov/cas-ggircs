3c8f16dfa8c87ec0b74822034a378542a47666d6
Dylan Leard <dylan@button.is>
2021-08-18 14:56:14 -0700

    refactor: fix broken xpath & add missing row comments

--- deploy/swrs/transform/materialized_view/historical_report_attachment_data.sql
+++ deploy/swrs/transform/materialized_view/historical_report_attachment_data.sql
@@ -13,9 +13,9 @@ create materialized view swrs_transform.historical_report_attachment_data as (
         '//UploadedFileName[string-length(text()) > 0]/parent::FileDetails'
         passing xml_file
         columns
-          process_name varchar(1000) path 'normalize-space()./ancestor::Process/@ProcessName)',
-          sub_process_name varchar(1000) path 'normalize-space()./ancestor::SubProcess/@SubProcessName)',
-          information_requirement varchar(1000) path 'normalize-space()./ancestor::SubProcess/@InformationRequirement)',
+          process_name varchar(1000) path 'normalize-space(./ancestor::Process/@ProcessName)',
+          sub_process_name varchar(1000) path 'normalize-space(./ancestor::SubProcess/@SubProcessName)',
+          information_requirement varchar(1000) path 'normalize-space(./ancestor::SubProcess/@InformationRequirement)',
           file_number int path 'normalize-space(./File)',
           uploaded_file_name varchar(1000) path 'normalize-space(./UploadedFileName)',
           uploaded_by varchar(1000) path 'normalize-space(./UploadedBy)',
@@ -30,9 +30,10 @@ comment on column swrs_transform.historical_report_attachment_data.id is 'A gene
 comment on column swrs_transform.historical_report_attachment_data.eccc_xml_file_id is 'The internal primary key for the file';
 comment on column swrs_transform.historical_report_attachment_data.process_name is 'The process_name in this context describes the type of attachment (ie: Process Flow Diagram)';
 comment on column swrs_transform.historical_report_attachment_data.sub_process_name is 'The sub_process_name in this context is a more in-depth description of this attachment';
+comment on column swrs_transform.historical_report_attachment_data.information_requirement is 'Denotes whether or not this attachment is required. Can be one of [Optional, Required]';
 comment on column swrs_transform.historical_report_attachment_data.file_number is 'The file_number corresponds to the type of attachment';
 comment on column swrs_transform.historical_report_attachment_data.uploaded_file_name is 'The name of the attachment file that was uploaded';
-comment on column swrs_transform.historical_report_attachment_data.uploaded_file_name is 'The name of the user who uploaded the attachment file';
-comment on column swrs_transform.historical_report_attachment_data.uploaded_file_name is 'The date of upload';
+comment on column swrs_transform.historical_report_attachment_data.uploaded_by is 'The name of the user who uploaded the attachment file';
+comment on column swrs_transform.historical_report_attachment_data.uploaded_at is 'The date of upload';
 
 commit;
