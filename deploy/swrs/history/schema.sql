147861181a5ec2fd2c7365bc8fdf89c6fe0c35cf
Dylan Leard <dylan@button.is>
2021-08-18 14:55:40 -0700

    refactor: fix swrs_history schema comment

--- deploy/swrs/history/schema.sql
+++ deploy/swrs/history/schema.sql
@@ -3,6 +3,6 @@
 BEGIN;
 
 create schema swrs_history;
-comment on schema swrs is 'A schema containing all versions of the reports and attachments from SWRS. Available in Metabase';
+comment on schema swrs_history is 'A schema containing all versions of the reports and attachments from SWRS(Single Window Reporting System). This differs from the swrs schema in that the swrs schema contains only the final version of the data. Available in Metabase';
 
 COMMIT;
