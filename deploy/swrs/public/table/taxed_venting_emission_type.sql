1d21b6ba467d69b88e7b8609648bd398dbfd0551
Dylan Leard <dylan@button.is>
2021-11-23 15:19:20 -0800

    chore: fix comments on venting emission type table

--- deploy/swrs/public/table/taxed_venting_emission_type.sql
+++ deploy/swrs/public/table/taxed_venting_emission_type.sql
@@ -8,8 +8,8 @@ create table swrs.taxed_venting_emission_type (
   taxed_emission_type varchar(1000)
 );
 
-comment on table swrs.emission_category is 'Table of emisison types that are carbon taxed in relation to venting as per https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/249_2015#ScheduleA';
-comment on column swrs.emission_category.id is 'Unique ID for the taxed_venting_emission_type table';
-comment on column swrs.emission_category.swrs_emission_category is 'The name of the carbon taxed emission type';
+comment on table swrs.taxed_venting_emission_type is 'Table of emisison types that are carbon taxed in relation to venting as per https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/249_2015#ScheduleA';
+comment on column swrs.taxed_venting_emission_type.id is 'Unique ID for the taxed_venting_emission_type table';
+comment on column swrs.taxed_venting_emission_type.taxed_emission_type is 'The name of the carbon taxed emission type';
 
 commit;
