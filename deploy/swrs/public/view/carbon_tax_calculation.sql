3ba3934ee0c6706b3eceb17287cb3861cc1d760a
Dylan Leard <dylan@button.is>
2021-12-06 10:47:16 -0800

    chore: add emission_category column to CT calc view

--- deploy/swrs/public/view/carbon_tax_calculation.sql
+++ deploy/swrs/public/view/carbon_tax_calculation.sql
@@ -16,40 +16,73 @@ create or replace view swrs.carbon_tax_calculation as
   with fuel_data as (
 
     -- FLARED EMISSION
-    select e.report_id, 'Natural Gas (Sm^3)' as fuel_type, e.fuel_mapping_id, fm.fuel_carbon_tax_details_id, ctd.carbon_tax_act_fuel_type_id, (coalesce(e.quantity, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
-        from swrs.emission e
-        join swrs.fuel_mapping fm
-          on e.fuel_mapping_id = fm.id
-          and e.fuel_mapping_id = 145
-        join swrs.fuel_carbon_tax_details ctd
-          on fm.fuel_carbon_tax_details_id = ctd.id
+    select
+      e.report_id,
+      'Natural Gas (Sm^3)' as fuel_type,
+      e.emission_category,
+      e.fuel_mapping_id,
+      fm.fuel_carbon_tax_details_id,
+      ctd.carbon_tax_act_fuel_type_id,
+      (coalesce(e.quantity, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
+    from swrs.emission e
+    join swrs.fuel_mapping fm
+      on e.fuel_mapping_id = fm.id
+      and e.fuel_mapping_id = 145 -- Flared Natural Gas CO2
+    join swrs.fuel_carbon_tax_details ctd
+      on fm.fuel_carbon_tax_details_id = ctd.id
 
     union
 
     -- VENTED EMISSION
-    select e.report_id, 'Natural Gas (Sm^3)' as fuel_type, e.fuel_mapping_id, fm.fuel_carbon_tax_details_id, ctd.carbon_tax_act_fuel_type_id, (coalesce(e.quantity, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
-        from swrs.emission e
-        join swrs.fuel_mapping fm
-          on e.fuel_mapping_id = fm.id
-          and e.fuel_mapping_id = 148
-        join swrs.fuel_carbon_tax_details ctd
-          on fm.fuel_carbon_tax_details_id = ctd.id
+    select
+      e.report_id,
+      'Natural Gas (Sm^3)' as fuel_type,
+      e.emission_category,
+      e.fuel_mapping_id,
+      fm.fuel_carbon_tax_details_id,
+      ctd.carbon_tax_act_fuel_type_id,
+      (coalesce(e.quantity, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
+    from swrs.emission e
+    join swrs.fuel_mapping fm
+      on e.fuel_mapping_id = fm.id
+      and e.fuel_mapping_id = 148 -- Vented Natural Gas
+    join swrs.fuel_carbon_tax_details ctd
+      on fm.fuel_carbon_tax_details_id = ctd.id
 
     union
 
     -- FUEL
-    select f.report_id, ctd.normalized_fuel_type, f.fuel_mapping_id, fm.fuel_carbon_tax_details_id, ctd.carbon_tax_act_fuel_type_id, (coalesce(f.annual_fuel_amount, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
-        from swrs.fuel f
-        join swrs.fuel_mapping fm
-          on f.fuel_mapping_id = fm.id
-        join swrs.fuel_carbon_tax_details ctd
-          on fm.fuel_carbon_tax_details_id = ctd.id
-        join swrs.emission_category ec
-          on f.emission_category = ec.swrs_emission_category
-          and ec.carbon_taxed=true
+    select
+      f.report_id,
+      ctd.normalized_fuel_type as fuel_type,
+      f.emission_category,
+      f.fuel_mapping_id,
+      fm.fuel_carbon_tax_details_id,
+      ctd.carbon_tax_act_fuel_type_id,
+      (coalesce(f.annual_fuel_amount, 0::numeric) * ctd.unit_conversion_factor::numeric) as fuel_amount
+    from swrs.fuel f
+    join swrs.fuel_mapping fm
+      on f.fuel_mapping_id = fm.id
+    join swrs.fuel_carbon_tax_details ctd
+      on fm.fuel_carbon_tax_details_id = ctd.id
+    join swrs.emission_category ec
+      on f.emission_category = ec.swrs_emission_category
+      and ec.carbon_taxed=true
 
     )
-    select fd.*, _facility.id as facility_id, _facility.facility_name, r.reporting_period_duration as reporting_year, _fuel_charge.fuel_charge, round((fuel_amount * _fuel_charge.fuel_charge), 2) as calculated_carbon_tax from fuel_data fd
+    select
+      fd.report_id,
+      _facility.id as facility_id,
+      fd. fuel_mapping_id,
+      fd.fuel_carbon_tax_details_id,
+      fd.carbon_tax_act_fuel_type_id,
+      _facility.facility_name,
+      r.reporting_period_duration as reporting_year,
+      fd.fuel_type,
+      fd.emission_category,
+      fd.fuel_amount,
+      _fuel_charge.fuel_charge,
+      round((fuel_amount * _fuel_charge.fuel_charge), 2) as calculated_carbon_tax from fuel_data fd
     join swrs.report r
         on r.id = fd.report_id
     join swrs.facility as _facility
@@ -65,12 +98,13 @@ create or replace view swrs.carbon_tax_calculation as
 comment on view swrs.carbon_tax_calculation is 'Calculates carbon tax for each fuel type based on the data in the Schedule 2 table found in the carbon tax act https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/08040_01';
 comment on column swrs.carbon_tax_calculation.report_id is 'The id of the report each fuel was reported for';
 comment on column swrs.carbon_tax_calculation.fuel_type is 'The fuel type reported';
+comment on column swrs.carbon_tax_calculation.emission_category is 'The emission category the reported fuels falls under. Some categories are taxed and others are not (a carbon_taxed boolean is defined in the swrs.emission_category table). Emission categories are defined in the Greenhouse Gas Industrial Reporting and Control Act here: https://www.bclaws.gov.bc.ca/civix/document/id/lc/statreg/249_2015#section14';
 comment on column swrs.carbon_tax_calculation.fuel_mapping_id is 'The id to the the fuel mapping table matching the fuel type';
 comment on column swrs.carbon_tax_calculation.fuel_carbon_tax_details_id is 'The id to the the fuel carbon tax details table matching the fuel type';
 comment on column swrs.carbon_tax_calculation.carbon_tax_act_fuel_type_id is 'The id to the the carbon tax act fuel type table matching the fuel type, matches the Schedule 2 table in the carbon tax act https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/08040_01';
 comment on column swrs.carbon_tax_calculation.fuel_amount is $$
   The amount of fuel reported for the fuel type.
-  This fuel_amount is the annual_fuel_amount as reported in swrs multiplied by the unit_conversion_factor in the fuel_carbon_tax_details table to normalize the units with the rate defined in the carbon tax.
+  This fuel_amount is the annual_fuel_amount as reported in the swrs report multiplied by the unit_conversion_factor in the fuel_carbon_tax_details table to normalize the units with the rate defined in the carbon tax.
   Vented and Flared emissions return a fuel_amount that has been converted to a natural gas fuel_amount of m3.
   X tonnes of Flared CO2 emissions return a record with a Natural gas fuel amount of Y (as per WCI.20 EF for non-marketable gas, Table 20-3: 2.151 kgCO2/m3 non-marketable NG in BC)
 $$;
