6a7c77edd44eecc4f4cac53222b12c48feaf13ed
Dylan Leard <dylan@button.is>
2022-01-27 12:07:56 -0800

    chore: add update mutation for fuel_mapping table

---
+++ ggircs-app/app/mutations/fuelManagement/updateFuelMapping.ts
@@ -0,0 +1,46 @@
+import { graphql } from "react-relay";
+import {
+  updateFuelMappingMutation as mutationType,
+  updateFuelMappingMutationVariables as mutationVariables,
+} from "updateFuelMappingMutation.graphql";
+import RelayModernEnvironment from "relay-runtime/lib/store/RelayModernEnvironment";
+import BaseMutation from "mutations/BaseMutation";
+
+const mutation = graphql`
+  mutation updateFuelMappingMutation(
+    $input: UpdateFuelMappingByRowIdInput!
+  ) {
+    updateFuelMappingByRowId(input: $input) {
+      fuelMapping {
+        id
+        fuelType
+        fuelCarbonTaxDetailsId
+      }
+    }
+  }
+`;
+
+const updateFuelMappingMutation = async (
+  environment: RelayModernEnvironment,
+  variables: mutationVariables
+) => {
+  const optimisticResponse = {
+    updateFuelMappingByRowId: {
+      fuelMapping: {
+        rowId: variables.input.rowId,
+        ...variables.input.fuelMappingPatch,
+      },
+    },
+  };
+  const m = new BaseMutation<mutationType>(
+    "update-fuel-mapping-mutation"
+  );
+  return m.performMutation(
+    environment,
+    mutation,
+    variables,
+    optimisticResponse
+  );
+};
+
+export default updateFuelMappingMutation;
