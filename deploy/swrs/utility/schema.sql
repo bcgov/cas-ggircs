0294afaaf077666b73dff7617fd9af36e5673d51
Dylan Leard <dylan@button.is>
2022-02-07 10:01:39 -0800

    test: fix missing schema description, update search path

--- deploy/swrs/utility/schema.sql
+++ deploy/swrs/utility/schema.sql
@@ -4,6 +4,6 @@ begin;
 
 create schema swrs_utility;
 
-comment on schema swrs is 'A schema containing functions, mutations and contextual data relating to the ETL swrs schema that should not be dropped during the ETL process';
+comment on schema swrs_utility is 'A schema containing functions, mutations and contextual data relating to the ETL swrs schema that should not be dropped during the ETL process';
 
 commit;
