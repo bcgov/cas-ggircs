44b790781d18c9baaf942f35bcf01c396a813d45
Dylan Leard <dylan@button.is>
2022-02-28 16:14:54 -0800

    test: add pgtap tests for view

--- test/unit/view_facility_details_test.sql
+++ test/unit/view_facility_details_test.sql
@@ -4,7 +4,7 @@ create extension if not exists pgtap;
 reset client_min_messages;
 
 begin;
-select plan(2);
+select plan(4);
 
 select has_view(
     'swrs', 'facility_details',
@@ -18,6 +18,31 @@ select columns_are('swrs', 'facility_details', array[
   'status', 'latitude', 'longitude', 'facility_bc_ghg_id', 'naics_id',
   'naics_classification', 'naics_code'
 ]);
+
+insert into swrs.report(id, swrs_report_id) values (1,1), (2,2);
+
+insert into swrs.organisation(id, report_id) values (1,1), (2,2);
+
+insert into swrs.facility(id, report_id, organisation_id) values (1,1,1), (2,2,2);
+
+insert into swrs.naics(id, report_id, facility_id, registration_data_facility_id, naics_code, naics_priority, naics_classification)
+  values (1,1,1,1,00000,'Primary', 'Primary'), (2,1,1,1,12345,'Not Primary','Not Primary');
+
+select results_eq(
+  $$
+    select naics_code, naics_classification from swrs.facility_details where id=1
+  $$,
+  $$
+    values(00000::integer, 'Primary'::varchar)
+  $$,
+  'facility_details retrieves the Primary naics_code for a facility'
+);
+
+select is(
+  (select count(*) from swrs.facility_details where naics_code = 12345),
+  0::bigint,
+  'facility_details does not retrieve Non-Primary naics_codes for a facility'
+);
 
 select * from finish();
 rollback;
