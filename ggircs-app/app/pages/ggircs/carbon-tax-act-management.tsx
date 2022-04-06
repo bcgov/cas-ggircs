d450e66c0176d86a6728f67b79ad2fef8732ddac
Dylan Leard <dylan@button.is>
2022-04-06 13:46:38 -0700

    feat: add carbon tax management page

---
+++ ggircs-app/app/pages/ggircs/carbon-tax-act-management.tsx
@@ -0,0 +1,60 @@
+import { withRelay, RelayProps } from "relay-nextjs";
+import { graphql, usePreloadedQuery } from "react-relay/hooks";
+import withRelayOptions from "lib/relay/withRelayOptions";
+import type { NextPageContext } from "next";
+import { NextRouter } from "next/router";
+import { carbonTaxActManagementQuery } from "__generated__/carbonTaxActManagementQuery.graphql";
+import DefaultLayout from "components/Layout/DefaultLayout";
+import CarbonTaxActFuelType from "components/FuelManagement/CarbonTaxActFuelType";
+
+export const CarbonTaxActManagementQuery = graphql`
+  query carbonTaxActManagementQuery($carbonTaxActFuelTypeId: ID!) {
+    query {
+      ...CarbonTaxActFuelType_query
+        @arguments(carbonTaxActFuelTypeId: $carbonTaxActFuelTypeId)
+      session {
+        ...DefaultLayout_session
+      }
+    }
+  }
+`;
+
+export function CarbonTaxActManagement({
+  preloadedQuery,
+}: RelayProps<{}, carbonTaxActManagementQuery>) {
+  const { query } = usePreloadedQuery(
+    CarbonTaxActManagementQuery,
+    preloadedQuery
+  );
+
+  return (
+    <>
+      <DefaultLayout
+        session={query.session}
+        title="Fuel Type Management"
+        width="wide"
+      >
+        <div>
+          <CarbonTaxActFuelType
+            query={query}
+            pageQuery={CarbonTaxActManagementQuery}
+          />
+        </div>
+      </DefaultLayout>
+    </>
+  );
+}
+
+const withRelayOptionsOverride = {
+  ...withRelayOptions,
+  variablesFromContext: (ctx: NextPageContext | NextRouter) => ({
+    carbonTaxActFuelTypeId: "",
+    ...ctx.query,
+  }),
+};
+
+export default withRelay(
+  CarbonTaxActManagement,
+  CarbonTaxActManagementQuery,
+  withRelayOptionsOverride
+);
