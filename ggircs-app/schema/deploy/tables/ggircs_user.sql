94780e0402c58673d586c22cc1d5ed7b9fab55d6
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-12-14 09:07:55 -0800

    refactor: ggircs user table has a sub of type text

--- ggircs-app/schema/deploy/tables/ggircs_user.sql
+++ ggircs-app/schema/deploy/tables/ggircs_user.sql
@@ -2,72 +2,26 @@
 
 begin;
 
-create table ggircs_app.ggircs_user
-(
-  id integer primary key generated always as identity,
-  uuid uuid not null,
-  first_name varchar(1000),
-  last_name varchar(1000),
-  email_address varchar(1000),
-  created_at timestamp with time zone not null default now(),
-  created_by int references ggircs_app.ggircs_user,
-  updated_at timestamp with time zone not null default now(),
-  updated_by int references ggircs_app.ggircs_user,
-  deleted_at timestamp with time zone,
-  deleted_by int references ggircs_app.ggircs_user
+-- Dropping existing policies that depend on the uuid
+drop policy ggircs_user_insert_ggircs_user on ggircs_app.ggircs_user;
+drop policy ggircs_user_update_ggircs_user on ggircs_app.ggircs_user;
 
-);
+alter table ggircs_app.ggircs_user alter column uuid type varchar(1000);
+alter table ggircs_app.ggircs_user rename column uuid to session_sub;
 
-create trigger _100_timestamps
-  before insert or update on ggircs_app.ggircs_user
-  for each row
-  execute procedure ggircs_app_private.update_timestamps();
+-- Postgres rebuilds indexes when renaming columns and changing their types,
+-- but does not change their name
+alter index ggircs_app.ggircs_user_uuid rename to ggircs_user_session_sub;
 
-create unique index ggircs_user_uuid on ggircs_app.ggircs_user(uuid);
-
-do
-$grant$
-begin
--- Grant ggircs_user permissions
-perform ggircs_app_private.grant_permissions('select', 'ggircs_user', 'ggircs_user');
-perform ggircs_app_private.grant_permissions('insert', 'ggircs_user', 'ggircs_user');
-perform ggircs_app_private.grant_permissions('update', 'ggircs_user', 'ggircs_user',
-  ARRAY['first_name', 'last_name', 'email_address', 'created_at', 'created_by', 'updated_at', 'updated_by', 'deleted_at', 'deleted_by']);
-
--- Grant ggircs_guest permissions
-perform ggircs_app_private.grant_permissions('select', 'ggircs_user', 'ggircs_guest');
-
-end
-$grant$;
-
--- Enable row-level security
-alter table ggircs_app.ggircs_user enable row level security;
-
+-- Rebuilding policies with the proper session_sub reference
 do
 $policy$
 begin
--- ggircs_user RLS: can see all users, but can only modify its own record
-perform ggircs_app_private.upsert_policy('ggircs_user_select_ggircs_user', 'ggircs_user', 'select', 'ggircs_user', 'true');
-perform ggircs_app_private.upsert_policy('ggircs_user_insert_ggircs_user', 'ggircs_user', 'insert', 'ggircs_user', 'uuid=(select sub from ggircs_app.session())');
-perform ggircs_app_private.upsert_policy('ggircs_user_update_ggircs_user', 'ggircs_user', 'update', 'ggircs_user', 'uuid=(select sub from ggircs_app.session())');
-
--- ggircs_guest RLS: can only see its own record
--- perform ggircs_app_private.upsert_policy('ggircs_guest_select_ggircs_user', 'ggircs_user', 'select', 'ggircs_guest', 'uuid=(select sub from ggircs_app.session())');
-
+  perform ggircs_app_private.upsert_policy('ggircs_user_insert_ggircs_user', 'ggircs_user', 'insert', 'ggircs_user', 'session_sub=(select sub from ggircs_app.session())');
+  perform ggircs_app_private.upsert_policy('ggircs_user_update_ggircs_user', 'ggircs_user', 'update', 'ggircs_user', 'session_sub=(select sub from ggircs_app.session())');
 end
 $policy$;
 
-comment on table ggircs_app.ggircs_user is 'Table containing the benchmark and eligibility threshold for a product';
-comment on column ggircs_app.ggircs_user.id is 'Unique ID for the user';
-comment on column ggircs_app.ggircs_user.uuid is 'Universally Unique ID for the user, defined by the single sign-on provider';
-comment on column ggircs_app.ggircs_user.first_name is 'User''s first name';
-comment on column ggircs_app.ggircs_user.last_name is 'User''s last name';
-comment on column ggircs_app.ggircs_user.email_address is 'User''s email address';
-comment on column ggircs_app.ggircs_user.created_at is 'The date this record was inserted';
-comment on column ggircs_app.ggircs_user.created_by is 'The foreign key to the user id that created this record';
-comment on column ggircs_app.ggircs_user.updated_at is 'The date this record was last updated';
-comment on column ggircs_app.ggircs_user.updated_by is 'The foreign key to the user id that last updated this record';
-comment on column ggircs_app.ggircs_user.deleted_at is 'The date this record was deleted';
-comment on column ggircs_app.ggircs_user.deleted_by is 'The foreign key to the user id that deleted this record';
+comment on column ggircs_app.ggircs_user.session_sub is 'Unique ID for the user/provider combination, defined by the single sign-on provider.';
 
 commit;
--- ggircs-app/schema/revert/tables/ggircs_user.sql
+++ ggircs-app/schema/revert/tables/ggircs_user.sql
@@ -1,7 +1,25 @@
--- Revert ggircs-app:tables/ggircs_user from pg
+-- Deploy ggircs-app:tables/ggircs_user to pg
 
 begin;
 
-drop table ggircs_app.ggircs_user;
+-- Dropping existing policies that depend on the text
+drop policy ggircs_user_insert_ggircs_user on ggircs_app.ggircs_user;
+drop policy ggircs_user_update_ggircs_user on ggircs_app.ggircs_user;
+
+alter table ggircs_app.ggircs_user alter column session_sub type uuid using session_sub::uuid;
+alter table ggircs_app.ggircs_user rename column session_sub to uuid;
+
+alter index ggircs_app.ggircs_user_session_sub rename to ggircs_user_uuid;
+
+-- Rebuilding policies with the proper session_sub reference
+do
+$policy$
+begin
+  perform ggircs_app_private.upsert_policy('ggircs_user_insert_ggircs_user', 'ggircs_user', 'insert', 'ggircs_user', 'uuid::text=(select sub from ggircs_app.session())::text');
+  perform ggircs_app_private.upsert_policy('ggircs_user_update_ggircs_user', 'ggircs_user', 'update', 'ggircs_user', 'uuid::text=(select sub from ggircs_app.session())::text');
+end
+$policy$;
+
+comment on column ggircs_app.ggircs_user.uuid is 'Universally Unique ID for the user, defined by the single sign-on provider';
 
 commit;
