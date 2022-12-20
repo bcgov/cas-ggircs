38e8f0fe72a1baa2a231290edfb7cc03d872f448
Pierre Bastianelli <pierre.bastianelli@gov.bc.ca>
2022-12-19 16:36:15 -0800

    refactor: authenticating requests to the eccc API

--- ggircs-app/app/server/routers/api/eccc.js
+++ ggircs-app/app/server/routers/api/eccc.js
@@ -1,5 +1,8 @@
 require("isomorphic-fetch");
 const express = require("express");
+const { isAuthenticated } = require("@bcgov-cas/sso-express/dist/helpers");
+const { getUserGroups } = require("../../helpers/userGroupAuthentication");
+const { getUserGroupLandingRoute } = require("../../../lib/user-groups");
 
 const ecccApiRouter = express.Router();
 const { ECCC_FILE_BROWSER_HOST, ECCC_FILE_BROWSER_PORT, HOST, PORT } =
@@ -17,17 +20,26 @@ const graphqlEndpoint = secure
  * This router forwards all requests to the ECCC file browser service
  * after ensuring that the client is authenticated
  */
-ecccApiRouter.get("*", async (req, res) => {
-  const ecccApiRes = await fetch(
-    `${ECCC_FILE_BROWSER_HOST}:${ECCC_FILE_BROWSER_PORT}${req.url}`
-  );
+ecccApiRouter.get(
+  "*",
+  (req, res, next) => {
+    if (!isAuthenticated(req)) {
+      const groups = getUserGroups(req);
+      return res.redirect(getUserGroupLandingRoute(groups));
+    }
+    return next();
+  },
+  async (req, res) => {
+    const ecccApiRes = await fetch(
+      `${ECCC_FILE_BROWSER_HOST}:${ECCC_FILE_BROWSER_PORT}${req.url}`
+    );
 
-  if (ecccApiRes.ok && req.path.endsWith("/download")) {
-    const splitPath = req.path.split("/");
-    const ecccZipFileName = splitPath[splitPath.length - 2];
-    const mutationBody = {
-      id: "createEcccFileDownloadLogMutation",
-      query: `mutation createEcccFileDownloadLogMutation(
+    if (ecccApiRes.ok && req.path.endsWith("/download")) {
+      const splitPath = req.path.split("/");
+      const ecccZipFileName = splitPath[splitPath.length - 2];
+      const mutationBody = {
+        id: "createEcccFileDownloadLogMutation",
+        query: `mutation createEcccFileDownloadLogMutation(
             $input: CreateEcccFileDownloadLogInput!
           ) {
             createEcccFileDownloadLog(input: $input) {
@@ -36,42 +48,43 @@ ecccApiRouter.get("*", async (req, res) => {
               }
             }
           }`,
-      variables: {
-        input: {
-          ecccFileDownloadLog: {
-            ecccZipFileName,
-            ecccIndividualFilePath: req.query.filename,
+        variables: {
+          input: {
+            ecccFileDownloadLog: {
+              ecccZipFileName,
+              ecccIndividualFilePath: req.query.filename,
+            },
           },
         },
-      },
-    };
-    // log the download in the database.
-    try {
-      await fetch(graphqlEndpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Cookie: req.get("Cookie"), // forward the cookie so that the graphql request is authenticated
-        },
-        body: JSON.stringify(mutationBody),
-      });
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-      return;
+      };
+      // log the download in the database.
+      try {
+        await fetch(graphqlEndpoint, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Cookie: req.get("Cookie"), // forward the cookie so that the graphql request is authenticated
+          },
+          body: JSON.stringify(mutationBody),
+        });
+      } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+        return;
+      }
     }
+
+    res.setHeader("Content-Type", ecccApiRes.headers.get("content-type"));
+    res.setHeader("Content-Length", ecccApiRes.headers.get("content-length"));
+
+    const contentDispositionHeader = ecccApiRes.headers.get(
+      "content-disposition"
+    );
+    if (contentDispositionHeader)
+      res.setHeader("Content-Disposition", contentDispositionHeader);
+
+    ecccApiRes.body.pipe(res);
   }
-
-  res.setHeader("Content-Type", ecccApiRes.headers.get("content-type"));
-  res.setHeader("Content-Length", ecccApiRes.headers.get("content-length"));
-
-  const contentDispositionHeader = ecccApiRes.headers.get(
-    "content-disposition"
-  );
-  if (contentDispositionHeader)
-    res.setHeader("Content-Disposition", contentDispositionHeader);
-
-  ecccApiRes.body.pipe(res);
-});
+);
 
 module.exports = { ecccApiRouter };
