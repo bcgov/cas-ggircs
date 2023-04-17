db6cdcbc6a8415e5445a99958f4a78d7f577517f
Brianna Cerkiewicz <brianna@button.is>
2023-04-17 10:57:42 -0700

    chore: drop load_measured_emission_factor function

--- verify/swrs/transform/function/load_measured_emission_factor.sql
+++ verify/swrs/transform/function/load_measured_emission_factor.sql
@@ -2,6 +2,15 @@
 
 begin;
 
-select pg_get_functiondef('swrs_transform.load_measured_emission_factor()'::regprocedure);
+do $$
+  begin
+
+    if (select exists(select * from pg_proc where proname='swrs_transform.load_measured_emission_factor')) then
+      raise exception 'swrs_transform.load_measured_emission_factor() exists when it should not';
+    else
+      perform true;
+    end if;
+
+  end; $$;
 
 rollback;
