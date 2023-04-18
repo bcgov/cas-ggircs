9f35619cce0a4ac4005f5cb925406e7beb47436c
Brianna Cerkiewicz <brianna@button.is>
2023-04-18 08:47:42 -0700

    test: emission_factor_table test

---
+++ test/unit/table_emission_factor_test.sql
@@ -0,0 +1,331 @@
+set client_encoding = 'utf-8';
+set client_min_messages = warning;
+create extension if not exists pgtap;
+reset client_min_messages;
+
+begin;
+select * from no_plan();
+
+
+insert into swrs_extract.eccc_xml_file (xml_file, imported_at) values ($$
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <RegistrationData>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <FrenchTradeName/>
+        <CRABusinessNumber>12345</CRABusinessNumber>
+        <DUNSNumber>0</DUNSNumber>
+        <WebSite>www.nhl.com</WebSite>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>fname</FacilityName>
+        <RelationshipType>Owned and Operated</RelationshipType>
+        <PortabilityIndicator>N</PortabilityIndicator>
+        <Status>Active</Status>
+      </Details>
+    </Facility>
+  </RegistrationData>
+  <ReportDetails>
+    <ReportID>1234</ReportID>
+    <ReportType>R1</ReportType>
+    <FacilityId>0000</FacilityId>
+    <FacilityType>EIO</FacilityType>
+    <OrganisationId>0000</OrganisationId>
+    <ReportingPeriodDuration>2025</ReportingPeriodDuration>
+    <ReportStatus>
+      <Status>Submitted</Status>
+      <SubmissionDate>2013-03-27T19:25:55.32</SubmissionDate>
+      <LastModifiedBy>Buddy</LastModifiedBy>
+    </ReportStatus>
+  </ReportDetails>
+  <OperationalWorkerReport/>
+  <VerifyTombstone>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <CRABusinessNumber>123456778</CRABusinessNumber>
+        <DUNSNumber>00-000-0000</DUNSNumber>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>Bart Simpson</FacilityName>
+      </Details>
+    </Facility>
+    <ParentOrganisations/>
+  </VerifyTombstone>
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ActivityPages>
+      <Process ProcessName="ElectricityGeneration">
+        <SubProcess SubprocessName="Emissions from fuel combustion for electricity generation" InformationRequirement="Required">
+          <Units UnitType="Cogen Units">
+            <Unit>
+              <COGenUnit>
+                <CogenUnitName>Boiler 16 - hog fuel</CogenUnitName>
+                <NameplateCapacity>5.9</NameplateCapacity>
+                <NetPower>30165</NetPower>
+                <CycleType>Topping</CycleType>
+                <ThermalOutputQuantity>886917960</ThermalOutputQuantity>
+                <SupplementalFiringPurpose>Electr. Generation</SupplementalFiringPurpose>
+              </COGenUnit>
+              <Fuels>
+                <Fuel>
+                  <FuelType>Wood Waste</FuelType>
+                  <FuelClassification>Biomass in Schedule C</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>bone dry tonnes</FuelUnits>
+                  <AnnualFuelAmount>0</AnnualFuelAmount>
+                  <AnnualSteamGeneration>290471000</AnnualSteamGeneration>
+                  <MeasuredEmissionFactors>
+                    <MeasuredEmissionFactor>
+                      <MeasuredEmissionFactorGas>CO2</MeasuredEmissionFactorGas>
+                      <MeasuredEmissionFactorAmount>100</MeasuredEmissionFactorAmount>
+                      <MeasuredEmissionFactorUnitType>g/GJ</MeasuredEmissionFactorUnitType>
+                    </MeasuredEmissionFactor>
+                    <MeasuredEmissionFactor>
+                      <MeasuredEmissionFactorGas>CH4</MeasuredEmissionFactorGas>
+                      <MeasuredEmissionFactorAmount>111</MeasuredEmissionFactorAmount>
+                      <MeasuredEmissionFactorUnitType>g/GJ</MeasuredEmissionFactorUnitType>
+                    </MeasuredEmissionFactor>
+                  </MeasuredEmissionFactors>
+                  <AlternativeMethodologyDescription/>
+                </Fuel>
+                <Fuel>
+                  <FuelType>Residual Fuel Oil (#5 &amp; 6)</FuelType>
+                  <FuelClassification>non-biomass</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>kilolitres</FuelUnits>
+                  <AnnualFuelAmount>9441</AnnualFuelAmount>
+                  <AnnualWeightedAverageCarbonContent>0.862</AnnualWeightedAverageCarbonContent>
+                  <AlternativeMethodologyDescription/>
+                </Fuel>
+              </Fuels>
+            </Unit>
+          </Units>
+        </SubProcess>
+      </Process>
+      <Process>
+        <SubProcess SubprocessName="Additional Reportable Information as per WCI.352(i)(1)-(12)" InformationRequirement="MandatoryAdditional">
+          <Amount AmtDomain="PulpAndPaperBlackLiquor" AmtAction="Combusted" AmtPeriod="Annual">168389</Amount>
+          <PercentSolidsByWeight>53</PercentSolidsByWeight>
+          <PulpAndPaperCarbonates/>
+        </SubProcess>
+      </Process>
+    </ActivityPages>
+  </ActivityData>
+</ReportData>
+$$, '2017-06-22 19:10:25-07'::timestamptz), ($$
+<ReportData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <RegistrationData>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <FrenchTradeName/>
+        <CRABusinessNumber>12345</CRABusinessNumber>
+        <DUNSNumber>0</DUNSNumber>
+        <WebSite>www.nhl.com</WebSite>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>fname</FacilityName>
+        <RelationshipType>Owned and Operated</RelationshipType>
+        <PortabilityIndicator>N</PortabilityIndicator>
+        <Status>Active</Status>
+      </Details>
+    </Facility>
+    <Contacts/>
+  </RegistrationData>
+  <ReportDetails>
+    <ReportID>1235</ReportID>
+    <ReportType>R1</ReportType>
+    <FacilityId>0001</FacilityId>
+    <FacilityType>LF_a</FacilityType>
+    <OrganisationId>0000</OrganisationId>
+    <ReportingPeriodDuration>2020</ReportingPeriodDuration>
+    <ReportStatus>
+      <Status>Submitted</Status>
+      <SubmissionDate>2013-03-28T19:25:55.32</SubmissionDate>
+      <LastModifiedBy>Buddy</LastModifiedBy>
+    </ReportStatus>
+  </ReportDetails>
+  <OperationalWorkerReport/>
+  <VerifyTombstone>
+    <Organisation>
+      <Details>
+        <BusinessLegalName>Bart Simpson</BusinessLegalName>
+        <EnglishTradeName>Bart Simpson</EnglishTradeName>
+        <CRABusinessNumber>123456778</CRABusinessNumber>
+        <DUNSNumber>00-000-0000</DUNSNumber>
+      </Details>
+    </Organisation>
+    <Facility>
+      <Details>
+        <FacilityName>Bart Simpson</FacilityName>
+      </Details>
+    </Facility>
+    <ParentOrganisations/>
+  </VerifyTombstone>
+  <ActivityData xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+    <ActivityPages>
+      <Process ProcessName="ElectricityGeneration">
+        <SubProcess SubprocessName="Emissions from fuel combustion for electricity generation" InformationRequirement="Required">
+          <Units UnitType="Cogen Units">
+            <Unit>
+              <COGenUnit>
+                <CogenUnitName>Boiler 16 - hog fuel</CogenUnitName>
+                <NameplateCapacity>5.9</NameplateCapacity>
+                <NetPower>30165</NetPower>
+                <CycleType>Topping</CycleType>
+                <ThermalOutputQuantity>886917960</ThermalOutputQuantity>
+                <SupplementalFiringPurpose>Electr. Generation</SupplementalFiringPurpose>
+              </COGenUnit>
+              <Fuels>
+                <Fuel>
+                  <FuelType>Wood Waste</FuelType>
+                  <FuelClassification>Biomass in Schedule C</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>bone dry tonnes</FuelUnits>
+                  <AnnualFuelAmount>0</AnnualFuelAmount>
+                  <AnnualSteamGeneration>290471000</AnnualSteamGeneration>
+                  <EmissionFactors EmissionFactorType="DefaultOrMeasuredEF">
+                    <EmissionFactor>
+                      <EmissionFactorGas>CO2</EmissionFactorGas>
+                      <EmissionFactorAmount>200</EmissionFactorAmount>
+                      <EmissionFactorUnitType>g/GJ</EmissionFactorUnitType>
+                    </EmissionFactor>
+                    <EmissionFactor>
+                      <EmissionFactorGas>CH4</EmissionFactorGas>
+                      <EmissionFactorAmount>222</EmissionFactorAmount>
+                      <EmissionFactorUnitType>g/GJ</EmissionFactorUnitType>
+                    </EmissionFactor>
+                  </EmissionFactors>
+                  <AlternativeMethodologyDescription/>
+                </Fuel>
+                <Fuel>
+                  <FuelType>Residual Fuel Oil (#5 &amp; 6)</FuelType>
+                  <FuelClassification>non-biomass</FuelClassification>
+                  <FuelDescription/>
+                  <FuelUnits>kilolitres</FuelUnits>
+                  <AnnualFuelAmount>9441</AnnualFuelAmount>
+                  <AnnualWeightedAverageCarbonContent>0.862</AnnualWeightedAverageCarbonContent>
+                  <AlternativeMethodologyDescription/>
+                </Fuel>
+              </Fuels>
+            </Unit>
+          </Units>
+        </SubProcess>
+      </Process>
+      <Process>
+        <SubProcess SubprocessName="Additional Reportable Information as per WCI.352(i)(1)-(12)" InformationRequirement="MandatoryAdditional">
+          <Amount AmtDomain="PulpAndPaperBlackLiquor" AmtAction="Combusted" AmtPeriod="Annual">168389</Amount>
+          <PercentSolidsByWeight>53</PercentSolidsByWeight>
+          <PulpAndPaperCarbonates/>
+        </SubProcess>
+      </Process>
+    </ActivityPages>
+  </ActivityData>
+</ReportData>
+$$, '2018-06-22 19:10:25-07'::timestamptz);
+
+
+-- Run table export function without clearing the materialized views (for data equality tests below)
+SET client_min_messages TO WARNING; -- load is a bit verbose
+select swrs_transform.load(true, false);
+
+-- Table swrs.emission_factor exists
+select has_table('swrs'::name, 'emission_factor'::name);
+
+-- Emission Factor has pk
+select has_pk('swrs', 'emission_factor', 'emission_factor has primary key');
+
+-- Emission Factor has fk
+select has_fk('swrs', 'emission_factor', 'emission_factor has foreign key constraint(s)');
+
+-- Emission Factor has data
+select isnt_empty('select * from swrs.emission_factor', 'there is data in swrs.emission_factor');
+
+select results_eq(
+  $$ 
+  select count(*) from swrs.emission_factor where emission_factor_type is null
+  $$,
+  $$
+  values (2::bigint)
+  $$,
+  'emission_factor load function inserts pre-2017 records (emission_factor_type did not exist pre-2017)'
+);
+
+select results_eq(
+  $$ 
+  select count(*) from swrs_transform.emission_factor where emission_factor_type is not null
+  $$,
+  $$
+  values (2::bigint)
+  $$,
+  'emission_factor load function inserts post-2017 records'
+);
+
+-- FKey tests
+-- Emission Factor -> Fuel
+select set_eq(
+    $$
+    select distinct(fuel.eccc_xml_file_id) from swrs.emission_factor
+    join swrs.fuel
+    on emission_factor.fuel_id = fuel.id
+    $$,
+
+    'select distinct(eccc_xml_file_id) from swrs.fuel',
+
+    'Foreign key fuel_id in swrs.emission_factor references swrs.fuel.id'
+);
+-- Data in swrs_transform.emission_factor === data in swrs.emission_factor (not including emission_factor_type or default_or_measured because they don't exist in pre-2017 data)
+select set_eq(
+              $$
+              select
+                eccc_xml_file_id,
+                activity_name,
+                sub_activity_name,
+                emission_factor_amount,
+                emission_factor_gas,
+                emission_factor_unit_type
+              from swrs_transform.emission_factor
+                eccc_xml_file_id
+              union
+              select
+                eccc_xml_file_id,
+                activity_name,
+                sub_activity_name,
+                measured_emission_factor_amount as emission_factor_amount,
+                measured_emission_factor_gas as emission_factor_gas,
+                measured_emission_factor_unit_type as emission_factor_unit_type
+              from swrs_transform.measured_emission_factor
+              order by
+                eccc_xml_file_id
+              $$,
+
+              $$
+              select
+                  eccc_xml_file_id,
+                  activity_name,
+                  sub_activity_name,
+                  emission_factor_amount,
+                  emission_factor_gas,
+                  emission_factor_unit_type
+                from swrs.emission_factor
+                order by
+                    eccc_xml_file_id
+                 asc
+              $$,
+
+              'data in swrs_transform.emission_factor === swrs.emission_factor');
+
+
+
+select * from finish();
+rollback;
